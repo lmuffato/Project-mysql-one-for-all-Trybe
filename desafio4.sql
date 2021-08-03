73b20dc62ef000e218504e85f3f210c18f1d5557
Mateus <mtauws@gmail.com>
2021-08-03 18:56:21 -0300

    chore: req 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+CREATE VIEW spotifyclone.top_3_artistas AS
+SELECT
+  a.artista artista,
+  COUNT(DISTINCT us.usuario_id) seguidores
+FROM
+  spotifyclone.artistas a
+  INNER JOIN spotifyclone.usuarios_seguidas us ON a.artista_id = us.artista_id
+GROUP BY
+  artista
+ORDER BY
+  seguidores DESC
+LIMIT
+  3;
