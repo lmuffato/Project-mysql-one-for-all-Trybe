b5924a6149ea508dcd1f0ff0d81aff81ab249693
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-08-03 18:27:01 -0400

    feat: :sparkles: Requirement 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+DROP VIEW IF EXISTS perfil_artistas;
+CREATE VIEW SpotifyClone.perfil_artistas AS
+SELECT
+      artist.artista AS `artista`,
+      album.album AS `album`,
+      COUNT(follow.artista_id) AS `seguidores`
+FROM
+SpotifyClone.artistas AS artist
+INNER JOIN SpotifyClone.albuns AS album ON artist.artista_id = album.artista_id
+INNER JOIN SpotifyClone.seguindo AS follow ON artist.artista_id = follow.artista_id
+GROUP BY `artista`, `album`
+ORDER BY `seguidores` DESC, `artista`, `album`;
