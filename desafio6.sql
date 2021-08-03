d63807e5422556f555335f0d64f5c9178f11ba9d
maricegalla <maricegalla@gmail.com>
2021-08-03 14:55:13 -0300

    Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,8 @@
-USE SpotifyClone;
 CREATE VIEW faturamento_atual AS
 SELECT CONVERT(min(p.valor),DECIMAL(10,2)) AS 'faturamento_minimo',
 max(p.valor) AS 'faturamento_maximo',
 CONVERT(round(sum(p.valor)/count(u.usuario_id),2),DECIMAL(10,2)) AS 'faturamento_medio',
-CONVERT((p.valor),DECIMAL(10,2)) AS 'faturamento_total'
+CONVERT(sum(p.valor),DECIMAL(10,2)) AS 'faturamento_total'
 FROM SpotifyClone.Usuarios AS u
 INNER JOIN SpotifyClone.Planos AS p
-ON u.plano_id = p.plano_id
-GROUP BY valor;
+ON u.plano_id = p.plano_id;
