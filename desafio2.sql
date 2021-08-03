1595c2ed077362aea0d0060fb12b5f7df100340b
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-08-03 18:35:10 -0300

    feat: challenge 2

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+  SELECT 
+    COUNT(*) AS `cancoes`,
+    (
+      SELECT
+        COUNT(*)
+      FROM SpotifyClone.artists
+    ) AS `artistas`,
+    (
+      SELECT
+        COUNT(*)
+      FROM SpotifyClone.albums
+    ) AS `albuns`
+  FROM SpotifyClone.songs;
