f8bdb12f3c9e73dc5b8e40848a7f1b827e60dbe0
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-08-03 18:26:48 -0300

    requisito 9 refeito

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT tablesongs.song AS 'cancao', COUNT(tablehistory.song_id) AS 'reproducoes'
-FROM SpotifyClone.rep_history AS tablehistory
-JOIN SpotifyClone.songs AS tablesongs ON tablehistory.song_id_fk = tablesongs.song_id
-GROUP BY tablesongs.song ORDER BY 2 DESC , 1 ASC LIMIT 2;
+SELECT songs.song AS cancao, COUNT(tablehistory.song_id) AS reproducoes
+FROM SpotifyClone.songs AS songs
+JOIN SpotifyClone.rep_history AS tablehistory ON tablesongs.song_id = tablehistory.song_id_fk
+GROUP BY cancao ORDER BY reproducoes DESC , cancao ASC LIMIT 2;
