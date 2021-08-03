c08f6dd9283f978343d1db66927040f571ed72a2
RenzoSev <sevilharenzo@gmail.com>
2021-08-03 14:53:44 -0300

    fix?: desafio3/5 switch historico_de_reproducao to historico_reproducao

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.usuario AS usuario, c.cancao AS nome
-FROM SpotifyClone.historico_de_reproducao AS h
+FROM SpotifyClone.historico_reproducao AS h
 JOIN SpotifyClone.usuario AS u
 ON h.usuario_id = u.usuario_id
 JOIN SpotifyClone.cancao AS c
--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 CREATE VIEW top_2_hits_do_momento AS
 SELECT c.cancao, COUNT(h.cancao_id) AS reproducoes
-FROM SpotifyClone.historico_de_reproducao AS h
+FROM SpotifyClone.historico_reproducao AS h
 JOIN SpotifyClone.cancao AS c
 ON h.cancao_id = c.cancao_id
 GROUP BY h.cancao_id
