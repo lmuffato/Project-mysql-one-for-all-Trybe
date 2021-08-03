c83fac37e98637cbf45efb88d163b05048639ed6
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-08-03 13:13:57 -0300

    requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGERS trigger_usuario_delete BEFORE DELETE
+ON users
+FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.followers WHERE user_id_fk = OLD.user_id;
+    DELETE FROM SpotifyClone.history WHERE user_id_fk = OLD.user_id;
+END
+$$ DELIMITER ;
