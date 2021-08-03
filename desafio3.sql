eadd7d46f53a5ae10af796fa454f6ff9920e1687
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-08-03 17:07:25 -0300

    teste

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT users.username AS 'usuario', songs.song_name AS 'nome'
+SELECT tabuser.username AS 'usuario', tabsong.song_name AS 'nome'
 FROM SpotifyClone.rep_history AS historico
 INNER JOIN SpotifyClone.users AS tabuser ON historico.user_id_fk = tabuser.user_id
 INNER JOIN SpotifyClone.songs AS tabsong ON historico.song_id_fk = tabsong.song_id
