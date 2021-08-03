3b587b6d8535ad158ed83b17f238cc91645362e1
Robertson <robertson.nunes@gmail.com>
2021-08-03 17:54:33 -0300

    Requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        s.single cancao, COUNT(h.user_id) reproducoes
+    FROM
+        SpotifyClone.History h
+            INNER JOIN
+        SpotifyClone.Singles s ON h.single_id = s.single_id
+    GROUP BY c.single
+    ORDER BY COUNT(h.user_id) DESC
+    LIMIT 2;
