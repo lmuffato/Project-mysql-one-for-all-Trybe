54ae0106cf7654fd2cac677ee2904818cf1cf4ff
RenzoSev <sevilharenzo@gmail.com>
2021-08-03 14:36:40 -0300

    feat: desafio 5.sql

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.cancao, COUNT(h.cancao_id) AS reproducoes
+FROM SpotifyClone.historico_de_reproducao AS h
+JOIN SpotifyClone.cancao AS c
+ON h.cancao_id = c.cancao_id
+GROUP BY h.cancao_id
+ORDER BY `reproducoes` DESC, `cancao`
+LIMIT 2;
