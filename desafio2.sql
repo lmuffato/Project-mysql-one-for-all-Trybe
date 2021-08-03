99f44a455a1695d16b55d414ad52fc25c86d10f9
Flavio Franco <flavio.win@gmail.com>
2021-08-03 17:40:53 -0300

    requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+CREATE VIEW estatisticas_musicais AS
+  SELECT
+    (SELECT
+        COUNT(*)
+      FROM
+        SpotifyClone.cancao) AS `cancoes`,
+    (SELECT
+        COUNT(*)
+      FROM
+        SpotifyClone.artista) AS `artistas`,
+    (SELECT
+        COUNT(*)
+      FROM
+        SpotifyClone.album) AS `albuns`;
