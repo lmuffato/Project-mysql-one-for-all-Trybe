535f838c3c536603133cc27afe7244ac301cceb1
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-03 17:53:51 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+DROP VIEW IF EXISTS estatisticas_musicais;
+CREATE VIEW estatisticas_musicais AS
+  SELECT 
+    COUNT(*) AS `cancoes`,
+    (SELECT COUNT(*) FROM SpotifyClone.Artists) AS `artistas`,
+    (SELECT COUNT(*) FROM SpotifyClone.Albuns)  AS `albuns`
+  FROM SpotifyClone.Songs;
+  
