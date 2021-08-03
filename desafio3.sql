6912a1d539c9c3ce84d22f78277def76246b80b3
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-03 18:08:24 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DROP VIEW IF EXISTS historico_reproducao_usuarios;
+CREATE VIEW historico_reproducao_usuarios AS
+  SELECT 
+    U.user_name AS `usuario`,
+    S.song_name AS `nome`
+  FROM SpotifyClone.User_history AS UH
+  INNER JOIN SpotifyClone.Songs AS S
+  INNER JOIN SpotifyClone.Users AS U
+  ON UH.user_id = U.user_id AND UH.song_id = S.song_id
+  ORDER BY `usuario` ASC, `nome` ASC;
+  
