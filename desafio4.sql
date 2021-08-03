7b273d4a778919a4299641da7d3390ced0f6287f
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-03 18:32:25 -0300

    desafio3

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DROP VIEW IF EXISTS top_3_artistas;
+CREATE VIEW top_3_artistas AS
+  SELECT 
+    A.artist_name AS `artista`,
+    COUNT(F.user_id) AS `seguidores`
+  FROM SpotifyClone.Followed_artists AS F
+  INNER JOIN SpotifyClone.Artists AS A
+  ON F.artist_id = A.artist_id
+  GROUP BY F.artist_id
+  ORDER BY `seguidores` DESC, `artista` ASC
+  LIMIT 3;
