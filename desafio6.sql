801718a59474e1860a5c22deaa9d4a8930490e4d
Gui-lira <gui.lira.martins@gmail.com>
2021-08-03 20:09:36 -0300

    correção 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS 
-SELECT MIN(s.price) AS faturamento_minimo,
-MAX(s.price) AS faturamento_maximo,
-ROUND(AVG(s.price), 2) AS faturamento_medio,
-SUM(s.price) AS faturamento_total
+SELECT MIN(s.price)  AS faturamento_minimo,
+ROUND(MAX(s.price), 2) AS faturamento_maximo,
+CAST(ROUND(AVG(s.price), 2) AS DECIMAL(10,2)) AS faturamento_medio,
+ROUND(SUM(s.price), 2) AS faturamento_total
 FROM SpotifyClone.subscriptions s
 INNER JOIN SpotifyClone.`user`  u
 WHERE s.subscription_id = u.subscription;
