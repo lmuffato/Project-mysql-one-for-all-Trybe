8fb6f6a271ba099fe1944d7b7f4a33f95e427365
maricegalla <maricegalla@gmail.com>
2021-08-03 13:49:05 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT a.nome_artista AS 'artista',
+count(s.usuario_id) AS 'seguidores'
+FROM SpotifyClone.Seguindo AS s
+INNER JOIN SpotifyClone.Artistas AS a
+ON s.artista_id = a.artista_id
+GROUP BY a.nome_artista
+LIMIT 3;
