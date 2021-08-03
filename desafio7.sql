59f8f7029a7da15e4fd3c3d6e7763b808cb59988
RenzoSev <sevilharenzo@gmail.com>
2021-08-03 14:37:07 -0300

    feat: desafio 7.sql

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+CREATE VIEW perfil_artistas AS
+SELECT art.artista, alb.album, COUNT(alb.artista_id) seguidores
+FROM SpotifyClone.album AS alb
+JOIN SpotifyClone.artista AS art
+ON alb.artista_id = art.artista_id
+JOIN SpotifyClone.seguindo_artista AS sa
+ON alb.artista_id = sa.artista_id
+GROUP BY alb.artista_id, album
+ORDER BY `seguidores` DESC, `artista`;
