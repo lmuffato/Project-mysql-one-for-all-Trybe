49d41ba095a0437fad779002dcc686d320bdb3c8
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-03 19:09:09 -0300

    Desafio 1 in progress

--- desafio1.sql
+++ desafio1.sql
@@ -15,6 +15,7 @@ CREATE TABLE user(
   name_user VARCHAR(80) NOT NULL,
   birthdate DATE,
   id_plan INT,
+  id_music INT,
   FOREIGN KEY (id_plan) REFERENCES plan(id_plan)
   );
     
@@ -52,3 +53,83 @@ CREATE TABLE history(
   FOREIGN KEY (id_user) REFERENCES user(id_user),
   FOREIGN KEY (id_music) REFERENCES music(id_music)
   );
+
+INSERT INTO plan(name_plan, price) VALUES ('gratuito', 0);
+INSERT INTO plan(name_plan, price) VALUES ('familiar', 7.99);
+INSERT INTO plan(name_plan, price) VALUES ('universitário', 5.99);
+
+INSERT INTO user(name_user, birthdate, id_plan) VALUES ('Thati', '1998-02-01' , 1);
+INSERT INTO user(name_user, birthdate, id_plan) VALUES ('Cintia', '1986-08-30' , 2);
+INSERT INTO user(name_user, birthdate, id_plan) VALUES ('Bill', '2001-12-04' , 3);
+INSERT INTO user(name_user, birthdate, id_plan) VALUES ('Roger', '1976-10-25' , 1);
+
+INSERT INTO artist(name_artist, id_user) VALUES ('Walter Phoenix', 1);
+INSERT INTO artist(name_artist, id_user) VALUES ('Peter Strong', 2);
+INSERT INTO artist(name_artist, id_user) VALUES ('Lance Day', 3);
+INSERT INTO artist(name_artist, id_user) VALUES ('Freedie Shannon', 4);
+
+INSERT INTO following(id_user, id_artist) VALUES (1, 1);
+INSERT INTO following(id_user, id_artist) VALUES (1, 1);
+INSERT INTO following(id_user, id_artist) VALUES (1, 3);
+INSERT INTO following(id_user, id_artist) VALUES (1, 4);
+
+INSERT INTO following(id_user, id_artist) VALUES (2, 3);
+INSERT INTO following(id_user, id_artist) VALUES (2, 4);
+INSERT INTO following(id_user, id_artist) VALUES (2, 1);
+INSERT INTO following(id_user, id_artist) VALUES (2, 3);
+
+INSERT INTO following(id_user, id_artist) VALUES (3, 1);
+INSERT INTO following(id_user, id_artist) VALUES (3, 3);
+INSERT INTO following(id_user, id_artist) VALUES (4, 1);
+
+INSERT INTO following(id_user, id_artist) VALUES (4, 1);
+INSERT INTO following(id_user, id_artist) VALUES (4, 4);
+INSERT INTO following(id_user, id_artist) VALUES (4, 2);
+
+INSERT INTO album(name_album, id_artist) VALUES ('Envious', 1);
+INSERT INTO album(name_album, id_artist) VALUES ('Exuberant', 1);
+INSERT INTO album(name_album, id_artist) VALUES ('Hallowed Steam', 2);
+INSERT INTO album(name_album, id_artist) VALUES ('Incandescent', 3);
+INSERT INTO album(name_album, id_artist) VALUES ('Temporary Culture', 4);
+
+INSERT INTO music(name_music, id_album) VALUES ('Soul For Us', 1);
+INSERT INTO music(name_music, id_album) VALUES ('Reflections Of Magic', 1);
+INSERT INTO music(name_music, id_album) VALUES ('Dance With Her Own', 1);
+
+INSERT INTO music(name_music, id_album) VALUES ('Troubles Of My Inner Fire', 2);
+INSERT INTO music(name_music, id_album) VALUES ('Time Fireworks', 2);
+
+INSERT INTO music(name_music, id_album) VALUES ('Magic Circus', 3);
+INSERT INTO music(name_music, id_album) VALUES ('Honey, So Do I', 3);
+INSERT INTO music(name_music, id_album) VALUES ("Sweetie, Let's Go Wild", 3);
+INSERT INTO music(name_music, id_album) VALUES ('She Knows', 3);
+
+INSERT INTO music(name_music, id_album) VALUES ('Fantasy For Me', 4);
+INSERT INTO music(name_music, id_album) VALUES ('Celebration Of More', 4);
+INSERT INTO music(name_music, id_album) VALUES ('Rock His Everything', 4);
+INSERT INTO music(name_music, id_album) VALUES ('Home Forever', 4);
+INSERT INTO music(name_music, id_album) VALUES ('Diamond Power', 4);
+INSERT INTO music(name_music, id_album) VALUES ("Honey, Let's Be Silly", 4);
+
+INSERT INTO music(name_music, id_album) VALUES ('Thang Of Thunder', 5);
+INSERT INTO music(name_music, id_album) VALUES ('Words Of Her Life', 5);
+INSERT INTO music(name_music, id_album) VALUES ('Without My Streets', 5);
+
+INSERT INTO history(id_user, id_music) VALUES (1,1);
+INSERT INTO history(id_user, id_music) VALUES (1,6);
+INSERT INTO history(id_user, id_music) VALUES (1,14);
+INSERT INTO history(id_user, id_music) VALUES (1,16);
+
+INSERT INTO history(id_user, id_music) VALUES (2,13);
+INSERT INTO history(id_user, id_music) VALUES (2,17);
+INSERT INTO history(id_user, id_music) VALUES (2,2);
+INSERT INTO history(id_user, id_music) VALUES (2,15);
+
+INSERT INTO history(id_user, id_music) VALUES (3,4);
+INSERT INTO history(id_user, id_music) VALUES (3,16);
+INSERT INTO history(id_user, id_music) VALUES (3,6);
+
+INSERT INTO history(id_user, id_music) VALUES (4,3);
+INSERT INTO history(id_user, id_music) VALUES (4,18);
+INSERT INTO history(id_user, id_music) VALUES (4,11);
+
