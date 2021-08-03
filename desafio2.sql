bb9c3ae04f4d714e88adb84a5dac0a4fb1a9b57f
Orlando Flores <orlando_flores@id.uff.br>
2021-08-03 18:03:39 -0300

    feat: desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT COUNT(*) AS 'cancoes',
+    (SELECT COUNT(*) FROM SpotifyClone.artists) AS 'artistas',
+    (SELECT COUNT(*) FROM SpotifyClone.albums) AS 'albums'
+	FROM SpotifyClone.songs;
