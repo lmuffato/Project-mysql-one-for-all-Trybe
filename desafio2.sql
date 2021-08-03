e35846ce21e7bb6330d9a4165d68b5787cbf3fa1
MuriloBatista <mub4si@gmail.com>
2021-08-03 17:43:57 -0300

    Desafio2

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+  SELECT (SELECT COUNT(*) FROM SpotifyClone.songs) AS 'cancoes',
+    (SELECT COUNT(*) FROM SpotifyClone.artists) AS 'artistas',
+    (SELECT COUNT(*) FROM SpotifyClone.albuns) AS 'albuns';
