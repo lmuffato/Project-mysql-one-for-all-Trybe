c45b29127a0d53d36f7d1b23e81c2e4f7d9aa50f
Bruno Mendes <mendessbruno@gmail.com>
2021-08-03 17:10:51 -0300

    tentativa de resolver desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -2,10 +2,10 @@ USE SpotifyClone;
 
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-    AFTER DELETE ON `user`
+    BEFORE DELETE ON `user`
     FOR EACH ROW
 BEGIN
-DELETE FROM user_following WHERE user_id = 1;
-DELETE FROM play_history WHERE user_id = 1;
+DELETE FROM user_following WHERE user_id = OLD.user_id;
+DELETE FROM play_history WHERE user_id = OLD.user_id;
 END $$
 DELIMITER ;
