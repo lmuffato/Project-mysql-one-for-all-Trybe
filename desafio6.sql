ac0e319ecf0f45ea7a5664768462c8945ea1be4b
Flavio Franco <flavio.win@gmail.com>
2021-08-03 20:45:37 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+CREATE VIEW faturamento_atual AS
+  SELECT
+    MIN(p.valor_plano) AS 'faturamento_minimo',
+    MAX(p.valor_plano) AS 'faturamento_maximo',
+    ROUND(AVG(p.valor_plano), 2) AS 'faturamento_medio',
+    SUM(p.valor_plano) AS 'faturamento_total'
+  FROM
+    SpotifyClone.plano p
+  INNER JOIN
+    SpotifyClone.usuario u ON u.plano_id = p.plano_id;
