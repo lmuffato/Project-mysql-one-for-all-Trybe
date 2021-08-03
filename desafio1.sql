1104e11b1c3913aaa7f59b204abc53b34b639888
luanlsr <luanfswd@gmail.com>
2021-08-03 18:46:03 -0300

    requisito 1 finalizado

--- desafio1.sql
+++ desafio1.sql
@@ -2,25 +2,18 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
 
-CREATE TABLE SpotifyClone.usuarios(
-  usuario_id INT PRIMARY KEY AUTO_INCREMENT,
-  usuario VARCHAR(255) NOT NULL,
-  idade INT NOT NULL,
-  plano_id INT,
-  FOREIGN KEY (plano_id) REFERENCES planos(plano_id)
-) engine = InnoDB;
-
 CREATE TABLE SpotifyClone.planos(
   plano_id INT PRIMARY KEY AUTO_INCREMENT,
   plano VARCHAR(255) NOT NULL,
   valor_plano INT NOT NULL
 ) engine = InnoDB;
 
-CREATE TABLE SpotifyClone.cancoes(
-  cancao_id INT PRIMARY KEY AUTO_INCREMENT,
-  cancao VARCHAR(255) NOT NULL,
-  album_id INT,
-  FOREIGN KEY (album_id) REFERENCES albuns(album_id)
+CREATE TABLE SpotifyClone.usuarios(
+  usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario VARCHAR(255) NOT NULL,
+  idade INT NOT NULL,
+  plano_id INT,
+  FOREIGN KEY (plano_id) REFERENCES SpotifyClone.planos(plano_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.artistas(
@@ -32,21 +25,110 @@ CREATE TABLE SpotifyClone.albuns(
   album_id INT PRIMARY KEY AUTO_INCREMENT,
   album VARCHAR(255) NOT NULL,
   artista_id INT,
-  FOREIGN KEY(artista_id) REFERENCES artistas(artista_id)
+  FOREIGN KEY(artista_id) REFERENCES SpotifyClone.artistas(artista_id)
+) engine = InnoDB;
+
+CREATE TABLE SpotifyClone.cancoes(
+  cancao_id INT PRIMARY KEY AUTO_INCREMENT,
+  cancao VARCHAR(255) NOT NULL,
+  album_id INT,
+  FOREIGN KEY (album_id) REFERENCES SpotifyClone.albuns(album_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.seguindo_artistas(
   usuario_id INT NOT NULL,
   artista_id INT NOT NULL,
-  CONSTRAINT PRIMARY KEY(usuario_id, artista_id),
-  FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
-  FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
+  PRIMARY KEY(usuario_id, artista_id),
+  FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios(usuario_id),
+  FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas(artista_id)
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.historico_de_reproducoes(
-	usuario_id INT NOT NULL,
+  usuario_id INT NOT NULL,
   cancao_id INT NOT NULL,
-  CONSTRAINT PRIMARY KEY(usuario_id, cancao_id),
-  FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
-  FOREIGN KEY (cancao_id) REFERENCES cancoes(cancao_id)
+  PRIMARY KEY(usuario_id, cancao_id),
+  FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios(usuario_id),
+  FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancoes(cancao_id)
 ) engine = InnoDB;
+
+INSERT INTO SpotifyClone.planos (plano, valor_plano)
+VALUES
+  ('Gratuito', 0),
+  ('Universitário', 5.99),
+  ('Família', 7.99);
+
+INSERT INTO SpotifyClone.usuarios (usuario, idade, plano_id)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 3),
+  ('Bill', 20, 2),
+  ('Roger', 45, 1);
+  
+  INSERT INTO SpotifyClone.artistas (artista)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+
+INSERT INTO SpotifyClone.albuns (album, artista_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+  
+  INSERT INTO SpotifyClone.cancoes (cancao, album_id)
+VALUES
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ('Sweetie', 3),
+  ("Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ('Honey', 4),
+  ("Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
+
+
+  INSERT INTO SpotifyClone.seguindo_artistas (usuario_id, artista_id)
+VALUES
+  (1, 1),
+  (1, 3),
+  (1, 4),
+  (2, 1),
+  (2, 4),
+  (3, 1),
+  (3, 2),
+  (4, 3);
+  
+
+  INSERT INTO SpotifyClone.historico_de_reproducoes (usuario_id, cancao_id)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 15),
+  (1, 18),
+  (2, 14),
+  (2, 19),
+  (2, 2),
+  (2, 16),
+  (2, 17),
+  (3, 4),
+  (3, 18),
+  (3, 6),
+  (4, 3),
+  (4, 20),
+  (4, 12);
