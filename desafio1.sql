a576a6c860828fa40797d88cc67cf3ba0f2dbe95
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-08-03 20:29:24 -0300

    Fix: desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -1,10 +1,12 @@
-CREATE DATABASE IF NOT EXISTS SpotifyClone;
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
 
 CREATE TABLE SpotifyClone.plans(
 planID INT PRIMARY KEY AUTO_INCREMENT,
 planName VARCHAR(80) NOT NULL,
 planValue DECIMAL(5,2) NOT NULL
-);
+) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.users(
 userID INT PRIMARY KEY AUTO_INCREMENT,
@@ -12,26 +14,26 @@ userName VARCHAR(100) NOT NULL,
 userAge INT NOT NULL,
 planID INT NOT NULL,
 FOREIGN KEY (planID) REFERENCES SpotifyClone.plans(planID)
-);
+) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.artists(
 artistID INT PRIMARY KEY AUTO_INCREMENT,
 artistName VARCHAR(100) NOT NULL
-);
+) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.albums(
 albumID INT PRIMARY KEY AUTO_INCREMENT,
 albumName VARCHAR(200) NOT NULL,
 artistID INT NOT NULL,
 FOREIGN KEY (artistID) REFERENCES SpotifyClone.artists(artistID)
-);
+) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.songs(
 songID INT PRIMARY KEY AUTO_INCREMENT,
 songName VARCHAR(200) NOT NULL,
 albumID INT NOT NULL,
 FOREIGN KEY (albumID) REFERENCES SpotifyClone.albums(albumID)
-);
+) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.followingArtist(
 artistID INT NOT NULL,
@@ -39,7 +41,7 @@ userID INT NOT NULL,
 CONSTRAINT PRIMARY KEY(artistID, userID),
 FOREIGN KEY (artistID) REFERENCES SpotifyClone.artists(artistID),
 FOREIGN KEY (userID) REFERENCES SpotifyClone.users(userID)
-);
+) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.listeningHistory(
 songID INT NOT NULL,
@@ -47,56 +49,56 @@ userID INT NOT NULL,
 CONSTRAINT PRIMARY KEY(songID, userID),
 FOREIGN KEY (songID) REFERENCES SpotifyClone.songs(songID),
 FOREIGN KEY (userID) REFERENCES SpotifyClone.users(userID)
-);
+) engine = InnoDB;
 
-INSERT INTO SpotifyClone.plans(planID, planName, planValue)
+INSERT INTO SpotifyClone.plans(planName, planValue)
 VALUES
-(1, 'free', 0),
-(2, 'family', 7.99),
-(3, 'university', 5.99);
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99);
 
-INSERT INTO SpotifyClone.users(userID, userName, userAge, planID)
+INSERT INTO SpotifyClone.users(userName, userAge, planID)
 VALUES
-(1, 'Thati', 23, 1),
-(2, 'Cintia', 35, 2),
-(3, 'Bill', 20, 3),
-(4, 'Roger', 45, 1);
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
 
-INSERT INTO SpotifyClone.artists(artistID, artistName)
+INSERT INTO SpotifyClone.artists(artistName)
 VALUES
-(1, 'Walter Phoenix'),
-(2, 'Peter Strong'),
-(3, 'Lance Day'),
-(4, 'Freedie Shannon');
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
-INSERT INTO SpotifyClone.albums(albumID, albumName, artistID)
+INSERT INTO SpotifyClone.albums(albumName, artistID)
 VALUES
-(1, 'Envious', 1),
-(2, 'Exuberant', 1),
-(3, 'Hallowed Steam', 2),
-(4, 'Incandescent', 3),
-(5, 'Temporary Culture', 4);
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
-INSERT INTO SpotifyClone.songs(songID, songName, albumID)
+INSERT INTO SpotifyClone.songs(songName, albumID)
 VALUES
-(1, 'Soul For Us', 1),
-(2, 'Reflections Of Magic', 1),
-(3, 'Dance With Her Own', 1),
-(4, 'Troubles Of My Inner Fire', 2),
-(5, 'Time Fireworks', 2),
-(6, 'Magic Circus', 3),
-(7, 'Honey, So Do I', 3),
-(8, "Sweetie, Let's Go Wild", 3),
-(9, 'She Knows', 3),
-(10, 'Fantasy For Me', 4),
-(11, 'Celebration Of More', 4),
-(12, 'Rock His Everything', 4),
-(13, 'Home Forever', 4),
-(14, 'Diamond Power', 4),
-(15, "Honey, Let's Be Silly", 4),
-(16, 'Thang Of Thunder', 5),
-(17, 'Words Of Her Life', 5),
-(18, 'Without My Streets', 5);
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
 
 INSERT INTO SpotifyClone.followingArtist(artistID, userID)
 VALUES
