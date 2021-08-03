4559884e8bfde0d740b0af6b7efa9070e2815430
maricegalla <maricegalla@gmail.com>
2021-08-03 00:04:44 -0300

    Normalização

--- desafio1.sql
+++ desafio1.sql
@@ -85,3 +85,52 @@ VALUES
 (3, 'Hallowed Steam', 4),
 (4, 'Incandescent', 3),
 (5, 'Temporary Culture', 2);
+
+INSERT INTO  SpotifyClone.Cancoes(cancao_id, album_id, cancao)
+VALUES
+(1,1,'Soul For Us'),
+(2,1,'Reflections Of Magic'),
+(3,1,'Dance With Her Own'),
+(4,2,'Troubles Of My Inner Fire'),
+(5,2,'Time Fireworks'),
+(6,3,'Magic Circus'),
+(7,3,'Honey, So Do I'),
+(8,3,"Sweetie, Let's Go Wild"),
+(9,3,'She Knows'),
+(10,4,'Fantasy For Me'),
+(11,4,'Celebration Of More'),
+(12,4,'Rock His Everything'),
+(13,4,'Home Forever'),
+(14,4,'Diamond Power'),
+(15,4, "Honey, Let's Be Silly"),
+(16,5,'Thang Of Thunder'),
+(17,5,'Words Of Her Life'),
+(18,5,'Without My Streets');
+
+INSERT INTO SpotifyClone.Historico(historico_id, usuario_id, cancao_id)
+VALUES
+(1,1,1),
+(2,1,6),
+(3,1,14),
+(4,1,16),
+(5,2,13),
+(6,2,17),
+(7,2,2),
+(8,2,15),
+(9,3,4),
+(10,3,16),
+(11,3,6),
+(12,4,3),
+(13,4,18),
+(14,4,11);
+
+INSERT INTO SpotifyClone.Seguindo(seguindo_id, usuario_id, artista_id)
+VALUES
+(1,1,1),
+(2,1,2),
+(3,1,3),
+(4,2,1),
+(5,2,3),
+(6,3,4),
+(7,3,1),
+(8,4,2 );
