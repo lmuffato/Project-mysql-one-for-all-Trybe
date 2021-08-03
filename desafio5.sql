1a33210e6ecfcaa5864b147319bd404bfdb57c9b
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-03 18:40:22 -0300

    desafio5

---
+++ desafio5.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DROP VIEW IF EXISTS top_2_hits_do_momento;
+CREATE VIEW top_2_hits_do_momento AS
+  SELECT 
+    S.song_name AS `cancao`,
+    COUNT(UH.user_id) AS `reproducoes`
+  FROM SpotifyClone.User_history AS UH
+  INNER JOIN SpotifyClone.Songs AS S
+  ON UH.song_id = S.song_id
+  GROUP BY UH.song_id
+  ORDER BY `reproducoes` DESC, `cancao` ASC
+  LIMIT 2;
