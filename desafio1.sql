e5fe95c79577414c9326ed2b8af1ae5a9e6ebc3b
Maria Baeta <m.martinsdg@gmail.com>
2021-08-03 17:09:12 -0300

    Iniciando o projeto - Req1

---
+++ desafio1.sql
@@ -0,0 +1,117 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE plans (
+    flat_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    flat VARCHAR(250) NOT NULL UNIQUE,
+    price DOUBLE NOT NULL
+)  ENGINE=INNODB;
+
+CREATE TABLE SpotifyClone.users (
+    user_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL UNIQUE,
+    age INT(11) NOT NULL,
+    flat_id INT NOT NULL,
+    FOREIGN KEY (flat_id)
+        REFERENCES plans (flat_id)
+)  ENGINE=INNODB;
+
+CREATE TABLE SpotifyClone.artists (
+    artist_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    artist VARCHAR(250) NOT NULL
+)  ENGINE=INNODB;
+
+CREATE TABLE SpotifyClone.albums (
+    album_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    album VARCHAR(250) NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id)
+        REFERENCES artists (artist_id)
+)  ENGINE=INNODB;
+
+CREATE TABLE SpotifyClone.following_artists (
+    user_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (user_id , artist_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id),
+    FOREIGN KEY (artist_id)
+        REFERENCES artists (artist_id)
+)  ENGINE=INNODB;
+
+CREATE TABLE SpotifyClone.songs (
+    song_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    song VARCHAR(250) NOT NULL,
+    album_id INT NOT NULL,
+    FOREIGN KEY (album_id)
+        REFERENCES albums (album_id)
+)  ENGINE=INNODB;
+
+CREATE TABLE SpotifyClone.reproduction_history (
+    user_id INT NOT NULL,
+    song_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY (user_id , song_id),
+    FOREIGN KEY (user_id)
+        REFERENCES users (user_id),
+    FOREIGN KEY (song_id)
+        REFERENCES songs (song_id)
+)  ENGINE=INNODB;
+
+INSERT INTO SpotifyClone.plans (flat, price) 
+VALUES ('gratuito', 0), ('familiar', 7.99), ('universitario', 5.99);
+
+INSERT INTO SpotifyClone.users (name, age, flat_id) 
+VALUES ('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+INSERT INTO SpotifyClone.artists (artist) 
+VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
+
+INSERT INTO SpotifyClone.albums (album, artist_id) 
+VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
+
+INSERT INTO SpotifyClone.following_artists (user_id, artist_id) 
+VALUES (1, 1), (1, 4), (1, 3), (2, 1), (2, 3), (3, 2), (3, 1),(4, 4);
+
+INSERT INTO SpotifyClone.songs (song, album_id) 
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3), 
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO SpotifyClone.reproduction_history(user_id, song_id) 
+VALUES 
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2,17),
+(2, 2),
+(2, 15),
+(3, 3),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
