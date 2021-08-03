8e20a9bef68cef941d6a59426a72081eb2acbde8
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-08-03 20:46:58 -0300

    add "

--- desafio1.sql
+++ desafio1.sql
@@ -60,9 +60,9 @@ INSERT INTO Musicas (Nome, Album_ID)
 VALUES ('Soul For Us', 1), ('Reflections Of Magic', 1),
 ('Dance With Her Own', 1), ('Troubles Of My Inner Fire', 2),
 ('Time Fireworks', 2), ('Magic Circus', 3), ('Honey, So Do I', 3),
-('Sweetie, Let"s Go Wild', 3), ('She Knows', 3), ('Fantasy For Me', 4),
+("Sweetie, Let's Go Wild", 3), ('She Knows', 3), ('Fantasy For Me', 4),
 ('Celebration Of More', 4),  ('Rock His Everything', 4), ('Home Forever', 4),
-('Diamond Power', 4),  ('Honey, Let"s Be Silly', 4), ('Thang Of Thunder', 5),
+('Diamond Power', 4),  ("Honey, Let's Be Silly', 4"), ('Thang Of Thunder', 5),
 ('Words Of Her Life', 5), ('Without My Streets', 5);
 
 CREATE TABLE Artistas_seguidos (
