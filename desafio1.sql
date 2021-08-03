2a2f613131376c9040302845e2ed5749fda3a243
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-03 18:21:33 -0300

    colocando values na tabela

--- desafio1.sql
+++ desafio1.sql
@@ -32,6 +32,12 @@ CREATE TABLE artists(
   name VARCHAR(50)
 ) engine = InnoDB;
 
+INSERT INTO artists (artist_id, name) VALUES
+  (1, 'Walter Phoenix'),
+  (2, 'Peter Strong'),
+  (3, 'Lance Day'),
+  (4, 'Freedie Shanno');
+
 CREATE TABLE albuns(
   album_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
@@ -39,12 +45,40 @@ CREATE TABLE albuns(
   FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 ) engine = InnoDB;
 
+INSERT INTO albuns (album_id, name, artist_id) VALUES
+  (1, 'Envious', 1),
+  (2, 'Exuberant', 1),
+  (3, 'Hallowed Steam', 2),
+  (4, 'Incandescent', 3),
+  (5, 'Temporary Culture', 4);
+
 CREATE TABLE songs(
   song_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50),
   album_id INT,
   FOREIGN KEY (album_id) REFERENCES albuns (album_id)
 ) engine = InnoDB;
+
+INSERT INTO songs (song_id, name, album_id) VALUES
+  (1, 'Soul For Us', 1),
+  (2, 'Reflections Of Magic', 1),
+  (3, 'Dance With Her Own', 1),
+  (4, 'Troubles Of My Inner Fire', 2),
+  (5, 'Time Fireworks', 2),
+  (6, 'Magic Circus', 3),
+  (7, 'Honey, So Do I', 3),
+  (8, "Sweetie, Let's Go Wild", 3),
+  (9, 'She Knows', 3),
+  (10, 'Fantasy For Me', 4),
+  (11, 'Celebration Of More', 4),
+  (12, 'Rock His Everything', 4),
+  (13, 'Home Forever', 4),
+  (14, 'Diamond Power', 4),
+  (15, "Honey, Let's Be Silly", 4),
+  (16, 'Thang Of Thunder', 5),
+  (17, 'Words Of Her Life', 5),
+  (18, 'Without My Streets', 5);
+  
 
 CREATE TABLE follows(
   user_id INT,
