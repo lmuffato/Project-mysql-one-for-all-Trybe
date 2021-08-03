037cec5065fd24f6cd29e1555b1937e768a70daa
João Nasc <joaonasc@yahoo.com>
2021-08-03 19:33:06 -0300

    feat: challenge 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+CREATE VIEW `SpotifyClone`.estatisticas_musicais AS
+  SELECT
+    COUNT (`songs`.song_id) AS `cancoes`,
+    (SELECT COUNT(`artist_id`) FROM `SpotifyClone`.artists) AS `artistas`,
+    (SELECT COUNT(`album_id`) FROM `SpotifyClone`.albums) AS `albuns`
+  FROM
+    `SpotifyClone`.songs AS `songs`;
