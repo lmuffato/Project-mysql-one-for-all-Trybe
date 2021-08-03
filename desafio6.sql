97ef79b1e81c54c4d914040fb5af755aa35a0f37
lucas-paz <lucas.hpaz@outlook.com>
2021-08-03 19:07:53 -0300

    fixes req 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,9 @@
-DROP VIEW IF EXISTS faturamento_atual;
 CREATE VIEW faturamento_atual AS
 SELECT
-  ROUND(MIN(P.price), 2) AS faturamento_minimo,
-  ROUND(MAX(P.price), 2) AS faturamento_maximo,
+  MIN(P.price) AS faturamento_minimo,
+  MAX(P.price) AS faturamento_maximo,
   ROUND(AVG(P.price), 2) AS faturamento_medio,
-  ROUND(SUM(P.price), 2) AS faturamento_total
+  SUM(P.price) AS faturamento_total
 FROM
   SpotifyClone.plans AS P
   INNER JOIN SpotifyClone.users AS U ON P.id = U.plan_id;
