2c07bb340c94f7398fcf0bd1a0fa855f00678263
RenzoSev <sevilharenzo@gmail.com>
2021-08-03 14:36:16 -0300

    feat: desafio 3.sql

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.usuario AS usuario, c.cancao AS nome
+FROM SpotifyClone.historico_de_reproducao AS h
+JOIN SpotifyClone.usuario AS u
+ON h.usuario_id = u.usuario_id
+JOIN SpotifyClone.cancao AS c
+ON h.cancao_id = c.cancao_id
+ORDER BY `usuario`, `nome`;
