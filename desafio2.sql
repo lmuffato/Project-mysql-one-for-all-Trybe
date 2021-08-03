22a6a8f6875ea5ecfcdfa710a0b4cff661c21aaf
Orlando Flores <orlando_flores@id.uff.br>
2021-08-03 18:12:52 -0300

    fix: code climate errors

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
-    SELECT COUNT(*) AS 'cancoes',
-    (SELECT COUNT(*) FROM SpotifyClone.artists) AS 'artistas',
-    (SELECT COUNT(*) FROM SpotifyClone.albums) AS 'albums'
-	FROM SpotifyClone.songs;
+SELECT COUNT(*) AS 'cancoes',
+(SELECT COUNT(*) FROM SpotifyClone.artists) AS 'artistas',
+(SELECT COUNT(*) FROM SpotifyClone.albums) AS 'albums'
+FROM SpotifyClone.songs;
