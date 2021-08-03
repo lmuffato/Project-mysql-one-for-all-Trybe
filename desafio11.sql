320cfa0376ad8540ae5e2b193d4db59e77191743
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-08-03 18:10:38 -0300

    requisito 11 alterado

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 CREATE VIEW cancoes_premium AS
-SELECT songs.song_name AS 'nome', COUNT(rep_history.song_id) AS 'reproducoes'
+SELECT songs.song_name AS 'nome', COUNT(rep_history.song_id_fk) AS 'reproducoes'
 FROM SpotifyClone.rep_history AS rep_history
 JOIN SpotifyClone.songs AS songs ON rep_history.song_id_fk = songs.song_id
 JOIN SpotifyClone.users AS users ON rep_history.user_id_fk = users.user_id
