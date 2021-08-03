fedafc902cec2ba37fddc4d4bb57de01a97f74dd
samuelmmjr <samuelmmjunior@gmail.com>
2021-08-03 15:33:13 -0300

    iniciando o projeto MySQL One For All com tudo

---
+++ desafio1.sql
@@ -0,0 +1,109 @@
+CREATE DATABASE SpotifyClone;
+
+CREATE TABLE plan(
+plan_id INT PRIMARY KEY AUTO_INCREMENT,
+plan VARCHAR(30) NOT NULL,
+price DECIMAL(6, 2) NOT NULL
+);
+
+CREATE TABLE user(
+user_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+age INT NOT NULL,
+plan_id INT,
+FOREIGN KEY (plan_id) REFERENCES plan (plan_id)
+);
+
+
+CREATE TABLE artist(
+artist_id INT PRIMARY KEY AUTO_INCREMENT,
+artist VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE album(
+album_id INT PRIMARY KEY AUTO_INCREMENT,
+album VARCHAR(30) NOT NULL,
+artist_id INT,
+FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
+);
+
+CREATE TABLE song(
+song_id INT PRIMARY KEY AUTO_INCREMENT,
+song VARCHAR(30) NOT NULL,
+album_id INT,
+FOREIGN KEY (album_id) REFERENCES album (album_id)
+);
+
+CREATE TABLE track_record(
+user_id INT,
+song_id INT,
+CONSTRAINT PRIMARY KEY (user_id, song_id),
+FOREIGN KEY (user_id) REFERENCES user (user_id),
+FOREIGN KEY (song_id) REFERENCES song (song_id)
+);
+
+CREATE TABLE following(
+user_id INT,
+artist_id INT,
+CONSTRAINT PRIMARY KEY (user_id, artist_id),
+FOREIGN KEY (user_id) REFERENCES user (user_id),
+FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
+);
+
+INSERT INTO plan (plan, price)
+VALUES
+('gratuito', 0.00),
+('universitário', 5.99),
+('familiar', 7.99);
+
+INSERT INTO artist (artist)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+INSERT INTO user (name, age, plan_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 3),
+('Bill', 20, 2),
+('Roger', 45, 1);
+
+INSERT INTO album (album, artist_id)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO song (song, album_id)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO track_record (user_id, song_id)
+VALUES
+(1, 1), (1, 6), (1, 14), (1, 16), (2, 13), (2, 17), (2, 2), (2, 15),
+(3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
+
+INSERT INTO following (user_id, artist_id)
+VALUES
+(1, 1), (1, 4), (1, 3), (2, 1), (2, 3), (3, 2), (3, 1), (4, 4);
