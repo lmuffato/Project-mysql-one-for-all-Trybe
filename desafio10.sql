da4be2517b9784c355dea502a0ce9dd9cc80ecb5
Lucas André <lucas.cancado89@gmail.com>
2021-08-03 19:26:00 -0300

    desafio10-change param user to user_id

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(chosenUser VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(chosenUser INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE music_total INT;
@@ -8,7 +8,7 @@ BEGIN
     FROM SpotifyClone.usuario_reproducao AS ur
     LEFT JOIN SpotifyClone.usuarios AS u
     ON u.usuario_id = ur.usuario_id
-    WHERE u.usuario = chosenUser INTO music_total;
+    WHERE u.usuario_id = chosenUser INTO music_total;
     RETURN music_total;
 END $$
 
