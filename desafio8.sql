ed3a70d4362efa2187cf716eb48ab3367ed6c7b2
gabrieldeori <deori.pessoal@gmail.com>
2021-08-03 17:26:21 -0300

    CC: Removing tabs from DELETES lines 8 and 9

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users
 FOR EACH ROW
 BEGIN
-	DELETE FROM historic WHERE user_id = OLD.user_id;
-    DELETE FROM followers WHERE user_id = OLD.user_id;
+DELETE FROM historic WHERE user_id = OLD.user_id;
+DELETE FROM followers WHERE user_id = OLD.user_id;
 END $$
 DELIMITER ;
