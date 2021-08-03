f9d0f38d0ac582e8eeab7bc47164699cc450edbd
RenzoSev <sevilharenzo@gmail.com>
2021-08-03 14:37:18 -0300

    feat: desafio 8.sql

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+  BEFORE DELETE ON SpotifyClone.usuario
+  FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.historico_reproducao
+  WHERE OLD.usuario_id = usuario_id;
+
+  DELETE FROM SpotifyClone.seguindo_artista
+  WHERE OLD.usuario_id = usuario_id;
+END $$
