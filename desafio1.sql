512899b880a32e2c8a6b5aac7c9e780c10204212
Bruno Mendes <mendessbruno@gmail.com>
2021-08-03 16:44:51 -0300

    tentativa de resolver problemas de lint

--- desafio1.sql
+++ desafio1.sql
@@ -5,57 +5,57 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE plan(
-	plan_id INTEGER,
-    plan_name VARCHAR(40),
-    plan_value DECIMAL(4,2),
-    CONSTRAINT PRIMARY KEY(plan_id)
+plan_id INTEGER,
+plan_name VARCHAR(40),
+plan_value DECIMAL(4,2),
+CONSTRAINT PRIMARY KEY(plan_id)
 ) engine = InnoDB;
 
 CREATE TABLE `user`(
-    user_id INTEGER,
-    username VARCHAR(40),
-    age INTEGER,
-    plan_id INTEGER,
-    CONSTRAINT PRIMARY KEY(user_id),
-    FOREIGN KEY(plan_id) REFERENCES plan(plan_id)
+user_id INTEGER,
+username VARCHAR(40),
+age INTEGER,
+plan_id INTEGER,
+CONSTRAINT PRIMARY KEY(user_id),
+FOREIGN KEY(plan_id) REFERENCES plan(plan_id)
 ) engine = InnoDB;
 
 CREATE TABLE artist(
-	artist_id INTEGER,
-    artist_name VARCHAR(40),
-    CONSTRAINT PRIMARY KEY(artist_id)
+artist_id INTEGER,
+artist_name VARCHAR(40),
+CONSTRAINT PRIMARY KEY(artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE album(
-	album_id INTEGER,
-    album_name VARCHAR(40),
-    artist_id INTEGER,
-    CONSTRAINT PRIMARY KEY(album_id),
-    FOREIGN KEY(artist_id) REFERENCES artist(artist_id)
+album_id INTEGER,
+album_name VARCHAR(40),
+artist_id INTEGER,
+CONSTRAINT PRIMARY KEY(album_id),
+FOREIGN KEY(artist_id) REFERENCES artist(artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE songs(
-	song_id INTEGER,
-    song_name VARCHAR(40),
-    album_id INTEGER,
-    CONSTRAINT PRIMARY KEY(song_id),
-    FOREIGN KEY(album_id) REFERENCES album(album_id)
+song_id INTEGER,
+song_name VARCHAR(40),
+album_id INTEGER,
+CONSTRAINT PRIMARY KEY(song_id),
+FOREIGN KEY(album_id) REFERENCES album(album_id)
 ) engine = InnoDB;
 
 CREATE TABLE play_history(
-	user_id INTEGER,
-    song_id INTEGER,
-    CONSTRAINT PRIMARY KEY(user_id, song_id),
-    FOREIGN KEY(user_id) REFERENCES `user`(user_id),
-    FOREIGN KEY(song_id) REFERENCES songs(song_id)
+user_id INTEGER,
+song_id INTEGER,
+CONSTRAINT PRIMARY KEY(user_id, song_id),
+FOREIGN KEY(user_id) REFERENCES `user`(user_id),
+FOREIGN KEY(song_id) REFERENCES songs(song_id)
 ) engine = InnoDB;
 
 CREATE TABLE user_following(
-	user_id INTEGER,
-    artist_id INTEGER,
-    CONSTRAINT PRIMARY KEY(user_id, artist_id),
-    FOREIGN KEY(user_id) REFERENCES `user`(user_id),
-    FOREIGN KEY(artist_id) REFERENCES artist(artist_id)
+user_id INTEGER,
+artist_id INTEGER,
+CONSTRAINT PRIMARY KEY(user_id, artist_id),
+FOREIGN KEY(user_id) REFERENCES `user`(user_id),
+FOREIGN KEY(artist_id) REFERENCES artist(artist_id)
 ) engine = InnoDB;
 
 INSERT INTO plan (plan_id, plan_name, plan_value)
@@ -134,4 +134,3 @@ VALUES
   (3, 2),
   (3, 1),
   (4, 4);
-  
