81893944bc70a63cd46ab7c4ca1f3a3d09b3837a
Perycles reis <perycles_reis@hotmail.com>
2021-08-03 14:55:41 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+-- DESAFIO 3 Crie uma VIEW chamada historico_reproducao_usuarios. Essa VIEW deverá ter apenas duas colunas:
+
+CREATE VIEW historico_reproducao_usuarios (`usuario`, `nome`) AS
+  SELECT
+    U.usuario,
+    C.cancao_nome
+  FROM SpotifyClone.USUARIO U
+  INNER JOIN SpotifyClone.REPRODUCAO R
+  ON U.usuario_id = R.usuario_id
+  INNER JOIN SpotifyClone.CANCAO C
+  ON C.cancao_id = R.cancao_id
+  ORDER BY U.usuario, C.cancao_nome;
