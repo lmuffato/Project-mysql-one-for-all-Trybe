7a8bde1ef9806c66b128908fe7ea5b2e873cc220
maricegalla <maricegalla@gmail.com>
2021-08-03 14:01:11 -0300

    Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.cancao AS 'cancao',
+count(h.usuario_id) AS 'reproducoes'
+FROM SpotifyClone.Historico AS h
+INNER JOIN SpotifyClone.Cancoes AS c
+ON h.cancao_id = c.cancao_id
+GROUP BY c.cancao
+ORDER BY count(h.usuario_id) DESC
+LIMIT 2;
