7b8ca8ea7e57056055e9090b8919347575f1066a
Giovanni <giovannitadeu08@gmail.com>
2021-08-03 21:03:22 -0300

    feat: desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,6 @@
+CREATE VIEW faturamento_atual AS
+SELECT MIN(p.valor_plano) AS faturamento_minimo,
+MAX(p.valor_plano) AS faturamento_maximo,
+ROUND(AVG(p.valor_plano), 2) AS faturamento_medio,
+SUM(p.valor_plano) AS faturamento_total FROM SpotifyClone.planos AS p
+JOIN usuario AS u ON u.plano_id = p.plano_id;
