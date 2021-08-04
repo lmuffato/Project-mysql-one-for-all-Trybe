d4b8407b8507ce75ee63e766de16e34ec67840aa
Flavio Franco <flavio.win@gmail.com>
2021-08-03 21:19:50 -0300

    requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,13 +1,13 @@
-CREATE VIEW perfil_artistas AS
-  SELECT
-    artista_name AS 'artista',
-    album_name AS 'album',
-    COUNT(s.artista_id) AS 'seguidores'
-  FROM
-    SpotifyClone.artista a
-  INNER JOIN
-    SpotifyClone.album ab ON a.artista_id = ab.artista_id
-  INNER JOIN
-    SpotifyClone.seguindo s ON ab.artista_id = s.artista_id
-  GROUP BY 2
-  ORDER BY 3 DESC, 1, 2;
+  CREATE VIEW perfil_artistas AS
+    SELECT
+      a.artista_name AS 'artista',
+      ab.album_name AS 'album',
+      COUNT(s.artista_id) AS 'seguidores'
+    FROM
+      SpotifyClone.artista a
+    INNER JOIN
+      SpotifyClone.album ab ON a.artista_id = ab.artista_id
+    INNER JOIN
+      SpotifyClone.seguindo s ON ab.artista_id = s.artista_id
+    GROUP BY 2
+    ORDER BY 3 DESC, 1, 2;
