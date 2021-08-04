cc3b0bf2ab7798b602e29e3adbaf9eeb75c33892
hheloisa <hhackenhaar@gmail.com>
2021-08-03 21:28:06 -0300

    Requisito 4 - Exibe top 3 artistas com maior quantidade de pessoas seguidoras

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+a.artist_name AS artista,
+COUNT(DISTINCT fa.user_id) AS seguidores
+FROM SpotifyClone.ARTISTS AS a
+INNER JOIN SpotifyClone.FOLLOWING_ARTISTS AS fa
+ON a.artist_id = fa.artist_id
+GROUP BY fa.artist_id
+ORDER BY 2 DESC, 1
+LIMIT 3;
