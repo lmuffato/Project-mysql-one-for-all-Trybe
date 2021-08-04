58a41027bb27574f8b9a894742aa8540f436d70d
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-08-04 14:59:12 -0300

    fix: challenge 3

--- desafio3.sql
+++ desafio3.sql
@@ -6,4 +6,5 @@ CREATE VIEW historico_reproducao_usuarios AS
   INNER JOIN SpotifyClone.reproductions_history AS rh
   ON u.users_id = rh.users_id
   INNER JOIN SpotifyClone.songs AS s
-  ON rh.songs_id = s.songs_id;
+  ON rh.songs_id = s.songs_id
+  ORDER BY `usuario` ASC, `nome` ASC;
