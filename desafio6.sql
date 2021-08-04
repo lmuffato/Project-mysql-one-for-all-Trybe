3aff7d221ee34b0c4ee07eb2808b90ff980c0b4b
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-04 12:43:35 -0300

    Desafio 6 OK

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+CREATE VIEW faturamento_atual AS
+  SELECT
+    ROUND(MIN(p.price), 2) AS 'faturamento_minimo',
+    ROUND(MAX(p.price), 2) AS 'faturamento_máximo',
+    ROUND(AVG(p.price), 2) AS 'faturamento_medio',
+    ROUND(SUM(p.price), 2) AS 'faturamento_total'
+  FROM plan AS p
+  INNER JOIN user AS u
+  ON u.id_plan = p.id_plan;
