fa337eeb2bda7bd2c80cbcce5307e3eb7dfa6ffb
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-04 14:48:48 -0300

    Desafio 9 OK - 8 em andamento

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+-- USE SpotifyClone;
+
+-- DELIMITER $$
+-- CREATE TRIGGER trigger_usuario_delete
+-- 	AFTER DELETE ON user
+--     FOR EACH ROW
+-- BEGIN
+-- 	
+-- END $$
+
+-- DELIMITER ;
---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(artistParam VARCHAR(50))
+  BEGIN
+	SELECT 
+    art.name_artist, alb.name_album
+    FROM artist AS art
+    INNER JOIN album as alb
+	ON art.id_artist = alb.id_artist
+	WHERE art.name_artist = artistParam;
+  END $$
+
+DELIMITER ;
