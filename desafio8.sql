ce71fd772bc72088774702cc52bf390c9e01db91
Maria Baeta <m.martinsdg@gmail.com>
2021-08-04 10:45:04 -0300

    Requisito 8 completo

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+-- DROP TRIGGER trigger_usuario_delete;
+
+DELIMITER $$ 
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+ DELETE FROM SpotifyClone.following_artists  WHERE OLD.user_id = user_id;
+DELETE FROM SpotifyClone.reproduction_history 
+WHERE
+    OLD.user_id = user_id;
+END $$
+DELIMITER ;
+
+-- DELETE FROM SpotifyClone.users WHERE user_id = 1;
