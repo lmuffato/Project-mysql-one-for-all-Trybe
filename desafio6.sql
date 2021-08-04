f22296bf0969363c6baef1093e056ddb8a4c6e3a
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-04 13:04:31 -0300

    fix: bug que impedia req 6 de passar - correcao de datatype

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT ROUND(MIN(P.price), 2) AS `faturamento_minimo`,
-ROUND(MAX(P.price), 2) AS `faturamento_maximo`,
+MAX(P.price) AS `faturamento_maximo`,
 ROUND(AVG(P.price), 2) AS `faturamento_medio`,
-ROUND(SUM(P.price), 2) AS `faturamento_total`
+SUM(P.price) AS `faturamento_total`
 FROM SpotifyClone.users AS U
 INNER JOIN SpotifyClone.plans AS P
 ON U.plan_id=P.plan_id;
