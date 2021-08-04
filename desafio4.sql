002e7b113800e01c81da163636d6f6ca6dbd21b2
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-03 21:04:16 -0300

    feat: desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,11 @@
 CREATE VIEW top_3_artistas AS (
-  SELECT CONCAT(a.FIRST_NAME, a.LAST_NAME) AS `artista`
-)
+  SELECT
+  CONCAT(a.FIRST_NAME, ' ', a.LAST_NAME) AS `artista`,
+  COUNT(*) AS `seguidores`
+  FROM SpotifyClone.followed_artist AS fa
+  INNER JOIN SpotifyClone.artist AS a
+  ON a.ARTIST_ID = fa.ARTIST_ID
+  GROUP BY fa.ARTIST_ID
+  ORDER BY 2 DESC, 1
+  LIMIT 3
+);
