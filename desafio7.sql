35849b84850946a368205d482825f2b2d7ccc3d0
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-04 12:48:39 -0300

    Fix: Desafio7.sql

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 CREATE VIEW SpotifyClone.perfil_artistas AS
-SELECT Artista AS 'artista', al.Album as 'album', COUNT(s.Usuario_id) AS 'seguidores'
+SELECT Artista AS artista, al.Album as album, COUNT(s.Usuario_id) AS seguidores
 FROM SpotifyClone.Album AS al
 INNER JOIN SpotifyClone.Artista AS ar ON ar.Artista_id = al.Artista_id
 INNER JOIN SpotifyClone.Seguindo AS s ON s.Artista_id = ar.Artista_id
