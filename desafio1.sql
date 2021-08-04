40fb7e42c57050fe63db92ea51a5150bb63d65ad
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-03 23:15:32 -0300

    Tudo pronto

---
+++ desafio1.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE tabela1(
+    coluna1 tipo restricoes,
+    coluna2 tipo restricoes,
+    colunaN tipo restricoes,
+) engine = InnoDB;
+
+CREATE TABLE tabela2(
+    coluna1 tipo restricoes,
+    coluna2 tipo restricoes,
+    colunaN tipo restricoes,
+) engine = InnoDB;
+
+INSERT INTO tabela1 (coluna1, coluna2)
+VALUES
+  ('exemplo de dados 1', 'exemplo de dados A'),
+  ('exemplo de dados 2', 'exemplo de dados B'),
+  ('exemplo de dados 3', 'exemplo de dados C');
+
+INSERT INTO tabela2 (coluna1, coluna2)
+VALUES
+  ('exemplo de dados 1', 'exemplo de dados X'),
+  ('exemplo de dados 2', 'exemplo de dados Y');
