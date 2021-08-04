ebb0d04d578b0bcdb117a864923a39ebb64ccae8
Marcus C. H. Filho <mchusemann@gmail.com>
2021-08-04 20:39:35 -0300

    fix(challenge6): adds format

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-ROUND(MIN(p.PlanValue), 2) AS 'faturamento_minimo',
-ROUND(MAX(p.PlanValue), 2) AS 'faturamento_maximo',
-ROUND(AVG(p.PlanValue), 2) AS 'faturamento_medio',
-ROUND(SUM(p.PlanValue), 2) AS 'faturamento_total'
+FORMAT(ROUND((MIN(p.PlanValue)), 2 ), 2) AS 'faturamento_minimo',
+FORMAT(ROUND(MAX(p.PlanValue), 2), 2) AS 'faturamento_maximo',
+FORMAT(ROUND(AVG(p.PlanValue), 2), 2) AS 'faturamento_medio',
+FORMAT(ROUND(SUM(p.PlanValue), 2), 2) AS 'faturamento_total'
 FROM SpotifyClone.Plans p
 INNER JOIN SpotifyClone.Users u
 ON u.PlanID = p.PlanID;
