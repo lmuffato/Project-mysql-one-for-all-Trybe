ba0732d202a47d5d6f593806bd0ea97eab3956fc
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-08-03 23:05:14 -0300

    Challenge 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT m.nome AS nome, COUNT(hr.Musica_ID)  AS reproducoes
+FROM SpotifyClone.Musicas AS m
+INNER JOIN SpotifyClone.historico_reproducao AS hr
+ON m.Musica_ID = hr.Musica_ID
+INNER JOIN SpotifyClone.Usuarios AS u
+ON u.Usuario_ID = hr.Usuario_ID
+INNER JOIN SpotifyClone.Planos AS p
+ON p.Plano_ID = u.Plano_ID
+WHERE p.Plano IN('familiar', 'universitário')
+GROUP BY nome 
+ORDER BY nome
