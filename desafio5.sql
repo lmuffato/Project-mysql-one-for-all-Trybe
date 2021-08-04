e5ca8bda5e3a6c8458d30404afa499c096cc6695
hheloisa <hhackenhaar@gmail.com>
2021-08-03 21:33:53 -0300

    Requisito 5 - Exibe top 2 hits mais tocados no momento

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT
+s.song_name AS cancao,
+COUNT(hr.song_id) AS reproducoes
+FROM SpotifyClone.SONGS AS s
+INNER JOIN SpotifyClone.HISTORY_REPRODUCTIONS AS hr
+ON s.song_id = hr.song_id
+GROUP BY hr.song_id
+ORDER BY 2 DESC, 1
+LIMIT 2;
