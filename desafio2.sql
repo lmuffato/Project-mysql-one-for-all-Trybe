a791d647246019acc8c96806c7e677ae5e91a227
w-cypriano <profwellington17@gmail.com>
2021-08-03 23:42:26 -0300

    Desafio02

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT COUNT(cancao) AS cancoes, 
+    (SELECT COUNT(artista) FROM SpotifyClone.artistas)  AS artistas,
+    (SELECT COUNT(album) FROM SpotifyClone.albuns) AS albuns
+    FROM SpotifyClone.cancoes;
+    
