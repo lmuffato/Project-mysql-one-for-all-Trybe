01263386d8e432e3beb1fe3b42cc87f276b1bc1d
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-04 11:01:21 -0300

    Feat:Desafio7.sql (Created view taht shows artist with album and followers

---
+++ desafio7.sql
@@ -0,0 +1,6 @@
+CREATE VIEW perfil_artista AS
+SELECT Artista AS 'artista', al.Album as 'album', COUNT(s.Usuario_id) AS 'seguidores'
+FROM SpotifyClone.Album AS al
+INNER JOIN SpotifyClone.Artista AS ar ON ar.Artista_id = al.Artista_id
+INNER JOIN SpotifyClone.Seguindo AS s ON s.Artista_id = ar.Artista_id
+GROUP BY Album, Artista ORDER BY seguidores DESC, artista;
