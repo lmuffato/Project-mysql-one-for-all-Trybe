5ec1dfdb40c5b4e93fa4a22018577f7f7c9678a4
Luiz <luizfvaccarif@gmail.com>
2021-08-04 14:50:38 -0300

    fix: req2

--- desafio2.sql
+++ desafio2.sql
@@ -3,4 +3,3 @@ SELECT COUNT(song_id) AS `cancoes`,
 (SELECT COUNT(artist_id) FROM SpotifyClone.artists) AS `artistas`,
 (SELECT COUNT(album_id) FROM SpotifyClone.albums) AS `albuns`
 FROM SpotifyClone.songs;
-SELECT * FROM SpotifyClone.estatisticas_musicais;
