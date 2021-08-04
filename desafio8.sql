89dd9aa6c1b9f3c5418ef7ae0de09caed36fe277
Maria Baeta <m.martinsdg@gmail.com>
2021-08-04 10:47:32 -0300

    Adequação Code Climate

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
- DELETE FROM SpotifyClone.following_artists  WHERE OLD.user_id = user_id;
+DELETE FROM SpotifyClone.following_artists  WHERE OLD.user_id = user_id;
 DELETE FROM SpotifyClone.reproduction_history 
 WHERE
     OLD.user_id = user_id;
