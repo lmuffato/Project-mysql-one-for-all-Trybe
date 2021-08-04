67dd08f77843767d63db0f8043b896728a98bea3
Viegas <mvrdgs@gmail.com>
2021-08-03 22:29:30 -0300

    requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(search_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_count INT;
+SELECT COUNT(*) AS quantidade_musicas_no_historico FROM users_history WHERE user_id = search_id;
+RETURN total_count;
+END $$
+
+DELIMITER ;
--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT
 (SELECT FORMAT(MIN(plan_price), 2) FROM plans) AS faturamento_minimo,
-(SELECT MAX(plan_price) FROM plans) AS faturamento_maximo,
+(SELECT FORMAT(MAX(plan_price), 2) FROM plans) AS faturamento_maximo,
 FORMAT((SELECT CEIL(SUM(plan_price) / (SELECT COUNT(*) FROM users) * 100) / 100 FROM plans), 2) AS faturamento_medio,
-ROUND((SELECT SUM(plan_price) FROM plans), 2) AS faturamento_total
+FORMAT((SELECT SUM(plan_price) FROM plans), 2) AS faturamento_total
 FROM user_status AS us
 GROUP BY faturamento_total;
