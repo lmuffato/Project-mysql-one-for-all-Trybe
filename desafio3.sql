48a1026a3839d7b9d621f7187c24a45a79dd2914
phlopes <phlopes93@gmail.com>
2021-08-03 23:31:38 -0300

    challenge 3 completed

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+CREATE VIEW historico_reproducao_usuarios AS
+	SELECT 
+    u.usuario AS usuario,
+	c.cancao AS nome
+	FROM SpotifyClone.usuarios AS u
+	INNER JOIN SpotifyClone.historico_reproducoes AS hr
+	ON u.usuario_id = hr.usuario_id
+	INNER JOIN SpotifyClone.cancoes AS c
+	ON hr.cancao_id = c.cancao_id
+	ORDER BY usuario, nome;
+    
