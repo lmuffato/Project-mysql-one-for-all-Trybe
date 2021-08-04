1a1f716311c5bbacb1b0ecbd30d43cae4d830dae
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-08-03 23:09:49 -0300

    Challenge 11 fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,4 @@
+CREATE VIEW cancoes_premium AS 
 SELECT m.nome AS nome, COUNT(hr.Musica_ID)  AS reproducoes
 FROM SpotifyClone.Musicas AS m
 INNER JOIN SpotifyClone.historico_reproducao AS hr
@@ -8,4 +9,4 @@ INNER JOIN SpotifyClone.Planos AS p
 ON p.Plano_ID = u.Plano_ID
 WHERE p.Plano IN('familiar', 'universitário')
 GROUP BY nome 
-ORDER BY nome
+ORDER BY nome;
