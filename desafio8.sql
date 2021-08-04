4ee2cb1f15cf38e617df90c145f27f78aaccaee4
Giovanni <giovannitadeu08@gmail.com>
2021-08-04 12:48:05 -0300

    feat: desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE TRIGGER SpotifyClone.trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.seguindo_artistas WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_reproducao WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
