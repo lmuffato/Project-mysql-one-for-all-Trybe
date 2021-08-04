9ca412dc7a946927bced361ae78e710b33dba33b
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-08-04 19:41:39 -0300

    feat: challenge 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+CREATE VIEW cancoes_premium AS
+  SELECT
+    s.songs_name AS `nome`,
+    COUNT(s.songs_name) AS `repoducoes`
+  FROM SpotifyClone.users AS u
+  INNER JOIN SpotifyClone.plans AS p
+  ON u.plans_id = p.plans_id
+  INNER JOIN SpotifyClone.reproductions_history AS r
+  ON u.users_id = r.users_id
+  INNER JOIN SpotifyClone.songs AS s
+  ON s.songs_id = r.songs_id
+  WHERE p.plans_name = 'familliar' OR p.plans_name = 'universitário'
+  GROUP BY s.songs_name
+  ORDER BY `nome` ASC;
