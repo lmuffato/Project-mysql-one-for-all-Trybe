f1593ad3ba40d105ed7b106c3baa32c066fc075f
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-08-03 22:45:41 -0300

    Challenge 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+CREATE VIEW perfil_artistas AS
+SELECT a.nome AS artista, al.nome AS album, COUNT(ars.Usuario_ID) as Seguidores
+ FROM SpotifyClone.Artistas AS a
+ INNER JOIN  SpotifyClone.Albuns AS al
+ ON a.Artista_ID = al.Artista_ID
+ INNER JOIN SpotifyClone.Artistas_seguidos AS ars
+ ON a.Artista_ID = ars.Artista_ID
+ GROUP BY album
+ ORDER BY Seguidores DESC, artista, album;
+ 
