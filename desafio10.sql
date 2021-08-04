d04fecc1680f7a8dec6ef273e741f5e33b7746fc
lucas-paz <lucas.hpaz@outlook.com>
2021-08-04 11:25:33 -0300

    fixes req 10

--- desafio10.sql
+++ desafio10.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(userName VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE songsCount INT;
+    DECLARE likesCount INT;
     SELECT COUNT(*)
     FROM user_history AS UH
     INNER JOIN users AS U ON UH.user_id = U.id
-    WHERE U.user_name = userName INTO songsCount;
-    RETURN songsCount;
+    WHERE U.user_name = userName INTO likesCount;
+    RETURN likesCount;
 END $$
 
 DELIMITER ;
