54f6dff89979f0198fb7d3362a383d3ad16f6381
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-04 15:04:57 -0300

    Desafio 8 OK

--- desafio8.sql
+++ desafio8.sql
@@ -2,11 +2,11 @@ USE SpotifyClone;
 
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-	AFTER DELETE ON user
-    FOR EACH ROW
+  AFTER DELETE ON user
+  FOR EACH ROW
 BEGIN
-	DELETE FROM artist WHERE id.artist = OLD.id_user;
-    DELETE FROM history WHERE id.artist = OLD.id_user;
+  DELETE FROM artist WHERE id.artist = OLD.id_user;
+  DELETE FROM history WHERE id.artist = OLD.id_user;
 END $$
 
 DELIMITER ;
