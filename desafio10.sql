2a46b5fc362d8ead36275154e993ecd573834017
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-08-03 23:34:44 -0300

    Challenge10

--- desafio10.sql
+++ desafio10.sql
@@ -4,8 +4,8 @@ CREATE FUNCTION quantidade_musicas_no_historico(Usuario_ID INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE quantity INT;
-SELECT  COUNT(hr.Musica_ID) FROM spotifyclone.usuarios AS u
-INNER JOIN spotifyclone.historico_reproducao AS hr
+SELECT  COUNT(hr.Musica_ID) FROM SpotifyClone.Usuarios AS u
+INNER JOIN SpotifyClone.historico_reproducao AS hr
 ON hr.Usuario_ID = u.Usuario_ID
 WHERE u.Usuario_ID = Usuario_ID
 INTO quantity
