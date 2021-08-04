a191cfa059424e83a02f56d7801251896cc02cff
Maria Baeta <m.martinsdg@gmail.com>
2021-08-03 21:50:47 -0300

    Requiremente 7 complete e correction requirement 6

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,18 @@
-DROP VIEW perfil_artistas;
+-- DROP VIEW perfil_artistas;
 
 CREATE VIEW perfil_artistas AS
-SELECT * FROM SpotifyClone.albums;
+    SELECT 
+        a.artist AS artista,
+        ab.album AS album,
+        COUNT(f.user_id) AS seguidores
+    FROM
+        SpotifyClone.artists AS a
+            INNER JOIN
+        SpotifyClone.following_artists AS f ON a.artist_id = f.artist_id
+            INNER JOIN
+        SpotifyClone.albums AS ab ON a.artist_id = ab.artist_id
+    GROUP BY 1 , 2
+    ORDER BY 3 DESC , 1
+;
 
-
-SELECT * FROM perfil_artistas;
+-- SELECT * FROM perfil_artistas;
