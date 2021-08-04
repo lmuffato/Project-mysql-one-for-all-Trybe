4df1bfca0e3190212859ad0364b3370caf4f9163
Michael <michaelpetterson06.com.br>
2021-08-03 23:06:02 -0300

    codeClimate11

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 CREATE VIEW top_3_artistas AS
-    SELECT a.artist_name AS artista, COUNT(u.user_name) AS seguidores
-    FROM SpotifyClone.artists a INNER JOIN SpotifyClone.followers f
-    ON a.artist_id = f.artist_id
+    SELECT aa.artist_name AS artista, COUNT(u.user_name) AS seguidores
+    FROM SpotifyClone.artists aa INNER JOIN SpotifyClone.followers f
+    ON aa.artist_id = f.artist_id
     INNER JOIN SpotifyClone.users u
 	ON u.user_id = f.user_id
     GROUP BY artista
