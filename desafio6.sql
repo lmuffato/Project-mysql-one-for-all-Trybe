698c2b797cef0b90c7df5696408342678d97f0ea
Luiz <luizfvaccarif@gmail.com>
2021-08-04 16:16:35 -0300

    feat: req6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+CREATE VIEW SpotifyClone.faturamento_atual AS
+SELECT ROUND(MIN(p.price), 2) AS `faturamento_minimo`,
+ROUND(MAX(p.price), 2) AS `faturamento_maximo`,
+ROUND(AVG(p.price), 2) AS `faturamento_medio`,
+ROUND(SUM(p.price), 2) AS `faturamento_total`
+FROM SpotifyClone.plans As p
+INNER JOIN SpotifyClone.users AS u ON u.plan_id = p.plan_id;
