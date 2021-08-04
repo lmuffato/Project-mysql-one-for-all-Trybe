471a131ddeeeb614f38030ad4041c266402dade5
MuriloBatista <mub4si@gmail.com>
2021-08-04 20:58:22 -0300

    Desafio10

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+CREATE VIEW `faturamento_atual` AS
+SELECT 
+MIN(p.plan_price) AS `faturamento_minimo`,
+MAX(p.plan_price) AS `faturamento_maximo`,
+ROUND(AVG(p.plan_price), 2) AS `faturamento_medio`,
+ROUND(SUM(p.plan_price), 2) AS `faturamento_total`
+FROM
+SpotifyClone.users AS u
+INNER JOIN
+SpotifyClone.plans AS p ON p.plan_id = u.plan_id;
