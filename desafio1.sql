3e858f8e51d47c3f5c34d6b258b854a34b41e24d
Michael <michaelpetterson06.com.br>
2021-08-04 01:02:05 -0300

    test2

--- desafio1.sql
+++ desafio1.sql
@@ -6,20 +6,20 @@ USE SpotifyClone;
 
 CREATE TABLE plans(
     plan_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	plan_name VARCHAR(50) NOT NULL,
+plan_name VARCHAR(50) NOT NULL,
     price DOUBLE NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE artists(
     artist_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	artist_name VARCHAR(100) NOT NULL
+artist_name VARCHAR(100) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE users(
     user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	user_name VARCHAR(100) NOT NULL,
+user_name VARCHAR(100) NOT NULL,
     age INT NOT NULL,
-	plan_id INT NOT NULL,
+plan_id INT NOT NULL,
     FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
 ) engine = InnoDB;
 
@@ -27,21 +27,21 @@ CREATE TABLE followers(
     user_id INT NOT NULL,
     artist_id INT NOT NULL,
     CONSTRAINT PRIMARY KEY(user_id, artist_id),
-	FOREIGN KEY (user_id) REFERENCES users(user_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE albums(
     album_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	album_name VARCHAR(100) NOT NULL,
-	artist_id INT NOT NULL,
+album_name VARCHAR(100) NOT NULL,
+artist_id INT NOT NULL,
     FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 ) engine = InnoDB;
 
 CREATE TABLE songs(
     song_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	song_name VARCHAR(100) NOT NULL,
-	album_id INT NOT NULL,
+song_name VARCHAR(100) NOT NULL,
+album_id INT NOT NULL,
     FOREIGN KEY (album_id) REFERENCES albums(album_id)
 ) engine = InnoDB;
 
@@ -49,7 +49,7 @@ CREATE TABLE reproductions(
     user_id INT NOT NULL,
     song_id INT NOT NULL,
     CONSTRAINT PRIMARY KEY(user_id, song_id),
-	FOREIGN KEY (user_id) REFERENCES users(user_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (song_id) REFERENCES songs(song_id)
 ) engine = InnoDB;
 
--- desafio4.sql
+++ desafio4.sql
@@ -3,7 +3,7 @@ CREATE VIEW top_3_artistas AS
     FROM SpotifyClone.artists a INNER JOIN SpotifyClone.followers f
     ON a.artist_id = f.artist_id
 INNER JOIN SpotifyClone.users u
-	ON u.user_id = f.user_id
+ON u.user_id = f.user_id
     GROUP BY 1
     ORDER BY 2 DESC, 1
 LIMIT 3;
