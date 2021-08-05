0174f20eb2a08971309d01d7574ba1ddcd78ccaa
Hernanne <hernannedomiciano@gmail.com>
2021-08-05 14:41:02 -0300

    fix: req03

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,8 @@
 CREATE VIEW historico_reproducao_usuarios AS 
-SELECT u.nome AS usuario,
-c.musicas AS nome
+SELECT u.nome AS usuario, c.musicas AS nome
 FROM SpotifyClone.historico_reproducao AS h
-JOIN SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.usuario AS u
 ON h.usuario_id = u.usuario_id
-JOIN SpotifyClone.cancoes as c 
+INNER JOIN SpotifyClone.cancoes as c 
 ON h.musicas_id = c.musicas_id
 ORDER BY `usuario`, `nome`;
