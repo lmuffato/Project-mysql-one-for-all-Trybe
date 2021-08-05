a5a35acbddcbadf9a7d11459b5175720c584545f
phlopes <phlopes93@gmail.com>
2021-08-05 18:48:12 -0300

    challenge 4 completed

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT a.artista AS artista,
+COUNT(sa.artista_id) AS seguidores
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.seguindo_artista AS sa
+ON a.artista_id = sa.artista_id
+GROUP BY a.artista
+ORDER BY seguidores DESC, artista
+LIMIT 3;
