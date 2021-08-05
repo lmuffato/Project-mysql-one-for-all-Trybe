a70b606307a0cf0bde815254544b0b44e4cba511
phlopes <phlopes93@gmail.com>
2021-08-05 20:50:47 -0300

    fix: challenge 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 CREATE VIEW cancoes_premium AS
-SELECT c.cancao AS cancao,
+SELECT c.cancao AS nome,
 COUNT(hr.cancao_id) AS reproducoes
 FROM SpotifyClone.cancoes AS c
 INNER JOIN SpotifyClone.historico_reproducoes AS hr
