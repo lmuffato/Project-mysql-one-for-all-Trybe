fcd94d7a0f67d36a1ced87bf508afdb7f998a631
Felipe Muller <felipeavmuller@gmail.com>
2021-08-05 20:33:59 -0300

    FIX requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,8 +6,8 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM historico_reproduces WHERE usuarios.usuario_id = historico_reproduces.usuario_id;
-DELETE FROM seguindo_artistas WHERE usuarios.usuario_id = seguindo_artistas.usuario_id;
+DELETE FROM historico_reproduces WHERE OLD.usuario_id = historico_reproduces.usuario_id;
+DELETE FROM seguindo_artistas WHERE OLD.usuario_id = seguindo_artistas.usuario_id;
 END $$
 
 DELIMITER ;
