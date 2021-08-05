5eb351aaebb71f06384dc70324a1387010819c39
Hernanne <hernannedomiciano@gmail.com>
2021-08-05 15:44:57 -0300

    fix: climate

--- desafio3.sql
+++ desafio3.sql
@@ -2,7 +2,6 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.nome AS usuario, c.musicas AS nome
 FROM SpotifyClone.historico_reproducao AS h
 JOIN SpotifyClone.usuario AS u
-ON h.usuario_id = u.usuario_id
 JOIN SpotifyClone.cancoes AS c
-ON h.musicas_id = c.musicas_id
-ORDER BY `usuario` ASC, `nome`;
+ON h.usuario_id = u.usuario_id AND h.musicas_id = c.musicas_id
+ORDER BY `usuario`, `nome`;
