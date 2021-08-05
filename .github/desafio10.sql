3b0ea5ccf564ff1ad02f9b528d43718927f808c6
phlopes <phlopes93@gmail.com>
2021-08-05 20:19:49 -0300

    challenge 10 completed

---
+++ .github/desafio10.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(idUser INT)
+RETURNS INT READS SQL DATA 
+BEGIN
+SELECT COUNT(hr.usuario_id) AS historico
+FROM SpotifyClone.historico_reproducoes AS hr
+INNER JOIN SpotifyClone.usuarios AS u
+ON hr.usuario_id = u.usuario_id
+WHERE u.usuario_id = idUser;
+END $$
+DELIMITER ;
