860f313929678fa6fc1b8508cb8f376b22818cf7
Felipe Muller <felipeavmuller@gmail.com>
2021-08-05 20:21:43 -0300

    Requisito 8 - criando trigger com DELETE

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM historico_reproduces WHERE usuarios.usuario_id = historico_reproduces.usuario_id;
+DELETE FROM seguindo_artistas WHERE usuarios.usuario_id = seguindo_artistas.usuario_id;
+END $$
+
+DELIMITER ;
