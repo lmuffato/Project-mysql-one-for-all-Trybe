437574a75c1dcc80ba204ecffd39442a681a27c7
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-05 20:55:39 -0300

    [new] req 7

---
+++ desafio7.sql
@@ -0,0 +1,18 @@
+DROP VIEW IF EXISTS perfil_artistas;
+USE SpotifyClone;
+
+CREATE VIEW perfil_artistas AS
+SELECT
+    ar.name AS artista,
+    al.name AS album,
+    COUNT(f.user_id) AS seguidores
+FROM 
+    SpotifyClone.artist AS ar
+INNER JOIN
+    SpotifyClone.album AS al
+    ON ar.artist_id = al.artist_id
+INNER JOIN
+    SpotifyClone.following AS f
+    ON ar.artist_id = f.artist_id
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista, album;
