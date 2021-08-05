88ffd05c21f5315f1df8d090c8c0c248a542bb82
Hernanne <hernannedomiciano@gmail.com>
2021-08-04 22:09:28 -0300

    feat: req 3,4

--- desafio4.sql
+++ desafio4.sql
@@ -1,1 +1,9 @@
-
+CREATE VIEW top_3_artistas AS
+SELECT a.artista_name AS artista,
+COUNT(sa.usuario_id) AS seguidores
+FROM SpotifyClone.artista AS a
+JOIN SpotifyClone.seguindo_artistas AS sa
+ON a.artista_id = sa.artista_id
+GROUP BY a.artista_id
+ORDER BY `seguidores` DESC, `artista` DESC
+LIMIT 3;
