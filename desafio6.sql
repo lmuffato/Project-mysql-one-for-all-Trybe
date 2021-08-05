4dd400500ae54affe46ba776c951c0a6d33e33ce
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-05 20:46:33 -0300

    [new] req 6

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+DROP VIEW IF EXISTS faturamento_atual;
+USE SpotifyClone;
+
+CREATE VIEW faturamento_atual AS
+SELECT
+    MIN(p.cost) AS faturamento_minimo,
+    MAX(p.cost) AS faturamento_maximo,
+    ROUND(AVG(p.cost), 2) AS faturamento_medio,
+    SUM(p.cost) AS faturamento_total
+FROM 
+    SpotifyClone.plan AS p
+INNER JOIN
+    SpotifyClone.user AS u
+    ON p.plan_id = u.plan_id;
