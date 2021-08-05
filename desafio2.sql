c0365c67731f8a34cb5cfdc1d68f13a7929bb8f6
Ana Ventura <anaventura1811@gmail.com>
2021-08-04 21:23:08 -0300

    feat: desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+CREATE VIEW estatisticas_musicais AS (
+SELECT
+(SELECT COUNT(song_name) from SpotifyClone.songs) AS 'cancões',
+(SELECT COUNT(artist_name) from SpotifyClone.artists) AS 'artistas',
+(SELECT COUNT(album_name) from SpotifyClone.albums) AS 'albuns'
+);
