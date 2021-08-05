c277fd82044b812589b34743f2f82dd5c8b417e3
phlopes <phlopes93@gmail.com>
2021-08-05 20:27:47 -0300

    fix: name column challenge 10

--- .github/desafio10.sql
+++ .github/desafio10.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(idUser INT)
 RETURNS INT READS SQL DATA 
 BEGIN
-SELECT COUNT(hr.usuario_id) AS historico
+SELECT COUNT(hr.usuario_id) AS quantidade_musicas_no_historico
 FROM SpotifyClone.historico_reproducoes AS hr
 INNER JOIN SpotifyClone.usuarios AS u
 ON hr.usuario_id = u.usuario_id
