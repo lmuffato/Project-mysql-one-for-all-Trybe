5e0f757ba8e604ccc3fd19f278088f4c2eb15d94
MuriloBatista <mub4si@gmail.com>
2021-08-04 21:05:32 -0300

    Desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,15 @@
-CREATE VIEW `faturamento_atual` AS
-SELECT 
-MIN(p.plan_price) AS `faturamento_minimo`,
-MAX(p.plan_price) AS `faturamento_maximo`,
-ROUND(AVG(p.plan_price), 2) AS `faturamento_medio`,
-ROUND(SUM(p.plan_price), 2) AS `faturamento_total`
-FROM
-SpotifyClone.users AS u
-INNER JOIN
-SpotifyClone.plans AS p ON p.plan_id = u.plan_id;
+CREATE VIEW `cancoes_premium` AS
+  SELECT 
+      s.song_name AS `nome`, COUNT(h.song_id) AS `reproducoes`
+  FROM
+      SpotifyClone.history AS h
+          INNER JOIN
+      SpotifyClone.songs AS s ON s.song_id = h.song_id
+          INNER JOIN
+      SpotifyClone.users AS u ON u.user_id = h.user_id
+          INNER JOIN
+      SpotifyClone.plans AS p ON p.plan_id = u.plan_id
+  WHERE
+      u.plan_id IN (2 , 3)
+  GROUP BY `nome`
+  ORDER BY `nome`;
