d1d80ca7519a5dfa5b94346d1847b89051fc2a49
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-05 17:27:37 -0300

    feat: desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.seguindo_artistas WHERE OLD.usuario_id = usuario_id;
+  DELETE FROM SpotifyClone.historico_reproducoes WHERE OLD.usuario_id = usuario_id;
+END $$
+
+DELIMITER $$ ;
