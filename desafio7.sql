d9768b579dec67a37f515a211704b7c21d6fad09
w-cypriano <profwellington17@gmail.com>
2021-08-04 21:36:55 -0300

    Desafio07

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT 
+A.artista AS artista,
+AL.album AS album,
+SUB.QS AS seguidores
+FROM
+(SELECT artista_id, COUNT(artista_id) AS QS
+FROM SpotifyClone.seguidores
+GROUP BY artista_id) AS SUB
+INNER JOIN SpotifyClone.albuns AS AL
+ON SUB.artista_id = AL.artista_id
+INNER JOIN SpotifyClone.artistas AS A
+ON SUB.artista_id = A.artista_id
+ORDER BY 3 DESC, 1, 2;
