c4fce228d59e432873094687369ec2e9ba57c053
Hernanne <hernannedomiciano@gmail.com>
2021-08-05 13:48:06 -0300

    fix: req03

--- desafio3.sql
+++ desafio3.sql
@@ -3,7 +3,7 @@ SELECT u.nome AS usuario,
 c.musicas AS nome
 FROM SpotifyClone.historico_reproducao AS h
 JOIN SpotifyClone.usuario AS u
-ON u.usuario_id = h.usuario_id
+ON h.usuario_id = u.usuario_id
 JOIN SpotifyClone.cancoes as c 
-ON c.musicas_id = h.musicas_id
+ON h.musicas_id = c.musicas_id
 ORDER BY `usuario`, `nome`;
