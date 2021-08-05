ed93dd61b06a261211e9e5447985cc67595ef7ae
Ana Ventura <anaventura1811@gmail.com>
2021-08-04 22:10:55 -0300

    feat: desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_3_artistas AS (
+SELECT ar.artist_name  AS 'artista',
+COUNT(fol.user_id) AS 'seguidores'
+FROM SpotifyClone.artists AS ar
+INNER JOIN SpotifyClone.artist_followers AS fol
+INNER JOIN SpotifyClone.users as u
+ON fol.user_id = u.user_id AND ar.artist_id = fol.artist_id
+GROUP BY ar.artist_name
+HAVING COUNT(fol.user_id) > 1
+ORDER BY COUNT(fol.user_id) DESC, ar.artist_name ASC
+);
