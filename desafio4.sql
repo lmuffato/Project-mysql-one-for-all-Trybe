c175ce4e876e489e01075d4ce6e0d65e1ed9ecd0
AndreBarroso <andre_barroso17@hotmail.com>
2021-08-05 15:19:49 -0300

    feat: desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS(
+SELECT a.artista AS 'artista', COUNT(sa.usuario_id) AS 'seguidores'FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.seguindo_artistas AS sa
+INNER JOIN SpotifyClone.usuarios AS u
+ON a.artista_id = sa.artista_id AND sa.usuario_id = u.usuario_id
+GROUP BY a.artista
+ORDER BY `seguidores` DESC, `artista`
+);
