f5a9cc5ce195fb470b70ef5c8d27a8a5c5b1e276
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-05 20:29:43 -0300

    [new] req 3

---
+++ desafio3.sql
@@ -0,0 +1,16 @@
+DROP VIEW IF EXISTS historico_reproducao_usuarios;
+USE SpotifyClone;
+
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+    u.name AS usuario,
+    s.name AS nome
+FROM 
+    SpotifyClone.history AS h
+INNER JOIN
+    SpotifyClone.user AS u
+    ON h.user_id = u.user_id  
+INNER JOIN
+    SpotifyClone.song AS s
+    ON h.song_id = s.song_id   
+ORDER BY usuario, nome;
