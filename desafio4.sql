33835c4f634648fe32727dd99d4142e9002a8d06
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-05 20:35:01 -0300

    [new] req 4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+DROP VIEW IF EXISTS top_3_artistas;
+USE SpotifyClone;
+
+CREATE VIEW top_3_artistas AS
+SELECT
+    a.name AS artista,
+    COUNT(f.user_id) AS seguidores
+FROM 
+    SpotifyClone.artist AS a
+INNER JOIN
+    SpotifyClone.following AS f
+    ON f.artist_id = a.artist_id
+GROUP BY artista 
+ORDER BY seguidores DESC, artista
+LIMIT 3;
