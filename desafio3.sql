886f5b107e789f183face900fa43739d9d0ccad2
LucasMLara <lucasml@live.com>
2021-08-05 15:15:58 -0300

    Challenge 3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+CREATE VIEW historico_reproducao_usuarios AS
+  SELECT 
+    U.usuario usuario,
+    C.cancao nome
+  FROM 
+    SpotifyClone.USUARIOS U
+      INNER JOIN
+    SpotifyClone.USUARIOS_REPRODUCOES UR ON U.usuario_id = UR.usuario_id
+      INNER JOIN
+    SpotifyClone.CANCOES C ON C.cancao_id = UR.cancao_id
+  ORDER BY usuario, nome;
+  
