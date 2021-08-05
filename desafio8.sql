54bebced9c64282b8a68d40cc84306219757789c
phlopes <phlopes93@gmail.com>
2021-08-05 19:44:31 -0300

    challenge 8 completed

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+	AFTER DELETE ON SpotifyClone.usuarios
+    FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.seguindo_artista WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_reproducoes WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
