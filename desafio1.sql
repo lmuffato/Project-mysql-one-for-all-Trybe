87d5f947b6dad49b872291113181eb9e2a78fb4c
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-05 18:20:02 -0300

    fix: refatora tabelas e colunas, implementa json, corrige digitaçoes

--- desafio1.sql
+++ desafio1.sql
@@ -37,12 +37,26 @@ CREATE TABLE Songs(
 
 CREATE TABLE History(
   History_id INT PRIMARY KEY AUTO_INCREMENT,
-  User_id INT,
+  user_id INT,
   Song_id INT,
-  Artist_id INT,
   FOREIGN KEY (Song_id) REFERENCES Songs(Song_id),
-  FOREIGN KEY (Artist_id) REFERENCES Artist(Artist_id),
   FOREIGN KEY (User_id) REFERENCES User(User_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE User_plan(
+  plan_id INT,
+  user_id INT,
+  User_plan_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES User(user_id),
+  FOREIGN KEY (plan_id) REFERENCES Plan(plan_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE Artist_followers (
+  Artist_followers_id INT PRIMARY KEY AUTO_INCREMENT  NOT NULL,
+  user_id INT,
+  Artist_id INT,
+  FOREIGN KEY (user_id) REFERENCES User(user_id),
+  FOREIGN KEY (Artist_id) REFERENCES Artist(Artist_id)
 ) ENGINE = InnoDB;
 
 INSERT INTO User (user_name, user_age)
@@ -57,3 +71,74 @@ VALUES
 ('Gratuito', 0),
 ('Universitário', 5.99),
 ('Familiar', 7.99);
+
+INSERT INTO Artist (Artist_name)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+INSERT INTO Album (Album_name, Artist_id)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO Songs (Song_name, ALbum_id)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power',4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO History(User_id, Song_id)
+VALUES
+(1,1),
+(1,6),
+(1,14),
+(1,16),
+(2,13),
+(2,17),
+(2,2),
+(2,15),
+(3,4),
+(3,16),
+(3,6),
+(4,3),
+(4,18),
+(4,11);
+
+INSERT INTO User_plan (user_id, plan_id)
+VALUES
+(1,1),
+(2,2),
+(3,3),
+(4,1);
+
+INSERT INTO Artist_followers (user_id, Artist_id)
+VALUES
+(1,1),
+(1,4),
+(1,3),
+(2,1),
+(2,3),
+(3,2),
+(3,1),
+(4,4);
