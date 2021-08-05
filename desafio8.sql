78da1a0108a80fe7604b008c874f52e06de88286
Felipe Muller <felipeavmuller@gmail.com>
2021-08-05 20:40:09 -0300

    FIX requisito 8 v2

--- desafio8.sql
+++ desafio8.sql
@@ -3,11 +3,11 @@ USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON usuarios
+BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM historico_reproduces WHERE OLD.usuario_id = historico_reproduces.usuario_id;
-DELETE FROM seguindo_artistas WHERE OLD.usuario_id = seguindo_artistas.usuario_id;
+DELETE FROM historico_reproduces WHERE usuarios.usuario_id = historico_reproduces.usuario_id;
+DELETE FROM seguindo_artistas WHERE usuarios.usuario_id = seguindo_artistas.usuario_id;
 END $$
 
 DELIMITER ;
