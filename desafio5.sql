304c81495c9afdf088f9afa0195275ce6ac8fff5
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-05 18:25:07 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_2_hits_do_momento AS
+  SELECT s.name AS cancao,
+  (
+    SELECT COUNT(*)
+    FROM SpotifyClone.reproductions AS r
+    WHERE s.song_id = r.song_id
+  ) AS reproducoes
+  FROM SpotifyClone.songs AS s
+  ORDER BY `reproducoes` DESC, `cancao`
+  LIMIT 2;
+  
