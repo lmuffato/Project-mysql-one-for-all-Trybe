82765dabfdc60bae8195d4237ea43bcb6a1feb96
Edrei <edreimateus@gmail.com>
2021-08-06 13:29:16 -0300

    feat: 8 requiriment

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.`history`
+WHERE user_id = OLD.user_id;
+DELETE FROM SpotifyClone.follow_artist
+WHERE user_id = OLD.user_id;
+END$$
+DELIMITER ;
