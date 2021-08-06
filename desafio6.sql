da0ba1ffa00ee03650dd2dee0eb074278e783883
guilhermemd <guilherme.md@gmail.com>
2021-08-06 17:08:50 -0300

    feat: :sparkles: req 06 implementation

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+CREATE VIEW faturamento_atual AS
+SELECT 
+MIN(valor_plano) AS faturamento_minimo,
+MAX(valor_plano) AS faturamento_maximo,
+ROUND(AVG(valor_plano), 2) AS faturamento_medio,
+SUM(valor_plano) AS faturamento_total
+FROM 
+SpotifyClone.usuarios AS usuarios
+INNER JOIN
+SpotifyClone.planos AS planos
+ON usuarios.plano_id = planos.plano_id;
