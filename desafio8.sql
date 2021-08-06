092c0bd8f7d294e107160229154ff1ad426cc170
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-06 08:34:15 -0300

    [fix] Fixed missing db name

--- desafio8.sql
+++ desafio8.sql
@@ -5,8 +5,8 @@ CREATE TRIGGER trigger_usuario_delete
     BEFORE DELETE
     ON SpotifyClone.user FOR EACH ROW
 BEGIN
-    DELETE FROM history WHERE user_id = OLD.user_id;
-    DELETE FROM following WHERE user_id = OLD.user_id;
+    DELETE FROM SpotifyClone.history WHERE user_id = OLD.user_id;
+    DELETE FROM SpotifyClone.following WHERE user_id = OLD.user_id;
 END$$    
 
 DELIMITER ;
