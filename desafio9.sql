9143a20814d3b276a1a6bdd118d9082b45175de7
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-06 08:38:41 -0300

    [new] req 9

---
+++ desafio9.sql
@@ -0,0 +1,20 @@
+-- https://www.mysqltutorial.org/stored-procedures-parameters.aspx
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista (
+	IN  artist_name VARCHAR(32)
+)
+BEGIN
+SELECT
+    artist_name AS artista,
+    al.name AS album
+FROM 
+    SpotifyClone.album AS al
+INNER JOIN
+    SpotifyClone.artist AS ar
+    ON ar.artist_id = al.artist_id
+WHERE ar.name = artist_name
+ORDER BY album;
+END$$
+
+DELIMITER ;
