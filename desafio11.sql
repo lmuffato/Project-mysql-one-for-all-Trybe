a29d669f9d3cacd952f4fd0a450270eade6b56c5
Marina Alane <marina.alane@outlook.com>
2021-08-06 11:40:35 -0300

    agora vai

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,6 @@
 CREATE VIEW cancoes_premium AS (
-  SELECT s.musica_nome, COUNT(ps.usuario_id) 'reproducoes'
+  SELECT s.musica_nome,
+  COUNT(ps.usuario_id) 'reproducoes'
   FROM SpotifyClone.historicoReproducoes ps
   INNER JOIN SpotifyClone.musicas s ON s.musica_id = ps.musica_id
   WHERE usuario_id IN (SELECT usuario_id FROM SpotifyClone.usuario u WHERE u.plano_id IN (2, 3))
