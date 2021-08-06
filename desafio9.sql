6f2b8ef592444ad87bcd74069a5209c1d453420e
guilhermemd <guilherme.md@gmail.com>
2021-08-06 20:03:25 -0300

    fix: :bug: procedure of req 09

--- desafio9.sql
+++ desafio9.sql
@@ -5,13 +5,13 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN nome_do_artista VARCHAR(150))
 BEGIN
 SELECT
-artista.artista AS artista,
-album.album AS album
+artistas.artista,
+album.album
 FROM
-SpotifyClone.artistas AS artista
+SpotifyClone.artistas AS artistas
 INNER JOIN 
-SpotifyClone.albuns AS albuns .
-ON artista.artista_id = albuns.artista_id
+SpotifyClone.albuns AS albuns
+ON artistas.artista_id = albuns.artista_id
 WHERE artista = nome_do_artista;
 END $$
 
