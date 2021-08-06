aece7b8a6c6cd63120baf436ecfa5f839809bfa2
guilhermemd <guilherme.md@gmail.com>
2021-08-06 19:50:03 -0300

    feat: :sparkles: req 09 implementation

---
+++ desafio9.sql
@@ -0,0 +1,18 @@
+-- USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome_do_artista VARCHAR(150))
+BEGIN
+SELECT
+artista.artista AS artista,
+album.album AS album
+FROM
+SpotifyClone.artistas AS artista
+INNER JOIN 
+SpotifyClone.albuns AS albuns .
+ON artista.artista_id = albuns.artista_id
+WHERE artista = nome;
+END $$
+
+DELIMITER;
