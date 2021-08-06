1e88565b226e18231b680b1e2158e3f2d27396ff
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-05 21:09:25 -0300

    [new] req 11

---
+++ desafio11.sql
@@ -0,0 +1,21 @@
+DROP VIEW IF EXISTS cancoes_premium;
+USE SpotifyClone;
+
+CREATE VIEW cancoes_premium AS
+SELECT
+    s.name AS nome,
+    COUNT(h.user_id) AS reproducoes
+FROM 
+    SpotifyClone.song AS s
+INNER JOIN
+    SpotifyClone.history AS h
+    ON s.song_id = h.song_id
+INNER JOIN
+    SpotifyClone.user AS u
+    ON u.user_id = h.user_id
+INNER JOIN
+    SpotifyClone.plan AS p
+    ON p.plan_id = u.plan_id
+WHERE p.name IN ('familiar', 'universitário')
+GROUP BY nome
+ORDER BY nome;
