201a99ccc00e4d56d017b31d82f3a373d800b650
guilhermemd <guilherme.md@gmail.com>
2021-08-06 15:19:21 -0300

    feat: :sparkles: req 02 implementation

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+SELECT 
+COUNT(DISTINCT cancoes.cancao_id) AS cancoes,
+COUNT(DISTINCT artistas.artista_id) AS artistas,
+COUNT(DISTINCT albuns.album_id) AS albuns
+FROM 
+  SpotifyClone.cancoes AS cancoes
+INNER JOIN
+  SpotifyClone.albuns AS albuns
+ON cancoes.album_id = albuns.album_id
+INNER JOIN 
+  SpotifyClone.artistas AS artistas
+ON artistas.artista_id = albuns.artista_id
