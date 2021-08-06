04cf971525b4e109717f3c9551243b0f8ce562d8
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-06 08:27:48 -0300

    [new] req 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+-- https://www.mysqltutorial.org/mysql-triggers/mysql-before-delete-trigger/
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE
+    ON SpotifyClone.user FOR EACH ROW
+BEGIN
+    DELETE FROM history WHERE user_id = OLD.user_id;
+    DELETE FROM following WHERE user_id = OLD.user_id;
+END$$    
+
+DELIMITER ;
