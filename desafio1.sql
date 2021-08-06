7d1090f2542bb7edcc742eebd9c4de2e836505e0
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-06 18:52:19 -0300

    Desafio 1: tabela nao esta sendo preenchida

--- desafio1.sql
+++ desafio1.sql
@@ -53,77 +53,77 @@ FOREIGN KEY(usuario_id) REFERENCES usuario(usuario_id)
 
 INSERT INTO plano(plano_nome, valor) 
 VALUES
- ('gratuito',0),
- ('familiar',7.99),
- ('universitário',5.99);
+  ('gratuito',0),
+  ('familiar',7.99),
+  ('universitário',5.99);
 
 INSERT INTO usuario(usuario_nome, idade, plano_id) 
 VALUES
- ('Thati',23,1),
- ('Cintia',35,2),
- ('Bill',20,3),
- ('Roger',45,1);
+  ('Thati',23,1),
+  ('Cintia',35,2),
+  ('Bill',20,3),
+  ('Roger',45,1);
 
 INSERT INTO artista(artista_nome) 
 VALUES
- ('Walter Phoenix'),
- ('Peter Strong'),
- ('Lance Day'),
- ('Freedie Shannon');
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
 
 INSERT INTO album(album_nome, artista_id) 
 VALUES
- ('Envious',1),
- ('Exuberant',1),
- ('Hallowed Steam',2),
- ('Incadescent',3),
- ('Temporary Culture',4);
+  ('Envious',1),
+  ('Exuberant',1),
+  ('Hallowed Steam',2),
+  ('Incadescent',3),
+  ('Temporary Culture',4);
 
 INSERT INTO cancao(cancao_nome, album_id) 
 VALUES
- ('Soul For Us',1),
- ('Reflections Of Magic',1),
- ('Dance With Her Own',1),
- ('Troubles Of My Inner Fire',2),
- ('Time Fireworks',2),
- ('Magic Circus',3),
- ('Honey, So Do I',3),
- ("Sweetie, Let's Go Wild",3),
- ('She Knows',3),
- ('Fantasy For Me',4),
- ('Celebration Of More',4),
- ('Rock His Everything',4),
- ('Home Forever',4),
- ('Diamond Power',4),
- ("Honey, Let's Be Silly",4),
- ('Thang Of Thunder',5),
- ('Words Of Her Life',5),
- ('Without My Streets',5);
+  ('Soul For Us',1),
+  ('Reflections Of Magic',1),
+  ('Dance With Her Own',1),
+  ('Troubles Of My Inner Fire',2),
+  ('Time Fireworks',2),
+  ('Magic Circus',3),
+  ('Honey, So Do I',3),
+  ("Sweetie, Let's Go Wild",3),
+  ('She Knows',3),
+  ('Fantasy For Me',4),
+  ('Celebration Of More',4),
+  ('Rock His Everything',4),
+  ('Home Forever',4),
+  ('Diamond Power',4),
+  ("Honey, Let's Be Silly",4),
+  ('Thang Of Thunder',5),
+  ('Words Of Her Life',5),
+  ('Without My Streets',5);
 
 INSERT INTO seguindo_artistas (usuario_id,artista_id) 
 VALUES
- (1,1),
- (1,4),
- (1,3),
- (2,1),
- (2,3),
- (3,2),
- (3,1),
- (4,4);
+  (1,1),
+  (1,4),
+  (1,3),
+  (2,1),
+  (2,3),
+  (3,2),
+  (3,1),
+  (4,4);
 
 INSERT INTO historico_reproducoes(cancao_id, usuario_id) 
 VALUES
- (1,1),
- (2,1),
- (15,1),
- (17,1),
- (14,2),
- (18,2),
- (2,2),
- (16,2),
- (4,3),
- (17,3),
- (6,3),
- (3,4),
- (19,4),
- (12,4);
+  (1,1),
+  (2,1),
+  (15,1),
+  (17,1),
+  (14,2),
+  (18,2),
+  (2,2),
+  (16,2),
+  (4,3),
+  (17,3),
+  (6,3),
+  (3,4),
+  (19,4),
+  (12,4);
