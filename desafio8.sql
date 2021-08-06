25cb4ccf54eb39e1c84ce32b436e812ea27519b1
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-08-06 06:00:26 -0300

    :sparkles: Requirement 08

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+CREATE TRIGGER trigger_usuario_delete
+	BEFORE DELETE ON SpotifyClone.usuarios
+    FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.historico WHERE usuario_id=OLD.usuario_id;
+    DELETE FROM SpotifyClone.seguidores WHERE usuario_id=OLD.usuario_id;
+END $$
+
+DELIMITER ;
