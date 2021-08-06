ebb6df9d75993dfbd3fd20b64db170cdbf0337b8
Felipe Muller <felipeavmuller@gmail.com>
2021-08-05 21:34:16 -0300

    FIX requisito 10 E salvando imagem das planilnhas

--- desafio10.sql
+++ desafio10.sql
@@ -2,13 +2,12 @@ USE SpotifyClone;
 
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(nome_usuario VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(id VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE qtd_musica INT;
-SELECT COUNT(h.usuario_id) FROM historico_reproducoes h
-INNER JOIN usuarios u ON u.usuario_id = h.usuario_id
-WHERE u.nome = nome_usuario
+SELECT COUNT(usuario_id) FROM historico_reproducoes
+WHERE usuario_id = id
 INTO qtd_musica;
 RETURN qtd_musica;
 END $$
