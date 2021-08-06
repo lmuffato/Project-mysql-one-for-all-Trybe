ce4c67635bc6fa38b72071c0b56fd19fa0a683dd
Elisa <elisafranca.carvalho@gmail.com>
2021-08-06 14:43:37 -0300

    feat: requirement 7

--- desafio1.sql
+++ desafio1.sql
@@ -84,7 +84,7 @@ VALUES
 (2, 1),
 (2, 3),
 (3, 1),
-(3,2),
+(3, 2),
 (4, 4);
 
 INSERT INTO albums (title, singer_id)
---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+-- Para esse desafio consultei o PR #63 do Murilo Gonçalves.
+CREATE VIEW perfil_artistas AS (
+SELECT per.singer_name AS 'artista',
+al.title AS 'album',
+COUNT(fol.user_id) AS 'seguidores'
+FROM SpotifyClone.performers AS per
+INNER JOIN SpotifyClone.albums AS al
+INNER JOIN SpotifyClone.following_artists as fol
+ON (per.singer_id = fol.singer_id) AND (per.singer_id = al.singer_id)
+GROUP BY al.title, per.singer_id
+ORDER BY seguidores DESC, artista, album);
