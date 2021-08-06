57e8b8950475eaf02d63fa279dae36b4f0065f28
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-08-06 06:44:12 -0300

    :rotating_light: Fix linter sintax [code_climate]

--- desafio9.sql
+++ desafio9.sql
@@ -2,10 +2,10 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(50))
 BEGIN
-	SELECT
-		art.nome AS artista,
-        alb.nome AS album
-	FROM SpotifyClone.albuns AS alb
+  SELECT
+    art.nome AS artista,
+    alb.nome AS album
+  FROM SpotifyClone.albuns AS alb
     INNER JOIN SpotifyClone.artistas AS art ON alb.artista_id=art.artista_id AND art.nome=artista
     ORDER BY alb.nome;
 END $$
