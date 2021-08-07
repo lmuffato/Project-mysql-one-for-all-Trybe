938369766e7d8955ebf5c1060b656d8a86942360
Nathalia Zebral <natzebral@gmail.com>
2021-08-07 19:08:18 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+CREATE VIEW estatisticas_musicais AS 
+SELECT
+(SELECT  COUNT(SongID) FROM SONGS) AS 'cancoes',
+(SELECT  COUNT(SingerID) FROM SINGERS) AS 'artistas',
+(SELECT  COUNT(AlbumID) FROM ALBUNS) AS 'albuns';
+
+SELECT * FROM estatisticas_musicais;
