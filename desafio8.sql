c7d6242a4e67ae0df828fc459eaf43d3c0282368
luanlsr <luanfswd@gmail.com>
2021-08-07 11:53:04 -0300

    requisito 8 finalizado

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON usuarios
 FOR EACH ROW
 BEGIN
- DELETE FROM seguindo_artistas WHERE usuario_id = OLD.usuario_id;
+DELETE FROM seguindo_artistas WHERE usuario_id = OLD.usuario_id;
 DELETE FROM historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
