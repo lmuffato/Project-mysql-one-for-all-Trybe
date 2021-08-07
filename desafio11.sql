935850f2b74bf7e9179a7b9f5eb97faff4d2664a
luanlsr <luanfswd@gmail.com>
2021-08-07 13:01:46 -0300

    requisito 11 finalizado

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+CREATE VIEW cancoes_premium AS (
+SELECT c.cancao AS nome,
+COUNT(h.usuario_id) AS reproducoes
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h
+INNER JOIN SpotifyClone.usuarios AS u
+ON u.usuario_id = h.usuario_id
+AND h.cancao_id = c.cancao_id
+WHERE u.plano_id = 2
+OR u.plano_id = 3
+GROUP BY nome
+ORDER BY nome
+);
