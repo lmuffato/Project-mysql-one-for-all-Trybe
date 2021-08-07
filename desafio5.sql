8333928ee20caba261ce12f65d794519b8d94d6e
luanlsr <luanfswd@gmail.com>
2021-08-07 12:00:06 -0300

    requisito 8 finalizado

--- desafio5.sql
+++ desafio5.sql
@@ -7,3 +7,16 @@ GROUP BY cancao
 ORDER BY reproducoes DESC, cancao
 LIMIT 2
 );
+
+
+
+CREATE VIEW perfil_artistas AS (
+SELECT ar.artista AS artista, album AS album, COUNT(sa.usuario_id) AS seguidores
+FROM SpotifyClone.artistas AS ar
+INNER JOIN SpotifyClone.albuns AS al
+ON al.artista_id = ar.artista_id
+INNER JOIN SpotifyClone.seguindo_artistas AS sa
+ON sa.artista_id = al.artista_id
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista, album;
+);
--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,4 @@
 USE SpotifyClone;
-
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON usuarios
