2e64e329898b803d401629a826285b9473e3323a
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-08-07 17:44:37 -0300

    Fix result req7

--- desafio7.sql
+++ desafio7.sql
@@ -2,7 +2,7 @@ CREATE VIEW perfil_artistas AS
 SELECT artista.artista AS `artista`,
   album.album AS `album`,
   COUNT(artista_seguido.usuario_id) AS `seguidores`
-FROM SpotifyClone.artista
+FROM SpotifyClone.artists
 JOIN SpotifyClone.album ON artista.artista_id = album.artista_id
 JOIN SpotifyClone.artista_seguido ON artista.artista_id = artista_seguido.artista_id
 GROUP BY `album`, `artista`
