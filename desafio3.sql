1f3f6b354103aafa33f543e81e8ea595dd9c77c7
biazidioti <bzferreira.bio@gmail.com>
2021-08-07 15:54:17 -0300

    Fiz codeclimate.

--- desafio3.sql
+++ desafio3.sql
@@ -6,7 +6,7 @@ CREATE VIEW historico_reproducao_usuarios AS
     SpotifyClone.customer c
       INNER JOIN
     SpotifyClone.customer_song cs ON c.customer_id = cs.customer_id
-      INNER JOIN
+    INNER JOIN
     SpotifyClone.song s ON s.song_id = cs.song_id
   ORDER BY usuario, nome;
   
