c1a80eb7e45bfcf9eb175e4d73119a1112c91143
iago <iago.pferreiravr@gmail.com>
2021-08-07 17:01:28 -0300

    Feat: req10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id_usuario INT)
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE contador INT;
+  SELECT COUNT(usuario_id) FROM SpotifyClone.historico_reproducoes WHERE usuario_id = id_usuario INTO contador;
+  RETURN contador;
+END $$
+
+DELIMITER ;
