14d5f15d541584af2ba01a28a149b52d9541ba77
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-08-07 19:27:41 -0300

    Fix tables req7

--- desafio7.sql
+++ desafio7.sql
@@ -3,7 +3,7 @@ SELECT artist.artists AS `artista`,
   album.albums AS `album`,
   COUNT(artista_seguido.usuario_id) AS `seguidores`
 FROM SpotifyClone.artists
-JOIN SpotifyClone.albums ON artista.artista_id = album.artista_id
-JOIN SpotifyClone.artista_seguido ON artista.artista_id = artista_seguido.artista_id
+JOIN SpotifyClone.albums ON artist.follower_id = album.artista_id
+JOIN SpotifyClone.followers ON artist.follower_id = artista_seguido.follower_id
 GROUP BY `album`, `artista`
 ORDER BY `seguidores` DESC, `artista`, `album`;
