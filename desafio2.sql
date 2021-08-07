5fc536ad9bcd8995e5230c4b503514d2a36d5ae4
biazidioti <bzferreira.bio@gmail.com>
2021-08-07 13:38:54 -0300

    Challenge 2.

--- desafio2.sql
+++ desafio2.sql
@@ -1,12 +1,12 @@
-CREATE VIEW estatisticas_musicais AS (
+CREATE VIEW estatisticas_musicais AS
   SELECT
     COUNT(s.song_id) AS `cancoes`,
-    COUNT(DISTINCT(a.artist_id)) AS `artistas`,
-    COUNT(DISTINCT(al.album_id)) AS `albuns`
+    COUNT(DISTINCT a.artist_id) AS `artistas`,
+    COUNT(DISTINCT al.album_id) AS `albuns`
     FROM 
-      SpotifyClone.song s
-    INNER JOIN 
-      SpotifyClone.album al ON s.album_id = al.album_id
+      SpotifyClone.artist a
     INNER JOIN
-      SpotifyClone.artist a ON al.album_id = a.artist_id
-);
+	SpotifyClone.album al
+    ON al.artist_id = a.artist_id
+     INNER JOIN
+    SpotifyClone.song s ON s.album_id = al.album_id;
