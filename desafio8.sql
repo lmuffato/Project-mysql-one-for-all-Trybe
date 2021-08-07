0e997eebf51d39a037761c26179e1a78ef3af3e3
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-08-07 19:37:38 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.followers WHERE user_id = OLD.user_id;
+DELETE FROM SpotifyClone.`history` WHERE user_id = OLD.user_id;
+END $$
+
+DELIMITER ;
