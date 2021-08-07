53a23f08f64b60ebee678492af631ec02c0a7c8b
luanlsr <luanfswd@gmail.com>
2021-08-07 15:49:52 -0300

    requisito 10 finalizado

---
+++ desafio10.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone; -- obrigatório para criar a função no banco correto
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(userId INT)
+RETURNS INT READS SQL DATA
+BEGIN
+   DECLARE totalMusicas INT;
+   SELECT COUNT(cancao_id) 
+   FROM SpotifyClone.historico_de_reproducoes
+   WHERE usuario_id = userId
+   INTO totalMusicas ;
+   RETURN totalMusicas;
+   
+END $$
+
+DELIMITER ;
