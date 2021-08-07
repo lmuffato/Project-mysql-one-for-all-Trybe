e2df1fb9f6731a0fbbf678c5c0deab49a9c1cee7
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-08-07 17:22:44 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+CREATE VIEW perfil_artistas AS
+SELECT artista.artista AS `artista`,
+  album.album AS `album`,
+  COUNT(artista_seguido.usuario_id) AS `seguidores`
+FROM SpotifyClone.artista
+JOIN SpotifyClone.album ON artista.artista_id = album.artista_id
+JOIN SpotifyClone.artista_seguido ON artista.artista_id = artista_seguido.artista_id
+GROUP BY `album`, `artista`
+ORDER BY `seguidores` DESC, `artista`, `album`;
