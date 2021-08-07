70a4dd9528c018690b6603f9aa988b5f565ae2e6
biazidioti <bzferreira.bio@gmail.com>
2021-08-07 16:26:42 -0300

    Fix spelling

--- desafio9.sql
+++ desafio9.sql
@@ -5,7 +5,7 @@ BEGIN
 SELECT
 a.artist_name AS `artista`,
 ab.album_name AS `album`
-FROM SpotifyClone.artits a
+FROM SpotifyClone.artist a
 INNER JOIN SpotifyClone.album ab
 ON a.artist_id = ab.artist_id
 WHERE a.artist_name = `artista`
