5a6058bbc9a099c7c1b59e81aa366aff970a3c8e
iago <iago.pferreiravr@gmail.com>
2021-08-07 15:34:12 -0300

    Feat: req5

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+-- CREATE VIEW top_2_hits_do_momento AS
+  SELECT
+    c.cancao AS `cancao`,
+    COUNT(h.cancao_id) AS `reproducoes`
+  FROM SpotifyClone.cancoes AS c
+  INNER JOIN SpotifyClone.historico_reproducoes AS h
+  ON h.cancao_id = c.cancao_id
+  GROUP BY c.cancao
+  ORDER BY `reproducoes` DESC, `cancao`
+  LIMIT 2;
