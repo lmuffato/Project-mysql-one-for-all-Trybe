55c714650aa174b876219495b8e003f2813fcaeb
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-08 08:57:25 -0300

    fix: requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
 SELECT 
     ROUND(MIN(plan_value), 2) AS faturamento_minimo,
     ROUND(MAX(plan_value), 2) AS faturamento_maximo,
-    ROUND(AVG(plan_value), 2) AS faturamento_medio,
+    CONVERT(ROUND(AVG(plan_value), 2), DECIMAL(4,2)) AS faturamento_medio,
     ROUND(SUM(plan_value), 2) AS faturamento_total
 FROM
     Plan AS p
