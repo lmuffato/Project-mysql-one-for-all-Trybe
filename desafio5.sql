0e35dd5d854fe77cf6b14dbb6644d92ab2dc4119
igson <igson660@gmail.com>
2021-08-08 14:37:50 -0500

    FIX: desafio 05

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+CREATE VIEW SpotifyClone.top_2_hits_do_momento AS 
+  SELECT 
+    music_name AS `cancao`,
+    COUNT(music_name) AS `reproducoes`
+  FROM SpotifyClone.MUSIC AS m
+  INNER JOIN SpotifyClone.PLAYBACK_HISTORY AS ph
+  ON m.music_id = ph.music_id
+  GROUP BY music_name
+  ORDER BY `reproducoes` DESC, `cancao` ASC
+  LIMIT 2;
+  
