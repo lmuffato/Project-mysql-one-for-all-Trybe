47aa701b816c96208cad8b300e2d712082564af6
Patrick Morais <ppternunes@gmail.com>
2021-08-08 20:49:38 -0300

    req 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+CREATE VIEW historico_reproducao_usuarios AS
+  SELECT
+    users.user_name AS usuario, 
+    songs.song_name AS nome
+  FROM 
+    SpotifyClone.users AS users
+      INNER JOIN
+    SpotifyClone.reproduction_history AS history ON users.user_id = history.user_id
+	  INNER JOIN
+    SpotifyClone.songs AS songs ON songs.song_id = history.song_id
+ORDER BY usuario, nome;
