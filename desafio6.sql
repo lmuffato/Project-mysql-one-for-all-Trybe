b2ff40e9e9c8a59b14e02075abf63fd92fe69ee1
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-08 08:31:09 -0300

    feat: requisito 6,test

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+CREATE VIEW faturamento_atual AS
+SELECT 
+    ROUND(MIN(plan_value), 2) AS faturamento_minimo,
+    ROUND(MAX(plan_value), 2) AS faturamento_maximo,
+    ROUND(AVG(plan_value), 2) AS faturamento_medio,
+    ROUND(SUM(plan_value), 2) AS faturamento_total
+FROM
+    Plan AS p
+        INNER JOIN
+    User_plan AS u ON p.plan_id = u.plan_id;
+    
