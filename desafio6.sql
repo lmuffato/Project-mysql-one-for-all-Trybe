32cdf643e9446b6875c61fc261db3bd71121cd8e
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-08-07 21:48:53 -0300

    requisito 6 e 7

---
+++ desafio6.sql
@@ -0,0 +1,13 @@
+CREATE VIEW faturamento_atual AS (
+SELECT
+MIN(p.plan_value) AS 'faturamento_minimo',
+MAX(p.plan_value) AS 'faturamento_maximo',
+ROUND(AVG(p.plan_value), 2)  AS 'faturamento_medio',
+SUM(p.plan_value) AS 'faturamento_totaL'
+FROM SpotifyClone.plan AS p
+INNER JOIN SpotifyClone.user_info AS i
+ON p.plan_id = i.plan_id
+);
+
+-- uso o inner join para juntar com a quantidade de planos que tenho na tabela user_info, assim quando faço o avg, 
+-- pego a media de quantas pessoas estao usandos os planos
---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+CREATE VIEW faturamento_atual AS (
+SELECT
+	a.artist_name AS 'artista',
+    al.album_name AS 'album',
+    COUNT(f.artist_id) AS 'seguidores'
+FROM SpotifyClone.artist AS a
+INNER JOIN SpotifyClone.album AS al ON a.artist_id = al.artist_id
+INNER JOIN SpotifyClone.follow AS f ON a.artist_id = f.artist_id
+GROUP BY f.artist_id, al.album_name
+ORDER BY COUNT(f.artist_id) DESC, a.artist_name ASC, al.album_name ASC
+);
+
+-- SELECT artist_id, COUNT(artist_id) FROM SpotifyClone.follow
+-- GROUP BY artist_id
+-- também uso o al.album_name no group by, pois um artista pode ter mais de um album
