6910a45f7ece67a7b0625efde3bcec61ed1d43d8
mallmann02 <leomallmann020302@gmail.com>
2021-08-08 20:49:28 -0300

    fix: column ALIAS name

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE albuns_do_artista(IN nome_do_artista VARCHAR(30))
 BEGIN
 SELECT
-albuns.nome_album AS `albunm`,
+albuns.nome_album AS `album`,
 artistas.nome_artista AS `artista`
 FROM SpotifyClone.Albuns AS albuns
 INNER JOIN SpotifyClone.Artistas AS artistas
