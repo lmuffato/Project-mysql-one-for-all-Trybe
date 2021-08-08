d1cc92a20bd3dc100784ddbfa917b2cd4c3e13d7
mallmann02 <leomallmann020302@gmail.com>
2021-08-08 20:29:50 -0300

    challeges: c3

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+REATE VIEW historico_reproducao_usuarios AS
+SELECT
+nome_usuario AS `usuario`,
+(
+SELECT nome_cancao
+FROM SpotifyClone.Cancoes AS cancoes
+WHERE historico.cancao_id = cancoes.cancao_id
+) AS `nome`
+FROM SpotifyClone.Usuarios AS usuarios
+INNER JOIN SpotifyClone.Historico_de_reproducoes AS historico
+ON usuarios.usuario_id = historico.usuario_id
+ORDER BY `usuario`, `nome`;
