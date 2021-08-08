bc64d1e3a01ada25bf3f17a367c8aa4031e77cee
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-08 13:10:46 -0300

    Desafio 6: faz levantamento do faturamento atual da empresa- MIN-MAX-SUM-AVG

---
+++ desafio6.sql
@@ -0,0 +1,6 @@
+CREATE VIEW faturamento_total AS
+SELECT ROUND(MIN(P.valor),2) AS 'faturamento_minimo', ROUND(MAX(P.valor),2) AS 'faturamento_maximo',
+ROUND(AVG(P.valor),2) AS 'faturamento_medio', ROUND(SUM(P.valor),2) AS 'faturamento_total'
+FROM SpotifyClone.plano P
+INNER JOIN SpotifyClone.usuario U
+ON P.plano_id = U.plano_id;
