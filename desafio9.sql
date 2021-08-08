d950abbe3ebc07deac8a838a3532e29eb968041e
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-08 20:23:48 -0300

    Feat: (req-9) Create a procedure called albuns_do_artista that takes as a parameter the name of a person artist and returns two columns, artist and album of the same

---
+++ desafio9.sql
@@ -0,0 +1,27 @@
+-- SELECT * FROM SpotifyClone.Album;
+-- SELECT * FROM SpotifyClone.Artista;
+
+-- SELECT
+-- ART.nome_artista AS `artista`,
+-- ALB.nome_album AS `album`
+-- FROM SpotifyClone.Album AS ALB
+-- INNER JOIN SpotifyClone.Artista AS ART ON ART.artista_ID = ALB.artista_ID
+-- WHERE ART.nome_artista = 'Walter Phoenix'
+-- ORDER BY `album`;
+
+-- USE SpotifyClone;
+
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN name_artista VARCHAR(50))
+BEGIN
+	SELECT
+	ART.nome_artista AS `artista`,
+	ALB.nome_album AS `album`
+	FROM SpotifyClone.Album AS ALB
+	INNER JOIN SpotifyClone.Artista AS ART ON ART.artista_ID = ALB.artista_ID
+	WHERE ART.nome_artista = name_artista
+	ORDER BY `album`;
+END $$
+DELIMITER ;
+
+-- CALL albuns_do_artista('Walter Phoenix');
