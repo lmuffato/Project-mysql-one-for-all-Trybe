77b5f441c8b78a13cd2eeb4c25beae39e8f2f025
mallmann02 <leomallmann020302@gmail.com>
2021-08-08 20:31:43 -0300

    Challenges: c6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+CREATE VIEW faturamento_total AS
+SELECT
+MIN(valor_plano) AS `faturamento minimo`,
+MAX(valor_plano) AS `faturamento_maximo`,
+ROUND(SUM(valor_plano)/4,2) AS `faturamento_medio`,
+SUM(valor_plano) AS `faturamento_total`
+FROM SpotifyClone.Planos;
