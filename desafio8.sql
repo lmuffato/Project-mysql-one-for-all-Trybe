af2b79af079ca027c069e45a2be5d56aa6bbe4df
mallmann02 <leomallmann020302@gmail.com>
2021-08-08 20:31:43 -0300

    Challenges: c8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.Usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.Historico_de_reproducoes
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.Seguindo_artistas
+WHERE usuario_id = OLD.usuario_id;
+END
