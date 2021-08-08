8a502b8d0b5e26bb0bf5c9fa2628ff72bcc16081
Patrick Morais <ppternunes@gmail.com>
2021-08-08 20:31:35 -0300

    req 2

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+CREATE VIEW estatisticas_musicais AS
+  SELECT
+    COUNT(songs.song_name) AS cancoes, 
+    COUNT(artists.artist_name) AS artistas,
+    COUNT(albuns.album_name) AS albuns
+  FROM 
+    SpotifyClone.artists AS artists
+      INNER JOIN
+    SpotifyClone.albuns AS albuns ON artists.artist_id = albuns.artist_id
+      INNER JOIN
+    SpotifyClone.songs AS songs ON songs.album_id = albuns.album_id;
+  
