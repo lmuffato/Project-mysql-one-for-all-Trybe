86863b84e4a72f0bce51961758cfb9d826d50dd8
mallmann02 <leomallmann020302@gmail.com>
2021-08-08 20:31:43 -0300

    Challenges: c9

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE albuns_do_artista(IN nome_do_artista VARCHAR(30))
+BEGIN
+SELECT
+albuns.nome_album AS `albunm`,
+artistas.nome_artista AS `artista`
+FROM SpotifyClone.Albuns AS albuns
+INNER JOIN SpotifyClone.Artistas AS artistas
+ON albuns.artista_id = artistas.artista_id
+WHERE artistas.nome_artista = nome_do_artista;
+END
