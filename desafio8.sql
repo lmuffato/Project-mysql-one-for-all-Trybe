dcd22d72bed09a867e97d54904e5d90813c699da
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-09 06:11:29 -0300

    fix: requisito 8, digitaçao errada

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,12 @@
-USE SpotifyCLone;
+USE SpotifyClone;
 
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON User
 FOR EACH ROW
 BEGIN 
-DELETE FROM History WHERE user_id = OLD.user_id;
-DELETE FROM Artist_followers WHERE user_id = OLD.user_id;
+  DELETE FROM History WHERE user_id = OLD.user_id;
+  DELETE FROM Artist_followers WHERE user_id = OLD.user_id;
 END $$
+
 DELIMITER ;
