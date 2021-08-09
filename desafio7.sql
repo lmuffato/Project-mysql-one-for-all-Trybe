77649f169a21a07706d01a159b2cfcfe7abb2c1e
Luciano Amâncio <madruga665@hotmail.com>
2021-08-09 11:44:56 -0300

    fix: Desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -7,5 +7,5 @@ INNER JOIN SpotifyClone.albuns AS albuns
 INNER JOIN SpotifyClone.seguindo_artistas AS seguindo_artistas
 ON artistas.artista_id = albuns.artista_id AND artistas.artista_id = seguindo_artistas.artista_id
 GROUP BY seguindo_artistas.artista_id, albuns.album_nome
-ORDER BY `seguidores` DESC, `artista`
+ORDER BY `seguidores` DESC, `artista`, `album`
 );
