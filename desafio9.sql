ac311398b6f51bd098caa9cd5072cb0e69ee6048
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-09 08:16:53 -0300

    fix: requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -12,18 +12,4 @@ HAVING `artista` = Artist_name
 ORDER BY `album`;
 END $$
 
-DELIMITER ;USE SpotifyClone;
-
-DELIMITER $$
-CREATE PROCEDURE albuns_do_artista(IN Artist_name VARCHAR(100))
-BEGIN
-SELECT Artist_name AS artista, Album_name AS album
-FROM Artist AS a
-INNER JOIN Album AS alb
-ON a.Artist_id = alb.Artist_id
-GROUP BY `artista`,`album`
-HAVING `artista` = Artist_name
-ORDER BY `album`;
-END $$
-
 DELIMITER ;
