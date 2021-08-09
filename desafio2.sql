02590c9ed3b8bc9892480c06a82f33f285ef0e03
Gabriel Ferreira <glucas709@gmail.com>
2021-08-08 23:38:25 -0300

    Challenge 2 completed

---
+++ desafio2.sql
@@ -0,0 +1,21 @@
+DROP VIEW IF EXISTS estatisticas_musicais;
+CREATE VIEW estatisticas_musicais AS
+SELECT
+  (
+    SELECT
+      COUNT(cancao_id)
+    FROM
+      SpotifyClone.cancoes
+  ) AS 'cancoes',
+  (
+    SELECT
+      COUNT(artista_id)
+    FROM
+      SpotifyClone.artistas
+  ) AS 'artistas',
+  (
+    SELECT
+      COUNT(album_id)
+    FROM
+      SpotifyClone.albuns
+  ) AS 'albuns';
