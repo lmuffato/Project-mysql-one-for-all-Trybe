7809b854881cd07773b2d4924924fc71578e110c
Gabriel Ferreira <glucas709@gmail.com>
2021-08-09 10:23:33 -0300

    fix: Challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,17 +3,13 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
-  DELETE
-    h, af 
-  FROM 
-    SpotifyClone.historico_de_reproducoes h
-  JOIN
-    SpotifyClone.artistas_favoritos af
-  ON
-    af.usuario_id = h.usuario_id
+  DELETE FROM
+  SpotifyClone.historico_de_reproducoes h
   WHERE
-    h.usuario_id = OLD.usuario_id
-  AND
+    h.usuario_id = OLD.usuario_id;
+  DELETE FROM
+    SpotifyClone.artistas_favorito af
+  WHERE
     af.usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
