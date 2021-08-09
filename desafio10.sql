57be8e180d42d6f35878ad8af551b221f2618eaf
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-09 09:37:40 -0300

    Fix: Desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,11 @@
+USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico(usuario_id INT)
+CREATE FUNCTION quantidade_musicas_no_historico (usuario_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE quantidade_musicas_no_historico INT;
-SELECT COUNT(1) 
+DECLARE quantidade_musicas_no_historico INT ;
+SELECT COUNT(1)
 FROM SpotifyClone.Historico AS h
 WHERE h.Usuario_id LIKE usuario_id INTO quantidade_musicas_no_historico;
 RETURN quantidade_musicas_no_historico;
