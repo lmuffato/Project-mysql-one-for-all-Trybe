503d83e85ea41138ae905f64e4aa71feaf0d6428
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-08-09 13:44:45 -0300

    requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -2,10 +2,12 @@ USE SpotifyClone;
 
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON SpotifyClone.user_info
+BEFOR DELETE ON SpotifyClone.user_info
 FOR EACH ROW
 BEGIN
   DELETE FROM SpotifyClone.follow WHERE user_id = OLD.user_id;
   DELETE FROM SpotifyClone.history_user WHERE user_id = OLD.user_id;
 END $$
 DELIMITER $$ ;
+
+-- uso before pois SpotifyClone.follow SpotifyClone.history_user são dependendes de user_id, então primeiro preciso deletar as dependencias 
