95b7acf701e8aeaec8fbe73d5c79c45a8d913bfb
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-09 19:38:35 -0300

    Desafio 6 correcao do acento

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 CREATE VIEW faturamento_atual AS
   SELECT
     ROUND(MIN(p.price), 2) AS 'faturamento_minimo',
-    ROUND(MAX(p.price), 2) AS 'faturamento_máximo',
+    ROUND(MAX(p.price), 2) AS 'faturamento_maximo',
     ROUND(AVG(p.price), 2) AS 'faturamento_medio',
     ROUND(SUM(p.price), 2) AS 'faturamento_total'
   FROM plan AS p
