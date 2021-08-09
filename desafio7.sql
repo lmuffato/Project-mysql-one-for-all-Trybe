3f019204de89c66ad8e88206e1c705d312622b68
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-09 13:19:47 -0300

    FIx:desafio8

--- desafio7.sql
+++ desafio7.sql
@@ -3,4 +3,4 @@ SELECT Artista AS artista, al.Album as album, COUNT(s.Usuario_id) AS seguidores
 FROM SpotifyClone.Album AS al
 INNER JOIN SpotifyClone.Artista AS ar ON ar.Artista_id = al.Artista_id
 INNER JOIN SpotifyClone.Seguindo AS s ON s.Artista_id = ar.Artista_id
-GROUP BY Album, Artista ORDER BY seguidores DESC, artista;
+GROUP BY Album, Artista ORDER BY seguidores DESC, artista, album;
--- desafio8.sql
+++ desafio8.sql
@@ -3,8 +3,8 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.Usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.Historico AS h WHERE OLD.Usuario_id = h.Usuario_id;
-DELETE FROM SpotifyClone.Seguindo AS s WHERE OLD.Usuario_id = s.Usuario_id;
+DELETE FROM SpotifyClone.Historico WHERE OLD.Usuario_id = Usuario_id;
+DELETE FROM SpotifyClone.Seguindo WHERE OLD.Usuario_id = Usuario_id;
 END $$
 
 DELIMITER ;
