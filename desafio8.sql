b73db592629cbc31b9681f9b7a9838c251ea1a4e
rogerio-lambert <rogerio.cl@gmail.com>
2021-08-09 19:00:06 -0300

    tentando retirar os alias da query no desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,9 +3,9 @@ CREATE TRIGGER trigger_usuario_delete
   BEFORE DELETE ON SpotifyClone.Users
   FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.Followed_artists AS F
-  WHERE F.user_id = OLD.user_id;
-  DELETE FROM SpotifyClone.User_history AS UH
-  WHERE UH.user_id = OLD.user_id;
+  DELETE FROM SpotifyClone.Followed_artists
+  WHERE user_id = OLD.user_id;
+  DELETE FROM SpotifyClone.User_history
+  WHERE user_id = OLD.user_id;
 END $$
-DELIMITER $$ ;
+DELIMITER ;
