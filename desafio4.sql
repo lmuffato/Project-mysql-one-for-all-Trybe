62da0e240208d5c56c82cf14223ed72ba1a0d012
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-09 20:45:48 -0300

    desafio 4 concluído

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+CREATE View top_3_artistas as (
+  select art.artist_name as 'artista',
+  count(artf.user_id) as 'seguidores'
+  from SpotifyClone.artists_follows as artf
+  inner join SpotifyClone.artists as art
+  inner join SpotifyClone.user as usr
+  on artf.user_id = usr.user_id and art.artist_id = artf.artist_id
+  group by `artista`
+  having count(artf.user_id) > 1
+  order by `artista`, `seguidores` desc
+);
