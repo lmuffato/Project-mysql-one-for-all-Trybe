b544a285fdca12b8d54ca3980cf6055313a179ee
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-09 13:13:37 -0300

    fix: desafio8

--- desafio8.sql
+++ desafio8.sql
@@ -5,6 +5,6 @@ FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.Historico AS h WHERE OLD.Usuario_id = h.Usuario_id;
 DELETE FROM SpotifyClone.Seguindo AS s WHERE OLD.Usuario_id = s.Usuario_id;
-END$$
+END $$
 
 DELIMITER ;
