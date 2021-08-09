ad6005bcfcc83ce721fed46d8de7db9d0f56c49a
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-08-09 14:15:24 -0300

    requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -7,7 +7,7 @@ FOR EACH ROW
 BEGIN
   DELETE FROM SpotifyClone.follow WHERE user_id = OLD.user_id;
   DELETE FROM SpotifyClone.history_user WHERE user_id = OLD.user_id;
-END $$
+END
 DELIMITER $$ ;
 
 -- uso before pois SpotifyClone.follow SpotifyClone.history_user são dependendes de user_id, então primeiro preciso deletar as dependencias 
