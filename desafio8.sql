b76d2bc0fa3a8a7e80abc93f90cdf1e9fd296e46
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-08 21:41:31 -0300

    Desafio 8: Trigger q apaga usuario e todos os seus registros de todas as tabelas

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuario
+FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.seguindo_artistas
+    WHERE OLD.usuario_id = usuario_id;
+    DELETE FROM SpotifyClone.historico_reproducoes
+    WHERE OLD.usuario_id = usuario_id;
+END $$
+
+DELIMITER ;
