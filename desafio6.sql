e1e743d1ab9b0ad73187fc268b52ff15450366b2
mallmann02 <leomallmann020302@gmail.com>
2021-08-08 21:43:06 -0300

    fix: column ALIAS name

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-MIN(valor_plano) AS `faturamento minimo`,
+MIN(valor_plano) AS `faturamento_minimo`,
 MAX(valor_plano) AS `faturamento_maximo`,
 ROUND(SUM(valor_plano)/4,2) AS `faturamento_medio`,
 SUM(valor_plano) AS `faturamento_total`
