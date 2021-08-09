f8cf608280a3b3ba4db51cb6ab751fd19117aa4a
Patrick Morais <ppternunes@gmail.com>
2021-08-08 23:06:55 -0300

    req 9

---
+++ desafio9.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(50))
+BEGIN
+  SELECT
+    artists.artist AS artista,
+    albuns.album_name AS album
+  FROM
+    SpotifyClone.artists AS artists
+      INNER JOIN
+    SpotifyClone.albuns AS albuns ON albuns.artist_id = artists.artist_id
+  WHERE artists.artist_name = artist_name
+  ORDER BY album_name;
+END $$
+
+DELIMITER ;
