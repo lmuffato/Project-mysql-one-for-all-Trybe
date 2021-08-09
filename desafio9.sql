29c54d60aa341fa8de2ae1cb5034a1e5a772605c
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-09 19:46:10 -0300

    Desafio 9 OK

--- desafio9.sql
+++ desafio9.sql
@@ -5,7 +5,7 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(artistParam VARCHAR(50))
   BEGIN
   SELECT 
-  art.name_artist, alb.name_album
+  art.name_artist AS 'artista', alb.name_album AS 'album'
   FROM artist AS art
   INNER JOIN album as alb
   ON art.id_artist = alb.id_artist
