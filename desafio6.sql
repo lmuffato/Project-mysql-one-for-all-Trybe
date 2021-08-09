603f29da46e37061017276a122eb410d096fbb49
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-08-09 13:12:39 -0300

    requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ SELECT
 MIN(p.plan_value) AS 'faturamento_minimo',
 MAX(p.plan_value) AS 'faturamento_maximo',
 ROUND(AVG(p.plan_value), 2)  AS 'faturamento_medio',
-SUM(p.plan_value) AS 'faturamento_totaL'
+SUM(p.plan_value) AS 'faturamento_total'
 FROM SpotifyClone.plan AS p
 INNER JOIN SpotifyClone.user_info AS i
 ON p.plan_id = i.plan_id
