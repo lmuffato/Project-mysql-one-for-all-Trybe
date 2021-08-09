655fd567ee6e54b628c667637032a65f9342d086
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-08-09 13:44:16 -0300

    fix desafio2 ao 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,3 @@
-DROP VIEW IF EXISTS top_2_hits_do_momento;
 CREATE VIEW top_2_hits_do_momento AS
 SELECT s.song AS cancao,COUNT(h.song_id) AS reproducoes FROM SpotifyClone.songs AS s
 INNER JOIN SpotifyClone.historico AS h
@@ -6,4 +5,3 @@ ON h.song_id = s.song_id
 GROUP BY `cancao`
 ORDER BY `reproducoes`DESC,`cancao`
 LIMIT 2;
-SELECT * FROM top_2_hits_do_momento;
