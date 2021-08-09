05d93f6117edc0706c20e3d020e0bfd886f6d86b
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-09 18:52:28 -0300

    fix: requisito 8, terceiro delete

--- desafio8.sql
+++ desafio8.sql
@@ -6,6 +6,7 @@ FOR EACH ROW
 BEGIN 
   DELETE FROM SpotifyClone.History WHERE user_id = OLD.user_id;
   DELETE FROM SpotifyClone.Artist_followers WHERE user_id = OLD.user_id;
+  DELETE FROM SpotifyClone.User_plan WHERE user_id = OLD.user_id;
 END $$
 
 DELIMITER ;
