e4fb9819f5f06979765d6620e63dd95a6decf65b
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-08-09 18:26:16 -0300

    fix:req08-cc

--- desafio8.sql
+++ desafio8.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER to_delete
 BEFORE DELETE ON SpotifyClone.Usuario
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.Usuario_Artista WHERE usuario_id = old.usuario_id;
-    DELETE FROM SpotifyClone.Usuario_Cancao WHERE usuario_id = old.usuario_id;
+DELETE FROM SpotifyClone.Usuario_Artista WHERE usuario_id = old.usuario_id;
+DELETE FROM SpotifyClone.Usuario_Cancao WHERE usuario_id = old.usuario_id;
 END $$
 DELIMITER ;
