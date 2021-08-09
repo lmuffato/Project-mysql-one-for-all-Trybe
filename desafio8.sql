b5900413b1128f5092004ee64e9ab996c7a7ff69
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-09 05:56:17 -0300

    feat: requisito 8, test

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+USE SpotifyCLone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON User
+FOR EACH ROW
+BEGIN 
+DELETE FROM History WHERE user_id = OLD.user_id;
+DELETE FROM Artist_followers WHERE user_id = OLD.user_id;
+END $$
+DELIMITER ;
