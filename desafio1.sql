4c39ee10165c23ce79c3667328123c3a15bb0d79
carlos25xrm <carlos25xrm@gmail.com>
2021-08-09 15:26:14 -0300

    fix(3): requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -90,16 +90,19 @@ VALUES
     ('Dance With Her Own', 1, 1),
     ('Troubles Of My Inner Fire', 1, 2),
     ('Time Fireworks', 1, 2),
+    
     ('Magic Circus', 2, 3),
     ('Honey, So Do I', 2, 3),
     ('Sweetie, Lets Go Wild', 2, 3),
     ('She Knows', 2, 3),
     ('Fantasy For Me', 3, 4),
+    
     ('Celebration Of More', 3, 4),
     ('Rock His Everything', 3, 4),
     ('Home Forever', 3, 4),
     ('Diamond Power', 3, 4),
     ('Honey, Lets Be Silly', 3, 4),
+    
     ('Thang Of Thunder', 4, 5),
     ('Words Of Her Life', 4, 5),
     ('Without My Streets', 4, 5);
@@ -111,23 +114,23 @@ VALUES
     (1, 14),
     (1, 16),
     (2, 13),
-    (2, 17),
-    (2, 15),
-    (2, 2),
-    (3, 4),
-    (3, 16),
-    (3, 6),
-    (4, 3),
-    (4, 11),
-    (4, 18);
+	(2, 17),
+	(2, 15),
+	(2, 2),
+	(3, 4),
+	(3, 16),
+	(3, 6),
+	(4, 3),
+	(4, 11),
+	(4, 18);
 
 INSERT INTO conexoes (usuario_id, artista_id)
 VALUES
     (1, 1),
-    (1, 3),
     (1, 4),
+    (1, 3),
     (2, 1),
     (2, 3),
-    (3, 1),
     (3, 2),
+    (3, 1),
     (4, 4);
