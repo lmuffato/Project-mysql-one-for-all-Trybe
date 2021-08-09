bce7aae6de9a0562f75619aa50edf2d68e78fe3f
Gabriel Ferreira <glucas709@gmail.com>
2021-08-08 23:49:45 -0300

    fix: Challenge 3 completed

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+DROP VIEW IF EXISTS historico_reproducao_usuarios;
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+  u.usuario_name AS 'usuario',
+  c.cancao_title AS 'nome'
+FROM
+  SpotifyClone.historico_de_reproducoes h
+  INNER JOIN SpotifyClone.usuarios u ON u.usuario_id = h.usuario_id
+  INNER JOIN SpotifyClone.cancoes c ON c.cancao_id = h.cancao_id
+  ORDER BY usuario, nome;
+
+SELECT * FROM historico_reproducao_usuarios;
