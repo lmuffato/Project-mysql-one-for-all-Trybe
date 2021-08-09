d7ed5eb117f200b94e30c11eaa1681961798d44e
Bob <wend3llsouza@gmail.com>
2021-08-09 18:52:33 -0300

    :package: one-for-all FIX code climate

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,10 @@
- CREATE VIEW historico_reproducao_usuarios AS
- SELECT us.usuario_nome AS 'usuario',
- song.musica_titulo AS 'nome'
- FROM
- SpotifyClone.historico AS hist
- INNER JOIN
- SpotifyClone.usuario AS us ON hist.usuario_id = us.usuario_id
- INNER JOIN
- SpotifyClone.musicas AS song ON hist.musica_id = song.musica_id
- ORDER BY 1 ASC, 2 ASC;
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT us.usuario_nome AS 'usuario',
+song.musica_titulo AS 'nome'
+FROM
+SpotifyClone.historico AS hist
+INNER JOIN
+SpotifyClone.usuario AS us ON hist.usuario_id = us.usuario_id
+INNER JOIN
+SpotifyClone.musicas AS song ON hist.musica_id = song.musica_id
+ORDER BY 1 ASC, 2 ASC;
