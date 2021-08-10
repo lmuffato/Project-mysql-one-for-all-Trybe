e1cb41832a800f31753942d9695028ed51e76636
samuelmmjr <samuelmmjunior@gmail.com>
2021-08-10 10:32:41 -0300

    Fix:req6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT 
-ROUND(MIN(p.cost), 2) AS faturamento_minimo,
-ROUND(MAX(p.cost), 2) AS faturamento_maximo,
-ROUND(AVG(p.cost), 2) AS faturamento_medio,
-ROUND(SUM(p.cost), 2) AS faturamento_total
-FROM plan p
-INNER JOIN user u ON p.plan_id = u.plan_id;
+MIN(p.price) AS faturamento_minimo,
+MAX(p.price) AS faturamento_maximo,
+ROUND(AVG(p.price), 2) AS faturamento_medio,
+SUM(p.price) AS faturamento_total
+FROM SpotifyClone.plan AS p
+JOIN user AS u
+ON u.plan_id = p.plan_id;
