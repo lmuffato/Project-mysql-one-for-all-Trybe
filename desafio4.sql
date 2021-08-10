f816b48b249e9b7287204d75da19d50d886f4b4b
samuelmmjr <samuelmmjunior@gmail.com>
2021-08-10 10:05:50 -0300

    feat: req4 utilizaçao GroupBy

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,13 @@
+DROP VIEW IF EXISTS top_3_artistas;
+
+
 CREATE VIEW top_3_artistas AS
-SELECT a.artist AS `artista`, 
-COUNT (*) AS `seguidores`
-FROM SpotifyClone.following AS f
-INNER JOIN SpotifyClone.artist AS a ON f.artist_id = a.artist_id
+SELECT a.artist AS artista, 
+COUNT(*) AS seguidores
+FROM following f 
+INNER JOIN artist a ON f.artist_id = a.artist_id
+GROUP BY artista
 ORDER BY seguidores DESC, artista
 LIMIT 3;
+
+SELECT * FROM top_3_artistas;
