424bcdf62a7bd2d55e3588b39b142d769b7f1f11
Stanrley Barbosa <3011stan@gmail.com>
2021-08-10 14:59:08 -0300

    Req 7 fix

--- desafio7.sql
+++ desafio7.sql
@@ -5,7 +5,7 @@ CREATE VIEW perfil_artistas AS (
   FROM SpotifyClone.artistas AS a
   JOIN SpotifyClone.albuns AS al
   JOIN SpotifyClone.seguindo AS s
-  ON (a.artista_id = s.artista_id) AND (al.artista_id = al.artista_id)
+  ON (a.artista_id = s.artista_id) AND (al.artista_id = a.artista_id)
   GROUP BY al.album, a.artista
   ORDER BY COUNT(s.usuario_id) DESC, a.artista ASC, al.album ASC
 );
