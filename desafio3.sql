56cc147105b0e7db4145c9c7d7100f5117f278df
Derik <andradederik1@gmail.com>
2021-08-10 20:00:49 -0300

    requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+CREATE VIEW historico_reproducao_usuarios AS
+  SELECT
+    us.nome AS usuario,
+    canc.cancao AS nome
+FROM SpotifyClone.usuario AS us
+INNER JOIN SpotifyClone.historico_reproducoes AS his
+ON us.usuario_id = his.usuario_id
+INNER JOIN SpotifyClone.cancoes AS canc
+ON canc.cancao_id = his.cancao_id
+ORDER BY usuario, nome
+;
