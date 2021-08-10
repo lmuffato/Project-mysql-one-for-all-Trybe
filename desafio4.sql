cb5165d2031155d0b3b94bf4046c51b049a49dcf
samuelmmjr <samuelmmjunior@gmail.com>
2021-08-10 10:09:17 -0300

    feat: req4(Salvando alteraçoes) && Fix:req5

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,3 @@
-DROP VIEW IF EXISTS top_3_artistas;
-
-
 CREATE VIEW top_3_artistas AS
 SELECT a.artist AS artista, 
 COUNT(*) AS seguidores
@@ -9,5 +6,3 @@ INNER JOIN artist a ON f.artist_id = a.artist_id
 GROUP BY artista
 ORDER BY seguidores DESC, artista
 LIMIT 3;
-
-SELECT * FROM top_3_artistas;
