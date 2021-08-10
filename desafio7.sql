400f4b3e50bcb0bddaaff4f1943b430818fdc598
Antônio Schappo <antonioschappo@gmail.com>
2021-08-10 11:16:53 -0300

    correct code climate challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,7 @@
 CREATE VIEW perfil_artistas AS
 SELECT t1.artista AS artista, t2.album as album,
-(
-	SELECT COUNT(t3.artista_id) FROM seguindo AS t3
-    WHERE t3.artista_id = t1.artista_id
-) AS seguidores
+(SELECT COUNT(t3.artista_id) FROM seguindo AS t3
+WHERE t3.artista_id = t1.artista_id) AS seguidores
 FROM artistas AS t1
 INNER JOIN albuns AS t2
 ON
