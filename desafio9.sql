17919e9cb76c5eb49d5b0da4fa92eec535a92b88
Walvesb65 <willian.alvesb65@gmail.com>
2021-08-10 13:58:04 -0300

    Fix: Ajustando desafio 9 identação Order by ASC

--- desafio9.sql
+++ desafio9.sql
@@ -1,15 +1,15 @@
-CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(50))
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(100))
 BEGIN
   SELECT
-    artists.artista AS artista,
-    artists.usuario AS artista,
-    albuns.album AS album
-  FROM
-    SpotifyClone.artista AS artists
-    INNER JOIN
-    SpotifyClone.albuns AS albuns
-    ON albuns.artista_id = artists.artista_id
-  WHERE artists.artista = artista_id
-  ORDER BY artista_id ASC;
+    art.artista AS artista,
+    alb.album AS album
+    FROM SpotifyClone.album alb
+    INNER JOIN SpotifyClone.artista art
+    ON alb.artista_id = art.artista_id
+    WHERE art.artista = nome_artista
+    ORDER BY album ASC;
 END $$
+
 DELIMITER ;
