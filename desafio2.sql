289e7707833025d79f867f03466437c25dc075b4
ScriptCamilo <script.camilo@gmail.com>
2021-08-10 08:20:07 -0300

    feat: second requirement done

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+CREATE VIEW estatisticas_musicais AS
+  SELECT COUNT(song_id) AS cancoes,
+  (
+    SELECT COUNT(artist_id)
+    FROM SpotifyClone.artists
+  ) AS artistas,
+  (
+    SELECT COUNT(album_id)
+    FROM SpotifyClone.albums
+  ) AS albuns
+  FROM SpotifyClone.songs;
+  
