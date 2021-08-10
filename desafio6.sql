20c17d24c468ab19f1becc210d24ceb12a4f9537
Stanrley Barbosa <3011stan@gmail.com>
2021-08-10 12:02:20 -0300

    Fix decimals req 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT MIN(p.valor_plano) AS 'faturamento_minimo',
 MAX(p.valor_plano) AS 'faturamento_maximo',
-ROUND(AVG(p.valor_plano), 2) AS 'faturamento_medio',
+-- https://stackoverflow.com/questions/29677347/how-to-show-decimals-in-sql-instead-of-0
+CONVERT(ROUND(AVG(p.valor_plano), 2), DECIMAL(10,2)) AS 'faturamento_medio',
 SUM(p.valor_plano) AS 'faturamento_total'
 FROM SpotifyClone.planos AS p
 JOIN SpotifyClone.usuarios AS u
