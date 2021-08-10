7563ea921a86d8296bdcc5e7223d674ff8ced8bf
William <w.willers@outlook.com>
2021-08-09 22:58:20 -0400

    Desafio 8 teste

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+CREATE TRIGGER trigger_usuario_delete
+  BEFORE DELETE ON SpotifyClone.users
+  FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.listening_history WHERE user_id=OLD.user_id;
+  DELETE FROM SpotifyClone.followed_artists WHERE user_id=OLD.user_id;
+END $$
+
+DELIMITER ;
