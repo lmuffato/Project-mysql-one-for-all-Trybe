302eb88e1955da7628b3de6271a7de13747a837e
vinicios <soicinivt@gmail.com>
2021-08-10 03:17:07 -0300

    Desafio 6 - Create a view faturamento_atual.

---
+++ desafio6.sql
@@ -0,0 +1,5 @@
+CREATE VIEW faturamento_atual AS
+SELECT MIN(valor_plano) AS faturamento_minimo,
+MAX(valor_plano) AS faturamento_maximo,
+ROUND((SUM(valor_plano)/4), 2)AS faturamento_medio,
+SUM(valor_plano)AS faturamento_total FROM SpotifyClone.planos;
