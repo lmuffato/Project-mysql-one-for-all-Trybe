c91ef067a2afd5b9f1495db0695c999034d700e0
Stanrley Barbosa <3011stan@gmail.com>
2021-08-10 15:33:47 -0300

    Spotifyclone to SpotifyClone[fix]

--- desafio9.sql
+++ desafio9.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(100))
 BEGIN
   SELECT a.artista AS artista, al.album AS album
   FROM SpotifyClone.artistas as a
-  INNER JOIN Spotifyclone.albuns as al
+  INNER JOIN SpotifyClone.albuns as al
   ON a.artista_id = al.artista_id
   AND a.artista = nome
   ORDER BY album;
