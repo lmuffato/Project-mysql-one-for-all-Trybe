06382b59f33b03d4f57247e22f69d9113864cc7f
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-08-10 15:20:05 -0300

    3rd requirement linter fix

--- desafio3.sql
+++ desafio3.sql
@@ -3,7 +3,7 @@ USE SpotifyClone;
 DROP VIEW IF EXISTS SpotifyClone.historico_reproducao_usuarios;
 
 CREATE VIEW historico_reproducao_usuarios AS
-	SELECT su.usuario usuario, ss.cancoes nome  FROM SpotifyClone.Songs_Played sp
+SELECT su.usuario usuario, ss.cancoes nome  FROM SpotifyClone.Songs_Played sp
     INNER JOIN SpotifyClone.Users su
     ON su.usuario_id = sp.usuario_id
     INNER JOIN SpotifyClone.Songs ss
