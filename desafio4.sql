9d778f725aa40111cd40b8a5e801067f30820499
Walvesb65 <willian.alvesb65@gmail.com>
2021-08-10 11:01:07 -0300

    Feat: desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+
+SELECT
+  art.artista AS `artista`,
+  COUNT(*) AS `seguidores`
+FROM SpotifyClone.artista art
+INNER JOIN SpotifyClone.seguindo s
+  ON art.artista_id = s.artista_id
+GROUP BY `artista`
+ORDER BY `seguidores` DESC, `artista` LIMIT 3;
