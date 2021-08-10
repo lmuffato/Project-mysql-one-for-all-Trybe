defd515e3ae4ccd60300164d9f417110dccd780c
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-10 00:04:39 -0300

    desafio 9 test/mais correções(desespero)

--- desafio9.sql
+++ desafio9.sql
@@ -1,13 +1,13 @@
 USE SpotifyClone;
 
 DELIMITER $$
-CREATE PROCEDURE albuns_do_artista(IN artist varchar(70))
+CREATE PROCEDURE albuns_do_artista(IN artist_name varchar(70))
 BEGIN
 SELECT arts.artist_name AS 'artista',
 albs.albuns_name AS 'album'
 FROM artists  arts
 INNER JOIN albuns  albs
-ON albs.artist_id = arts.artist_id AND artist = arts.artist_name
+ON albs.artist_id = arts.artist_id AND artist_name = arts.artist_name
 ORDER BY `album `ASC;
 END $$
 DELIMITER ;
