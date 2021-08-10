1cfb90a9cd97ce37b6a6f6fd28697b7461a8bae6
Stanrley Barbosa <3011stan@gmail.com>
2021-08-10 11:34:41 -0300

    Req 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+SELECT MIN(p.valor_plano) AS 'faturamento_minimo',
+MAX(p.valor_plano) AS 'faturamento_maximo',
+ROUND(AVG(p.valor_plano), 2) AS 'faturamento_medio',
+SUM(p.valor_plano) AS 'faturamento_total'
+FROM SpotifyClone.planos AS p
+JOIN SpotifyClone.usuarios as u
+ON p.plano_id = u.plano_id;
