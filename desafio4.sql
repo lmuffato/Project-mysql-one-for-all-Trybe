6dd4e21450f27f77cd788e0159da22f51f74d534
vinicios <soicinivt@gmail.com>
2021-08-10 02:52:42 -0300

    DEsafio 4 - Create a view that show columns artistas and seguidores with artist name and the followers.

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT t1.artista AS artista,
+COUNT(t3.usuario) AS seguidores FROM SpotifyClone.artistas AS t1
+INNER JOIN SpotifyClone.seguindoArtistas AS t2
+INNER JOIN SpotifyClone.usuarios AS t3
+ON t1.artista_id = t2.artista_id AND t3.usuario_id = t2.usuario_id
+GROUP BY artista
+ORDER BY seguidores DESC, artista;
