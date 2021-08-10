9620f60f15a8f5c56b2e21b1dcabdbf59ddc0a0e
andy silva <andssilva.23@gmail.com>
2021-08-10 18:56:55 -0400

    feat(req 10): :rocket: challenge 10

---
+++ desafio10.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantidade_musicas INT;
+SELECT
+COUNT(*)
+FROM SpotifyClone.historico_reproducao
+WHERE usuario_id = id
+INTO quantidade_musicas;
+RETURN quantidade_musicas;
+END $$
+
+DELIMITER ;
+
+-- SELECT quantidade_musicas_no_historico(3);
--- desafio9.sql
+++ desafio9.sql
@@ -1,15 +1,15 @@
 DELIMITER $$
-
 CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(100))
 BEGIN
-  SELECT
-    art.artista AS `artista`,
-    alb.album AS `album`
-    FROM SpotifyClone.album alb
-    INNER JOIN SpotifyClone.artista art
-    ON alb.artista_id = art.artista_id
-    WHERE art.artista = nome_artista
-    ORDER BY `album` ASC;
+SELECT
+art.artista AS `artista`,
+alb.album AS `album`
+FROM SpotifyClone.album alb
+INNER JOIN SpotifyClone.artista art
+ON alb.artista_id = art.artista_id
+WHERE art.artista = nome_artista
+ORDER BY `album` ASC;
 END $$
-
 DELIMITER ;
+
+-- CALL albuns_do_artista("Walter Phoenix");
