9c20fcb62a1ed9971fb781d447bef5fbb59bf09b
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-08-10 16:44:57 -0300

    8rd requirement new test

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,16 @@
-DROP TRIGGER IF EXISTS trigger_usuario_delete;
-
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON SpotifyClone.Users
+BEFORE DELETE ON SpotifyClone.Users
 FOR EACH ROW
 BEGIN
-    DELETE FROM SpotifyClone.Followers WHERE SpotifyClone.Users.usuario_id = SpotifyClone.Followers.usuario_id;
-    DELETE FROM SpotifyClone.Songs_Played WHERE SpotifyClone.Songs_Played.usuario_id = SpotifyClone.Users = usuario_id;
+    DELETE FROM SpotifyClone.Songs_Played
+    WHERE OLD.usuario_id = usuario_id;
+
+    DELETE FROM SpotifyClone.Followers
+    WHERE OLD.usuario_id = usuario_id;
 END $$
 
 DELIMITER $$ ;
+
+DELETE FROM SpotifyClone.Users WHERE usuario_id = 1
