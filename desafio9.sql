624244c3ed5da0562a94736e38cbed9a1b935af2
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-09 23:57:07 -0300

    desafio 9 test/mais correções(uma única letra)

--- desafio9.sql
+++ desafio9.sql
@@ -6,7 +6,7 @@ BEGIN
 SELECT arts.artist_name AS 'artista',
 albs.albuns_name AS 'album'
 FROM artists  arts
-INNER JOIN albums  albs
+INNER JOIN albuns  albs
 ON albs.artist_id = arts.artist_id AND artist = arts.artist_name
 ORDER BY `album `ASC;
 END $$
