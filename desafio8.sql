68224084c39e28f246fecb7412842b9f45a8acdd
Antônio Schappo <antonioschappo@gmail.com>
2021-08-10 11:44:53 -0300

    third try challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON SpotifyClone.usuarios FOR EACH ROW
+BEFORE DELETE ON SpotifyClone.usuarios FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.seguindo AS t1 WHERE t1.usuario_id = OLD.usuario_id
-DELETE FROM SpotifyClone.historico AS t3 WHERE t3.usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo WHERE usuario_id = OLD.usuario_id
+DELETE FROM SpotifyClone.historico AS usuario_id = OLD.usuario_id;
 END $$
 DELIMITER;
