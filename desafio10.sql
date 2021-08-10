04ee475740656758c3140cfe3681fd879e78eff3
William <w.willers@outlook.com>
2021-08-10 01:07:24 -0400

    Correção sintaxe desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(index int)
+CREATE FUNCTION quantidade_musicas_no_historico(indice int)
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE count_songs INT;
-  SELECT COUNT(*) FROM SpotifyClone.listening_history WHERE user_id=index INTO count_songs;
+  SELECT COUNT(*) FROM SpotifyClone.listening_history WHERE user_id=indice INTO count_songs;
   RETURN count_songs;
 END $$
 
