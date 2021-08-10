07c6e44734c58e1828a39496842e61e84ec543e6
Stanrley Barbosa <3011stan@gmail.com>
2021-08-10 12:08:01 -0300

    Number/string

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ SELECT MIN(p.valor_plano) AS 'faturamento_minimo',
 MAX(p.valor_plano) AS 'faturamento_maximo',
 -- https://stackoverflow.com/questions/29677347/how-to-show-decimals-in-sql-instead-of-0
 CONVERT(ROUND(AVG(p.valor_plano), 2), DECIMAL(10,2)) AS 'faturamento_medio',
-SUM(p.valor_plano) AS 'faturamento_total'
+CONVERT(SUM(p.valor_plano), DECIMAL(10,2)) AS 'faturamento_total'
 FROM SpotifyClone.planos AS p
 JOIN SpotifyClone.usuarios AS u
 ON p.plano_id = u.plano_id;
