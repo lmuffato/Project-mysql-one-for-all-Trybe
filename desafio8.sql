503ef2c5bd9fc275650870fac1f8ef8dcfd8a1c2
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-09 23:29:25 -0300

    desafio 8 correção

--- desafio8.sql
+++ desafio8.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON user
 FOR EACH ROW
 BEGIN
-DELETE FROM artist_follows WHERE user_id = OLD.user_id;
+DELETE FROM artists_follows WHERE user_id = OLD.user_id;
 DELETE FROM history WHERE user_id = OLD.user_id;
 END $$
 DELIMITER ;
