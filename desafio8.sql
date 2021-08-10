fc1f54319248957bc55f91bb74c3b9d0a9d8a9f7
andy silva <andssilva.23@gmail.com>
2021-08-10 11:55:18 -0400

    feat(req 8): :rocket: challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico_reproducao
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo
+WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
+
+/* DELETE FROM SpotifyClone.usuario
+WHERE usuario = "Thati"; */
