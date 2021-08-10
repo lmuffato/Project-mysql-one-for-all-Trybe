4b5abac54fc72424283a3a689899511c48c96896
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-09 23:07:24 -0300

    desafio 7 correção nos nomes

--- desafio7.sql
+++ desafio7.sql
@@ -6,6 +6,6 @@ CREATE View perfil_artistas as (
   inner join SpotifyClone.artists as arts
   inner join SpotifyClone.albuns as alns
   on (arts.artist_id = artf.artist_id) and (artf.artist_id = alns.artist_id)
-  group by `artista`, `albuns`
+  group by `artista`, `album`
   order by `seguidores` desc, `artista`
 );
