d76a452ed8fdf265bf8f782d75c61accdf52c65b
Sabrina <sasa2605.alves@gmail.com>
2021-08-11 08:33:46 -0300

    Requirements 3 and 5

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,12 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
-u.usuario_name AS `usuario`,
-c.cancao AS `nome`
+c.cancao AS `nome`,
+u.usuario_name AS `usuario`
 FROM SpotifyClone.historico_de_reproducoes AS h
 INNER JOIN SpotifyClone.usuarios AS u
 ON h.usuario_ID = u.usuario_ID
 INNER JOIN SpotifyClone.cancoes AS c
 ON h.cancoes_ID = c.cancoes_ID
 ORDER BY `usuario`, `nome` ASC;
+-- DROP VIEW  historico_reproducao_usuarios;
+-- SELECT * FROM historico_reproducao_usuarios;
--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,10 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-MIN(valor_plano) AS `faturamento_minimo`,
 MAX(valor_plano) AS `faturamento_maximo`,
-AVG(valor_plano) AS `faturamento_medio`,
+FORMAT(ROUND(AVG(valor_plano), 2), 2),
+MIN(valor_plano) AS `faturamento_minimo`,
 SUM(valor_plano) AS `faturamento_total`
-FROM SpotifyClone.planos;
+-- FROM SpotifyClone.planos;
+-- drop view faturamento_atual;
+-- SELECT*FROM faturamento_atual;
+-- SELECT AVG(valor_plano) FROM SpotifyClone.planos;
