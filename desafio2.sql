84c3809a2965e372e244c17dbb5c12d859a3452c
jepaur5 <jepaur5@gmail.com>
2021-08-11 05:23:05 -0300

    requisito2

---
+++ desafio2.sql
@@ -0,0 +1,13 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT 
+        COUNT(*) AS `cancoes`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.artists) AS `artistas`,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.albums) AS `albuns`
+    FROM
+        SpotifyClone.songs;
