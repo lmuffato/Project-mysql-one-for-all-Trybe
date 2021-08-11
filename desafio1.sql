16a458f59c9c6ccf57809dac03c78cb276817b97
jepaur5 <jepaur5@gmail.com>
2021-08-11 04:30:34 -0300

    requisito1-corrigindo2

--- desafio1.sql
+++ desafio1.sql
@@ -67,65 +67,65 @@ VALUES ('gratuito', 0),
 
 INSERT INTO `users` (`name`, `age`, `plan_id`) 
 VALUES ('Thati', 23, 1),
-       ('Cintia', 35, 3),
-       ('Bill', 20, 2),
-       ('Roger', 45, 1);
+        ('Cintia', 35, 3),
+        ('Bill', 20, 2),
+        ('Roger', 45, 1);
 
 INSERT INTO `artists` (`name`) 
 VALUES ('Walter Phoenix'),
-       ('Peter Strong'),
-       ('Lance Day'),
-       ('Freedie Shannon');
+        ('Peter Strong'),
+        ('Lance Day'),
+        ('Freedie Shannon');
 
 INSERT INTO `albums` (`name`, `artist_id`) 
 VALUES ('Envious', 1),
-       ('Exuberant', 1),
-       ('Hallowed Steam', 2),
-       ('Incandescent', 3),
-       ('Temporary Culture', 4);
+        ('Exuberant', 1),
+        ('Hallowed Steam', 2),
+        ('Incandescent', 3),
+        ('Temporary Culture', 4);
 
 INSERT INTO `songs` (`name`, `album_id`) 
 VALUES ('Soul For Us', 1),
-       ('Reflections Of Magic', 1),
-       ('Dance With Her Own', 1),
-       ('Troubles Of My Inner Fire', 2),
-       ('Time Fireworks', 2),
-       ('Magic Circus', 3),
-       ('Honey, So Do I', 3),
-       ('Sweetie, Let\'s Go Wild', 3),
-       ('She Knows', 3),
-       ('Fantasy For Me', 4),
-       ('Celebration Of More', 4),
-       ('Rock His Everything', 4),
-       ('Home Forever', 4),
-       ('Diamond Power', 4),
-       ('Honey, Let\'s Be Silly', 4),
-       ('Thang Of Thunder', 5),
-       ('Words Of Her Life', 5),
-       ('Without My Streets', 5);
+        ('Reflections Of Magic', 1),
+        ('Dance With Her Own', 1),
+        ('Troubles Of My Inner Fire', 2),
+        ('Time Fireworks', 2),
+        ('Magic Circus', 3),
+        ('Honey, So Do I', 3),
+        ('Sweetie, Let\'s Go Wild', 3),
+        ('She Knows', 3),
+        ('Fantasy For Me', 4),
+        ('Celebration Of More', 4),
+        ('Rock His Everything', 4),
+        ('Home Forever', 4),
+        ('Diamond Power', 4),
+        ('Honey, Let\'s Be Silly', 4),
+        ('Thang Of Thunder', 5),
+        ('Words Of Her Life', 5),
+        ('Without My Streets', 5);
 
 INSERT INTO `following_artists` (`user_id`, `artist_id`) 
 VALUES (1, 1),
-       (1, 4),
-       (1, 3),
-       (2, 1),
-       (2, 3),
-       (3, 2),
-       (3, 1),
-       (4, 4);
+        (1, 4),
+        (1, 3),
+        (2, 1),
+        (2, 3),
+        (3, 2),
+        (3, 1),
+        (4, 4);
 
 INSERT INTO `historical_reproduction` (`user_id`, `song_id`) 
 VALUES (1, 1),
-       (1, 6),
-       (1, 14),
-       (1, 16),
-       (2, 13),
-       (2, 17),
-       (2, 2),
-       (2, 15),
-       (3, 4),
-       (3, 16),
-       (3, 6),
-       (4, 3),
-       (4, 18),
-       (4, 11);
+        (1, 6),
+        (1, 14),
+        (1, 16),
+        (2, 13),
+        (2, 17),
+        (2, 2),
+        (2, 15),
+        (3, 4),
+        (3, 16),
+        (3, 6),
+        (4, 3),
+        (4, 18),
+        (4, 11);
