9d93a6fb778c9660216822f38e20eb17402749fd
Erick-Marinho <erickgaves@gmail.com>
2021-08-12 16:56:28 -0300

    Refatorando desafio 2, mudando funcao de agregacao

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,15 @@
 CREATE VIEW estatisticas_musicais AS
     SELECT
-        SUM(s.song) cancoes,
-        SUM(a.name) artistas,
-        SUM(albums) albuns
-    FROM
-        spotifyClone.songs s
-    INNER JOIN
-        spotifyClone.artists a USING (song_id)
-    INNER JOIN
-        spotifyClone.albums al USING (song_id);
+        (SELECT
+            COUNT(*)
+        FROM
+            SpotifyClone.songs) AS cancoes,
+        (SELECT
+            COUNT(*)
+        FROM
+            SpotifyClone.artists) AS artistas,
+        (SELECT
+            COUNT(*)
+        FROM
+            SpotifyClone.albums) AS albuns;
+      
