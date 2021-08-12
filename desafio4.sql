55fde650cefb8e3f7ae15efe94a361170e4f703b
Erick-Marinho <erickgaves@gmail.com>
2021-08-12 20:34:20 -0300

    renomeando coluna

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,13 @@
-CREATE VIEW top_3_artistas AS
-    SELECT a.artist_name artista,
+CREATE VIEW top_3_artistas AS (
+    SELECT a.name artista,
     COUNT(f.user_id) seguidores
-FROM 
-    SpotifyClone.follow_artist f
-INNER JOIN SpotifyClone.artists a 
-USING (artist_id) 
-GROUP BY 
-    a.artist_id
-ORDER BY 
-    2 DESC, 1
-LIMIT 3;
+    FROM 
+        SpotifyClone.follow_artist f
+    INNER JOIN SpotifyClone.artists a 
+        USING (artist_id) 
+    GROUP BY 
+        a.artist_id
+    ORDER BY 
+        2 DESC, 1
+    LIMIT 3
+);
