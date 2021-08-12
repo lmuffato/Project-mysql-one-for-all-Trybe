4a10e24933382f1a4729fb04b4a552269cb2af0c
Erick-Marinho <erickgaves@gmail.com>
2021-08-12 17:51:28 -0300

    Refatorando desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -5,8 +5,10 @@ CREATE VIEW historico_reproducao_usuarios AS
     FROM 
         SpotifyClone.history_reproductions hr
     INNER JOIN 
-        SpotifyClone.users u USING (user_id)
+        SpotifyClone.users u 
+    ON hr.user_id = u.user_id 
     INNER JOIN 
-        SpotifyClone.songs s USING (song_id)
+        SpotifyClone.songs s 
+    ON hr.song_id = s.song_id
     ORDER BY 
         1, 2;
