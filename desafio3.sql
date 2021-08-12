fff17c3bf28d8be60712413ec367b71a5ba7f05f
Erick-Marinho <erickgaves@gmail.com>
2021-08-12 17:30:30 -0300

    Corrigindo identação

--- desafio3.sql
+++ desafio3.sql
@@ -2,11 +2,11 @@ CREATE VIEW historico_reproducao_usuarios AS
     SELECT 
         u.user usuario, 
         s.song nome
-FROM 
-    SpotifyClone.history_reproductions hr
-INNER JOIN 
-    SpotifyClone.users u USING (user_id)
-INNER JOIN 
-    SpotifyClone.songs s USING (song_id)
-ORDER BY 
-    1, 2;
+    FROM 
+        SpotifyClone.history_reproductions hr
+    INNER JOIN 
+        SpotifyClone.users u USING (user_id)
+    INNER JOIN 
+        SpotifyClone.songs s USING (song_id)
+    ORDER BY 
+        1, 2;
