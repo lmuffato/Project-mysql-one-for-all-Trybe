306e9c3649ea9a6aa81733f038fd25bc1bf94437
Robertson <robertson.nunes@gmail.com>
2021-08-12 17:09:08 -0300

    Fixed Req 8

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,15 @@
 DELIMITER $$
 
-CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico (user_id INT)
+CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE quantidade_musicas_no_historico INT ;
-SELECT COUNT(1) AS quantidade_musicas_no_historico
-FROM SpotifyClone.History AS h
-WHERE h.user_id = user_id INTO quantidade_musicas_no_historico;
-RETURN quantidade_musicas_no_historico;
+DECLARE quantity INT;
+SELECT  COUNT(h.single_id) FROM SpotifyClone.Users AS u
+INNER JOIN SpotifyClone.History AS h
+ON h.user_id = u.user_id
+WHERE u.user_id = user_id
+GROUP BY u.username
+INTO quantity;
+RETURN quantity;
 END $$
-
 DELIMITER ;
--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,10 @@
 DELIMITER $$
-
-CREATE TRIGGER trigger_usuario_delete BEFORE DELETE ON SpotifyClone.Users
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.Users
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.History WHERE user_id = OLD.user_id;
-DELETE FROM SpotifyClone.Following WHERE user_id = OLD.user_id;
+DELETE FROM SpotifyClone.History WHERE OLD.user_id = user_id;
+DELETE FROM SpotifyClone.Following WHERE OLD.user_id = user_id;
 END $$
 
 DELIMITER ;
-
--- DELETE FROM SpotifyClone.Users WHERE username = 'Thati';
