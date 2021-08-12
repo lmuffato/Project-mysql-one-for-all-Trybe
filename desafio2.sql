9c358de774bef8a4f51ea78bf119c6a15fb998ac
Erick-Marinho <erickgaves@gmail.com>
2021-08-12 16:38:12 -0300

    Cumprindo desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+CREATE VIEW estatisticas_musicais AS
+    SELECT
+        SUM(s.song) cancoes,
+        SUM(a.name) artistas,
+        SUM(albums) albuns
+    FROM
+        spotifyClone.songs s
+    INNER JOIN
+        spotifyClone.artists a USING (song_id)
+    INNER JOIN
+        spotifyClone.albums al USING (song_id);
+    
