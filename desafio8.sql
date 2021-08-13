657d24a7cf084a616ed2d0a8ac3e9c21986370f2
lucas-chave <lcchavesr150@gmail.com>
2021-08-13 10:27:20 -0300

    fix challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -2,7 +2,7 @@
 
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-  AFTER DELETE ON usuarios
+  AFTER DELETE ON SpotifyClone.usuarios
   FOR EACH ROW
 BEGIN
   DELETE FROM SpotifyClone.historico_reproducoes WHERE usuario_id=OLD.usuario_id;
