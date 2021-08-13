f87a0ea225f447d3f0e61ad6957b0a8405448912
Erick-Marinho <erickgaves@gmail.com>
2021-08-12 21:00:39 -0300

    cumprindo desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON SpotifyClone.users
+    FOR EACH ROW
+BEGIN 
+    DELETE FROM SpotifyClone.history_reproductions
+    WHERE user_id = OLD.user_id;
+
+    DELETE FROM SpotifyClone.follow_artist
+    WHERE user_id = OLD.user_id;
+END 
+$$ DELIMITER ;
