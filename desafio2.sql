d9c6a608d53c71619b5e4df3b6030d4d125a459a
lucas-chave <lcchavesr150@gmail.com>
2021-08-13 01:05:32 -0300

    challenge 2

---
+++ desafio2.sql
@@ -0,0 +1,13 @@
+-- challenge 2;
+
+CREATE VIEW estatisticas_musicais AS
+  SELECT COUNT(cancao_id) AS cancoes,
+(
+  SELECT COUNT(artista_id) AS artistas
+  FROM SpotifyClone.artistas
+) AS artistas,
+(
+  SELECT COUNT(album_id) AS albuns
+  FROM SpotifyClone.albuns
+) AS albuns
+FROM SpotifyClone.cancoes;
