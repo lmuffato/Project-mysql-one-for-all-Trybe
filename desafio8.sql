6750c926517923521f34dbeb8e598a4d62533e5b
lucas-chave <lcchavesr150@gmail.com>
2021-08-13 10:11:09 -0300

    challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+-- challenge 8;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+  AFTER DELETE ON usuarios
+  FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.historico_reproducoes WHERE usuario_id=OLD.usuario_id;
+  DELETE FROM SpotifyClone.artistas_seguidos WHERE usuario_id=OLD;
+END $$
+
+DELIMITER ;
