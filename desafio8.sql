2ac8c0b0002b712f29f01068bebe9040a4793e51
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-08-13 23:25:25 -0300

    desafio 8 what????

--- desafio8.sql
+++ desafio8.sql
@@ -3,9 +3,9 @@ DELIMITER $$
 CREATE TRIGGER SpotifyClone.trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.playback_history
-  WHERE user_id = OLD.id;
   DELETE FROM SpotifyClone.followers
+  WHERE user_id = OLD.id;
+  DELETE FROM SpotifyClone.playback_history
   WHERE user_id = OLD.id;
 END $$
 
