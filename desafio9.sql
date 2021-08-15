641f5fe7e45b184eb571ccd22e7b1b0fecd97fb3
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-08-15 16:23:03 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN artist VARCHAR(50))
+BEGIN
+  SELECT
+    ar.artist AS `artista`,
+    al.album AS `album`
+  FROM SpotifyClone.artists AS ar
+  INNER JOIN SpotifyClone.albums AS al
+  ON ar.id = al.artist
+  ORDER BY `album` ASC;
+END $$
+
+DELIMITER ;
