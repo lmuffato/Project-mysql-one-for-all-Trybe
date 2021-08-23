a4ee39a5599d650a8e01151ac19da78f97be7e87
lmuffato <lmiremuffato@gmail.com>
2021-08-23 18:05:01 -0300

    requisit 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 /* 2. Crie uma VIEW chamada estatisticas_musicais que exiba três colunas: */
-USE SpotifyClone;
-CREATE VIEW top_10_customers AS
+-- USE SpotifyClone;
+CREATE VIEW estatisticas_musicais AS
   SELECT
     (SELECT COUNT(*) AS "cancoes" FROM songs),
     (SELECT COUNT(*) AS "artistas" FROM artists),
---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+/* 3. Crie uma VIEW chamada historico_reproducao_usuarios.
+Essa VIEW deverá ter apenas duas colunas:
+1. A primeira coluna deve possuir o alias "usuario" e exibir o nome
+da pessoa usuária.
+2. A segunda coluna deve possuir o alias "nome" e exibir o nome da
+canção ouvida pela pessoa com base no seu histórico de reprodução. */
+
+/*USE SpotifyClone;
+SELECT * FROM historic;*/
