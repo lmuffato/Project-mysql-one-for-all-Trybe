e80a2668c856cb9f0bea7ee2c3898c9767a41f28
Lucas Pedroso <lepsk8@gmail.com>
2021-08-23 22:01:17 -0300

    fix: desafio2 and desafio9

--- desafio9.sql
+++ desafio9.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(45))
 BEGIN
   SELECT
     ar.artist AS artista,
-    al.albumn AS `albumn`
+    al.albumn AS `album`
   FROM SpotifyClone.albumns al
   INNER JOIN SpotifyClone.artists ar ON al.artist_id = ar.artist_id
   WHERE artist_name = ar.artist
-  ORDER BY `albumn`;
+  ORDER BY `album`;
 END $$;
 
 DELIMITER $$;
