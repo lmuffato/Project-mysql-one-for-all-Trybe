6c4d35eef54f21ad185f4b2d544a5a2384e4fce1
lmuffato <lmiremuffato@gmail.com>
2021-08-23 21:30:00 -0300

    requisit 8

--- desafio8.sql
+++ desafio8.sql
@@ -4,3 +4,19 @@ dados, refletindo essa exclusão em todas as tabelas que ela estiver.
 
 este a funcionalidade correta de sua trigger, fazendo a exclusão
 da usuária "Thati".*/
+-- USE SpotifyClone;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON users
+    FOR EACH ROW
+BEGIN
+    DELETE FROM following WHERE user_id = OLD.user_id,
+    DELETE FROM historic WHERE user_id = OLD.user_id
+END $$
+DELIMITER;
+
+/* Teste do trigger */
+-- SET SQL_SAFE_UPDATES = 0;
+-- DELETE FROM users WHERE user_name = "Thati";
+-- SELECT * FROM users;
+-- SELECT * FROM historic;
