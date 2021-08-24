d919cd901d21f38fdfa49df85561fd1549893cdc
Lucas Pedroso <lepsk8@gmail.com>
2021-08-23 22:07:19 -0300

    fix: desafio2 albums for albuns

--- desafio2.sql
+++ desafio2.sql
@@ -2,5 +2,5 @@ CREATE VIEW estatisticas_musicais as (
   SELECT
     (SELECT COUNT(*) FROM SpotifyClone.songs) as cancoes,
     (SELECT COUNT(*) FROM SpotifyClone.artists) as artistas,
-    (SELECT COUNT(*) FROM SpotifyClone.albums) as albums
+    (SELECT COUNT(*) FROM SpotifyClone.albumns) as albuns
 );
