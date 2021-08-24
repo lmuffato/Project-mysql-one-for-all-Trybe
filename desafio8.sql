b6542f3fc4d131ffc1a8eee061618c703e5639a5
lmuffato <lmiremuffato@gmail.com>
2021-08-23 21:12:19 -0300

    requisit 6

---
+++ desafio8.sql
@@ -0,0 +1,6 @@
+/* Desafio 8 - Crie uma trigger chamada trigger_usuario_delete que deve
+ser disparada sempre que uma pessoa usuária for excluída do banco de
+dados, refletindo essa exclusão em todas as tabelas que ela estiver.
+
+este a funcionalidade correta de sua trigger, fazendo a exclusão
+da usuária "Thati".*/
