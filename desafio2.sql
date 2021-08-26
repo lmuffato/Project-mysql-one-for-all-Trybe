4d17aedddec5d6cb984011d76965530b85172afe
nyxnil <nsmnilsonn@gmail.com>
2021-08-26 19:32:42 -0300

    desafio 2, 3, 4(test) correcao CC

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 create view estatisticas_musicais as
 select count(distinct s.song_id) cancoes,
-	count(distinct a.artist_id) artistas,
-    count(distinct al.album_id) albuns
+count(distinct a.artist_id) artistas,
+count(distinct al.album_id) albuns
 from SpotifyClone.songs s, SpotifyClone.artists a, SpotifyClone.albuns al;
