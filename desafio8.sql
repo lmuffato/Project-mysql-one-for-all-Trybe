ac2cc839641eaf89ac47ebcc624c9c83fe796b15
Southier <patrick.southier@hotmail.com>
2021-08-26 22:45:22 -0300

    fix: errors pointed out by Code Climate

--- desafio8.sql
+++ desafio8.sql
@@ -4,9 +4,9 @@ BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.followers
-	WHERE (user_id = OLD.user_id);
+WHERE (user_id = OLD.user_id);
 DELETE FROM SpotifyClone.playback_history
-	WHERE (user_id = OLD.user_id);
+WHERE (user_id = OLD.user_id);
 END $$
 DELIMITER ;
 
