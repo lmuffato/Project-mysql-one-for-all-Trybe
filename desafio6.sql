dedc15477db198929f578351c1dd51c43aafc753
nyxnil <nsmnilsonn@gmail.com>
2021-08-27 12:11:41 -0300

    desafio 6, correcao CC

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 create view faturamento_atual as
 select min(p.price) faturamento_minimo,
-	max(p.price) faturamento_maximo,
-  round(avg(p.price),2) faturamento_medio,
-  sum(p.price) faturamento_total
+max(p.price) faturamento_maximo,
+round(avg(p.price),2) faturamento_medio,
+sum(p.price) faturamento_total
 from users u
 join paymeds p on u.paymed_id = p.paymed_id;
