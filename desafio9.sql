e663c32b5715c1abe3fdd60989958da5ef0ec1ec
nyxnil <nsmnilsonn@gmail.com>
2021-08-27 20:55:13 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+delimiter $$
+create procedure albuns_do_artista(in nome_artist varchar(50))
+begin
+select a.artist artista,
+al.album album
+from albuns al
+join artists a on al.artist_id = a.artist_id
+where a.artist like nome_artist
+group by album;
+end $$
+delimiter ;
+
+call albuns_do_artista("Walter Phoenix");
