d83a19b6ce4f1ba4a9a009c54bbf73a5b83a5d98
Southier <patrick.southier@hotmail.com>
2021-08-26 23:58:14 -0300

    feat: completes requirements 9 and 10

---
+++ desafio10.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(id_usuario INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE musics_on_history INT;
+SELECT COUNT(p.music_id)
+FROM SpotifyClone.playback_history AS p
+WHERE p.user_id = id_usuario
+INTO musics_on_history;
+RETURN musics_on_history;
+END $$
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
