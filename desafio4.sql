d10dc6b5cd99876ab24a90cbc1d0b95393701f04
nyxnil <nsmnilsonn@gmail.com>
2021-08-27 12:48:10 -0300

    desafio 7, modificacao no group by, desafio 4 refatorado join com users nao necessario.

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,4 @@
 create view top_3_artistas as
-select a.artist artista, count(u.user_id) seguidores from following_artists f
+select a.artist artista, count(f.user_id) seguidores from following_artists f
 join artists a on f.artist_id = a.artist_id
-join users u on f.user_id = u.user_id
 group by artista order by seguidores desc, artista limit 3;
--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,8 @@
 create view perfil_artistas as
 select a.artist artista,
 al.album album,
-count(u.user_id) seguidores
+count(f.user_id) seguidores
 from following_artists f
 join artists a on f.artist_id = a.artist_id
 join albuns al on f.artist_id = al.artist_id
-join users u on f.user_id = u.user_id
-group by album order by seguidores desc, artista, album;
+group by album, artista order by seguidores desc, artista, album;
