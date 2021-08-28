b2e37f0767767cb7681705939c82476cba629a2e
Ederson S <edersonrodrigo31@gmail.com>
2021-08-28 17:23:35 -0300

    feat: requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT
+    ROUND(MIN(p.Value),
+    2) AS 'faturamento_minimo',
+    ROUND(MAX(p.Value),
+    2) AS 'faturamento_maximo',
+    ROUND(avg(p.Value),
+    2) AS 'faturamento_medio',
+    ROUND(SUM(p.Value),
+    2) AS 'faturamento_total' 
+FROM
+    SpotifyClone.users AS u 
+INNER JOIN
+    SpotifyClone.plans AS p 
+        ON p.id = u.PlanId;
