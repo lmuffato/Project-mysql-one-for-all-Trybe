e835409f9b3126f39d80ecdfb8a96d48b3e2d2ef
Ederson S <edersonrodrigo31@gmail.com>
2021-08-28 16:10:47 -0300

    fix: requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,14 @@
 USE SpotifyClone;
-
-CREATE VIEW estatisticas_musicais AS
-	SELECT
-	(SELECT COUNT(id) FROM songs) AS cancoes,
-	(SELECT COUNT(id) FROM artists) AS artistas,
-	(SELECT COUNT(id) FROM albuns) AS albuns;
+CREATE VIEW estatisticas_musicais AS  SELECT
+    (SELECT
+        COUNT(id) 
+    FROM
+        songs) AS cancoes,
+    (SELECT
+        COUNT(id) 
+    FROM
+        artists) AS artistas,
+    (SELECT
+        COUNT(id) 
+    FROM
+        albuns) AS albuns;
