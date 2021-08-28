8ff5eba26db0fa6ab7a288191b7c632a19d1d60e
Ederson S <edersonrodrigo31@gmail.com>
2021-08-28 17:38:50 -0300

    feat: requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,20 @@
+SELECT
+    art.name AS 'artista',
+    alb.Name AS 'album',
+    COUNT(f.UserId) AS 'seguidores'  
+FROM
+    SpotifyClone.albuns AS alb 
+INNER JOIN
+    SpotifyClone.artists AS art 
+        ON art.id = alb.ArtistId 
+INNER JOIN
+    SpotifyClone.follow_artists AS f 
+        ON f.ArtistId = alb.ArtistId 
+GROUP BY
+    alb.Name,
+    art.name 
+ORDER BY
+    seguidores DESC,
+    artista,
+    album;
+    
