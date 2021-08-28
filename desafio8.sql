67c8706d5e35788a1aaf76830d5adc5a65537178
victor-castro <victor.castro@netbiis.com>
2021-08-28 15:28:44 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarios
+FOR EACH ROW
+
+BEGIN
+DELETE FROM seguindo_artistas
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM historico_de_reproducoes
+WHERE usuario_id = OLD.usuario_id;
+END
+
+DELIMITER ;
