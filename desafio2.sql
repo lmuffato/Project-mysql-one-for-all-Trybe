6d07046609400675b9ab6e212905e49b8fae10f8
Ederson S <edersonrodrigo31@gmail.com>
2021-08-28 16:07:46 -0300

    feat: requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,7 @@
+USE SpotifyClone;
+
+CREATE VIEW estatisticas_musicais AS
+	SELECT
+	(SELECT COUNT(id) FROM songs) AS cancoes,
+	(SELECT COUNT(id) FROM artists) AS artistas,
+	(SELECT COUNT(id) FROM albuns) AS albuns;
