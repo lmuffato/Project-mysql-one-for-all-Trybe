482289f688c2c37e620201df0f50648ec0eb0efd
Ederson S <edersonrodrigo31@gmail.com>
2021-08-28 17:11:07 -0300

    feat: requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,13 @@
+SELECT
+    s.Name as 'cancao',
+    COUNT(h.SongId) AS 'reproducoes' 
+FROM
+    SpotifyClone.user_history AS h 
+INNER JOIN
+    SpotifyClone.songs AS s 
+        ON s.id = h.SongId 
+GROUP BY
+    h.SongId 
+ORDER BY
+    reproducoes DESC,
+    cancao LIMIT 2;
