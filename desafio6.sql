93b840315a637e5cdba69763419011cdf2acbbda
edersonrdg <edersonrodrigo31@gmail.com>
2021-08-29 19:08:33 -0300

    fix: req 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW `SpotifyClone`.faturamento_atual AS
   SELECT
     MIN(`p`.Value) AS `faturamento_minimo`,
     MAX(`p`.Value) AS `faturamento_maximo`,
-    ROUND(SUM(`p`.Value) / COUNT (`users`.id), 2) AS `faturamento_medio`,
+    ROUND(SUM(`p`.Value) / COUNT (`u`.id), 2) AS `faturamento_medio`,
     SUM(`p`.Value) AS `faturamento_total`
   FROM
     `SpotifyClone`.plans AS `p`
