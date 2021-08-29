9db88e8a085e855f53670c0c401aba09f9670436
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-29 20:41:29 -0300

    Criaçao de VIEW (Desafio 2)

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+USE SpotifyClone;
+
+DROP VIEW IF EXISTS estatisticas_musicais;
+
+CREATE VIEW estatisticas_musicais AS 
+SELECT
+  (SELECT COUNT(cancao_id) FROM cancoes c) AS `cancoes`,
+  (SELECT COUNT(artista_id) FROM artistas a) AS `artistas`,
+  (SELECT COUNT(album_id) FROM albuns a2) AS `albuns`;
