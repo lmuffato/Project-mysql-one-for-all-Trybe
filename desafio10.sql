15a1d11fb981539d35d25979bd1fe996b40a55a9
edersonrdg <edersonrodrigo31@gmail.com>
2021-08-29 18:38:13 -0300

    feat: requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico (user_id VARCHAR(20))
+RETURNS INT READS SQL DATA
+BEGIN DECLARE music_total INT;
+SELECT
+  COUNT(*) INTO music_total
+FROM
+  SpotifyClone.user_history h
+WHERE
+  h.UserId = user_id;
+RETURN music_total;
+END $$
+
+DELIMITER ;
