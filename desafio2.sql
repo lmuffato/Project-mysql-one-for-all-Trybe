ff08ae693c4f2f1766de2d90c32453df3f20373b
Paulo Henrique <ph.pereira.7@gmail.com>
2021-08-29 14:47:03 -0300

    Desafio 2: fix

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
-SELECT 
+CREATE VIEW estatisticas_musicais AS SELECT 
 (SELECT COUNT(*) FROM cancao) AS cancoes, 
 (SELECT COUNT(*) FROM artista) AS artistas,
 (SELECT COUNT(*) FROM album) AS albuns
