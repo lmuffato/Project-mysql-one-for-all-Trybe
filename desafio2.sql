fc8c4534400bf73b98b32a1905d856f180f2487f
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-08-29 18:29:55 -0300

    feat: requirement 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS
+  SELECT
+    (SELECT COUNT(*) FROM songs) AS "cancoes",
+    (SELECT COUNT(*) FROM artists) AS "artistas" ,
+    (SELECT COUNT(*) FROM albums) AS "albuns";
