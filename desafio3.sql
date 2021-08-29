d3e26fa36608ea997f176b257195774b6b5edc10
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-08-29 12:15:14 -0300

    fix: desafio 3 - corrige codeclimate

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW historico_reproducao_usuarios AS
-	SELECT user_name AS `usuario`,
-	song_name AS `nome`
-	FROM SpotifyClone.users AS u
-	INNER JOIN SpotifyClone.reproduction_history AS rh
-	ON rh.user_id = u.user_id
-	INNER JOIN SpotifyClone.songs AS s
-	ON rh.song_id = s.song_id
-	ORDER BY `usuario`, `nome`;
+SELECT user_name AS `usuario`,
+song_name AS `nome`
+FROM SpotifyClone.users AS u
+INNER JOIN SpotifyClone.reproduction_history AS rh
+ON rh.user_id = u.user_id
+INNER JOIN SpotifyClone.songs AS s
+ON rh.song_id = s.song_id
+ORDER BY `usuario`, `nome`;
