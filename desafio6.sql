20867f53c49c4e8e6294ad2d8662110d8ce47c83
edersonrdg <edersonrodrigo31@gmail.com>
2021-08-29 18:43:37 -0300

    fix: req 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
-CREATE VIEW faturamento_atual AS
-SELECT MIN(plan.Value) AS faturamento_minimo, 
-MAX(plan.Value) AS faturamento_maximo, 
-ROUND(AVG(plan.Value), 2) AS faturamento_medio,
-SUM(plan.Value) AS faturamento_total
-FROM SpotifyClone.plans AS plan
+CREATE VIEW faturamento_atual AS 
+SELECT ROUND(MIN(p.Value), 2) AS faturamento_minimo,
+ROUND(MAX(p.Value), 2) AS faturamento_maximo,
+ROUND(AVG(p.Value), 2) AS faturamento_medio,
+ROUND(SUM(p.Value), 2) AS faturamento_total
+FROM SpotifyClone.plans AS p
 INNER JOIN SpotifyClone.users AS u
-ON plan.id = u.PlanId;
+ON u.PlanId = p.id;
