8d0e6db67e5c46c4e99cf1eecb73281ac9f16802
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-30 05:14:25 -0400

    feat: view that shows artists profile

---
+++ desafio7.sql
@@ -0,0 +1,14 @@
+DROP VIEW IF EXISTS perfil_artistas;
+USE SpotifyClone;
+CREATE VIEW perfil_artistas AS
+SELECT
+    art.artist_name AS `artista`,
+    alb.album_name AS `album`,
+    COUNT(fART.user_id) AS `seguidores`
+FROM artists art
+INNER JOIN albums alb
+    ON art.artist_id = alb.artist_id
+INNER JOIN followed_artists fART
+    ON art.artist_id = fArt.artist_id
+GROUP BY `album`
+ORDER BY `seguidores` DESC, `artista`, `album`; 
