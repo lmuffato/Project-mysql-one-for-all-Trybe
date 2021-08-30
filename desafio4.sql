f4e499fd2a3e6b88193434fffc543b5eb7a909ee
Roberto Natividade <robertonnv@gmail.com>
2021-08-30 12:46:13 -0300

    Desafio 5

--- desafio4.sql
+++ desafio4.sql
@@ -2,10 +2,10 @@ CREATE VIEW top_3_artistas AS  SELECT
     DISTINCT art.artista AS 'artista',
     COUNT(art.artista_id) AS 'seguidores' 
 FROM
-    SpotifyClone.follow_artistas AS foll 
+    SpotifyClone.artistas_seguidos AS seg 
 INNER JOIN
     SpotifyClone.artistas AS art 
-        ON art.artista_id = foll.Artist_id 
+        ON art.artista_id = seg.Artist_id 
 GROUP BY
     art.artista_id 
 ORDER BY
