6ec4f1c3c37655ee8278357a0e640d1c8bc5d4bf
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-30 04:45:17 -0400

    fix: i forgot to save the changes

--- desafio3.sql
+++ desafio3.sql
@@ -2,8 +2,8 @@ DROP VIEW IF EXISTS historico_reproducao_usuarios;
 USE SpotifyClone;
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
-users.user_name AS `usuario`,
-songs.song_name AS `nome`
+  users.user_name AS `usuario`,
+  songs.song_name AS `nome`
 FROM songs_history sgH
 INNER JOIN users users
     ON sgH.user_id = users.user_id
