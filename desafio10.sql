c920beb87cfa2fa18b348f5071a2b51596a05fdc
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-30 11:23:28 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+
+CREATE FUNCTION quantidade_musicas_no_historico(id_usuario INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE HistoricoMusical INT;
+SELECT COUNT(hr.cancao_id)
+FROM HistoricoReproducoes hr 
+WHERE hr.usuario_id = id_usuario
+INTO HistoricoMusical;
+RETURN HistoricoMusical;
+END //
+
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
