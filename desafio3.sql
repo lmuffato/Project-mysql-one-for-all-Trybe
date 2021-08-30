17fb95d0ea2baf5f2491a7db3603b7df3b14ee92
Roberto Natividade <robertonnv@gmail.com>
2021-08-30 10:54:28 -0300

    Desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,15 +1,11 @@
 CREATE VIEW historico_reproducao_usuarios AS  SELECT
-u.nome_do_usuario usuario,
-s.cancao nome 
-from
-SpotifyClone.historico_reproducoes f 
+u.nome_do_usuario usuario, s.cancao nome 
+from SpotifyClone.historico_reproducoes f 
 INNER JOIN
 SpotifyClone.usuarios u 
 ON f.usuario_id = u.usuario_id 
 INNER JOIN
 SpotifyClone.cancoes s 
 ON f.cancao_id = s.cancao_id 
-ORDER BY
-u.nome_do_usuario,
-s.cancao;
+ORDER BY u.nome_do_usuario, s.cancao;
     
