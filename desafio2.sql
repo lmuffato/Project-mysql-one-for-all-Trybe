58dbd76a06f92366333cd43c33b73915d8c51c0e
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-30 06:52:31 -0400

    fix: changing syntax

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,5 @@
-DROP VIEW IF EXISTS estatisticas_musicais ;
-USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
 SELECT
-  (SELECT COUNT(*) FROM songs) AS `cancoes`,
-  (SELECT COUNT(*) FROM artists) AS `artistas`,
-  (SELECT COUNT(*) FROM albums) AS `albuns`;
+  (SELECT COUNT(*) FROM SpotifyClone.songs) AS `cancoes`,
+  (SELECT COUNT(*) FROM SpotifyClone.artists) AS `artistas`,
+  (SELECT COUNT(*) FROM SpotifyClone.albums) AS `albuns`;
