0d30523a29a53a3d8baf00e43d30e9e7f3d59f5d
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-30 05:53:36 -0400

    feat: view that shows most reproducted songs of users with student or family plans

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+DROP VIEW IF EXISTS cancoes_premium;
+USE SpotifyClone;
+SELECT
+    s.song_name `Nome`,
+    COUNT(sH.user_id) `reproducoes`
+FROM songs_history sH
+INNER JOIN songs s
+    ON sH.song_id = s.song_id
+INNER JOIN users u
+    ON sH.user_id = u.user_id
+WHERE u.plan_id IN (2,5)
+GROUP BY `Nome`
+ORDER BY `reproducoes` DESC, `Nome`;
