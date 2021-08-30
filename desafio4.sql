e7c1afbef9ee169a01d9916b13d9f83f50f19f31
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-30 01:46:44 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,6 @@
+CREATE VIEW top_3_artistas AS
+SELECT artista_nome `artista`, COUNT(fa.artista_id) `seguidores`
+FROM artistas ar
+INNER JOIN FaArtistas fa ON fa.usuario_id = ar.artista_id
+GROUP BY artista_nome
+LIMIT 3;
