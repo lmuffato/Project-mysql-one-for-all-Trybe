eb4682c319f2d1e7df44f50631ad0bed53210325
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-30 02:17:02 -0300

    [Fix] Alteraçao LIMIT para HAVING

--- desafio4.sql
+++ desafio4.sql
@@ -2,5 +2,4 @@ CREATE VIEW top_3_artistas AS
 SELECT artista_nome `artista`, COUNT(fa.artista_id) `seguidores`
 FROM artistas ar
 INNER JOIN FaArtistas fa ON fa.usuario_id = ar.artista_id
-GROUP BY artista_nome
-LIMIT 3;
+GROUP BY artista_nome HAVING seguidores > 1;
