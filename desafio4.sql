7d974cded8c420b15a025c154e2aae2730bb4d12
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-30 04:28:27 -0300

    Fix ordenacao (Desafio 4)

--- desafio4.sql
+++ desafio4.sql
@@ -2,4 +2,5 @@ CREATE VIEW top_3_artistas AS
 (SELECT artista_nome `artista`, COUNT(fa.artista_id) `seguidores`
 FROM artistas ar
 INNER JOIN FaArtistas fa ON fa.usuario_id = ar.artista_id
-GROUP BY artista_nome HAVING seguidores > 1);
+GROUP BY artista_nome 
+ORDER BY `seguidores` DESC, `artista` ASC LIMIT 3);
