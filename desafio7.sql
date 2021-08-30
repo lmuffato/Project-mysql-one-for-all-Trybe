c18701fe15933bb5b196bae8561a4b6a07b7804c
pauloeliezerg <pauloeliezerg@gmail.com>
2021-08-30 10:12:00 -0300

    req07 and fix:req06

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artistas
+AS
+SELECT
+  artists.artist AS artista,
+  albums.album AS album,
+  COUNT(*) AS seguidores
+FROM artists AS artists
+JOIN albums AS albums
+  ON albums.artist_id = artists.artist_id;
+JOIN followers AS followers
+  ON followers.artist_id = artist.artist_id
+GROUP BY artists, albums
+ORDER BY seguidores DESC, artista, album;
