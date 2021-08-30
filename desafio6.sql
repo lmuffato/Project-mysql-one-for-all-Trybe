1c53edc2a4072ffe52f009639c7488d52a913469
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-30 05:05:17 -0400

    feat: view that shows min, max, avg and total billing

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+DROP VIEW IF EXISTS faturamento_atual;
+USE SpotifyClone;
+CREATE VIEW faturamento_atual AS
+SELECT
+    MIN(p.plan_price) AS `faturamento_minimo`,
+    MAX(p.plan_price) AS `faturamento_maximo`,
+    ROUND(AVG(p.plan_price), 2) AS `faturamento_medio`,
+    SUM(p.plan_price) AS `faturamento_total`
+FROM plans;
