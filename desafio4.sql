9e1c91d2eb68bfddb39f3fb827418ea51896bb8b
pauloeliezerg <pauloeliezerg@gmail.com>
2021-08-30 09:02:23 -0300

    req04

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_3_artistas
+AS
+SELECT
+  artists.artist AS artista, COUNT(*) AS seguidores
+FROM artists AS artists
+INNER JOIN followers AS followers
+  ON artists.artist_id = followers.artist_id
+ORDER BY seguidores DESC, artista
+LIMIT 3;
