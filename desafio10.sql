289a3b1540f92f601c9b096b5211bcc4bd09a4c5
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-08-30 09:49:06 -0300

    fix: desafio 10 - corrige codeclimate

--- desafio10.sql
+++ desafio10.sql
@@ -3,14 +3,14 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(artist VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE musicas_total INT;
-	SELECT COUNT(h.song_id)
-	FROM SpotifyClone.users AS u
-	inner join SpotifyClone.reproduction_history AS h
-	ON u.user_id = h.user_id
-	WHERE u.user_name = artist
-	GROUP BY u.user_name INTO musicas_total;
-  RETURN musicas_total;
+DECLARE musicas_total INT;
+SELECT COUNT(h.song_id)
+FROM SpotifyClone.users AS u
+inner join SpotifyClone.reproduction_history AS h
+ON u.user_id = h.user_id
+WHERE u.user_name = artist
+GROUP BY u.user_name INTO musicas_total;
+RETURN musicas_total;
 END $$
 
 DELIMITER ;
