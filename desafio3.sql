4214049c3a42a6aa65169ad4693323184e082e22
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-30 06:53:33 -0400

    feat: removing drop if exists and use database

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,10 @@
-DROP VIEW IF EXISTS historico_reproducao_usuarios;
-USE SpotifyClone;
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
   users.user_name AS `usuario`,
   songs.song_name AS `nome`
-FROM songs_history sgH
-INNER JOIN users users
+FROM SpotifyClone.songs_history sgH
+INNER JOIN SpotifyClone.users users
     ON sgH.user_id = users.user_id
-INNER JOIN songs songs
+INNER JOIN SpotifyClone.songs songs
     ON sgH.song_id = songs.song_id
 ORDER BY `usuario`, `nome`;
