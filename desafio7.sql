63413f6180948264242b53789dd130fe4f2156dd
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-30 12:43:18 -0300

    Fix Lint

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 CREATE VIEW perfil_artistas AS
 SELECT
-	ar.artista_nome `artista`,
-	ab.album_nome `album`,
-	COUNT(FA.usuario_id) `seguidores`
+ar.artista_nome `artista`,
+ab.album_nome `album`,
+COUNT(FA.usuario_id) `seguidores`
 FROM artistas ar
 INNER JOIN albuns ab ON ar.artista_id = ab.artista_id 
 INNER JOIN FaArtistas FA ON ar.artista_id = FA.artista_id
