7dd833d3753b009f4c2fb9275a9323a839fa2a8b
Junior Portela <portelajr14@gmail.com>
2021-08-30 03:55:30 -0300

    feat: desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+CREATE VIEW faturamento_anual AS
+SELECT
+  CONVERT( MIN(p.price), DECIMAL(10, 2)) AS 'faturamento_minimo',
+  MAX(p.price) AS 'faturamento_maximo',
+  CONVERT ( ROUND(SUM(p.price)/COUNT(u.user_id), 2), DECIMAL (10, 2)) AS 'faturamento_medio',
+  CONVERT ( SUM(p.price), DECIMAL(10, 2)) AS 'faturament_total'
+FROM
+  SpotifyClone.users AS u
+INNER JOIN
+  SpotifyClone.plans AS p
+ON u.plan_id = p.plan_id;
