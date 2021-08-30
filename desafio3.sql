9326a6bda92540445e2ce8dec77978709c38a7c4
Roberto Natividade <robertonnv@gmail.com>
2021-08-30 10:58:41 -0300

    Desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -5,4 +5,5 @@ INNER JOIN SpotifyClone.usuarios u
 ON f.usuario_id = u.usuario_id 
 INNER JOIN SpotifyClone.cancoes s 
 ON f.cancao_id = s.cancao_id 
-ORDER BY u.nome_do_usuario, s.cancao;    
+ORDER BY u.nome_do_usuario, s.cancao;   
+ 
